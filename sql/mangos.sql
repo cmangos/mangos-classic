18d1b6dafab2871e6317d76df8b555ee66071ab9
NeatElves <NeatElves@mangos.ru>
2008-12-24 07:10:44 +0300

    [6939] Fixing problems with `quest_template`.`RewHonorableKills`.
    1) field not been added in time original sql update.
    2) provided default value for field.
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    Backported from master branch with appropriate changes.
    SQL update order propetection removed for backported sql updates
    for allow later update DB without problems.
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- sql/mangos.sql
+++ sql/mangos.sql
@@ -10336,6 +10336,7 @@ CREATE TABLE `quest_template` (
   `RewRepValue3` mediumint(9) NOT NULL default '0',
   `RewRepValue4` mediumint(9) NOT NULL default '0',
   `RewRepValue5` mediumint(9) NOT NULL default '0',
+  `RewHonorableKills` int unsigned NOT NULL default '0',
   `RewOrReqMoney` int(11) NOT NULL default '0',
   `RewMoneyMaxLevel` int(10) unsigned NOT NULL default '0',
   `RewSpell` mediumint(8) unsigned NOT NULL default '0',
---
+++ sql/updates/6939_01_mangos_quest_template.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `quest_template`
+    CHANGE `RewHonorableKills` `RewHonorableKills` int unsigned NOT NULL default '0';
