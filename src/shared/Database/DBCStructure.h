bb7f7f9a08f2f488ec1a5305d8361f17bb466cd5
Energy <Energy@mangos.ru>
2009-01-23 19:33:21 +0300

    [7155] One more mount allowed instance.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    (cherry picked from commit 45079c7e068a108d73fc5133f1e6105a7187d98d)
    
    Also added some map id from more early author's patch for this check.
    
    Conflicts:
    
    	src/shared/Database/DBCStructure.h
    	src/shared/revision_nr.h

--- src/shared/Database/DBCStructure.h
+++ src/shared/Database/DBCStructure.h
@@ -448,8 +448,14 @@ struct MapEntry
     bool IsMountAllowed() const
     {
         return !IsDungeon() ||
-            MapID==568 || MapID==309 || MapID==209 || MapID==534 ||
-            MapID==560 || MapID==509 || MapID==269;
+            MapID==209 || MapID==269 || MapID==309 ||       // TanarisInstance, CavernsOfTime, Zul'gurub
+            MapID==509 || MapID==534 || MapID==560 ||       // AhnQiraj, HyjalPast, HillsbradPast
+            MapID==568 || MapID==580;                       // ZulAman, Sunwell Plateau
+    }
+
+    bool IsContinent() const
+    {
+        return MapID == 0 || MapID == 1 || MapID == 530;
     }
 };
 
