4a9e15169b141735664cb1ab52ab939efc224ae2
VladimirMangos <vladimir@getmangos.com>
2009-01-25 11:08:26 +0300

    [7166] Make reserved name check case-insensitive.
    Original patch provided by jpmythic.
    (cherry picked from commit ee9c4c4a90b51e90438823bb9b1c82bfa76a2c80)
    
    Conflicts:
    
    	src/shared/revision_nr.h
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/ObjectMgr.h
+++ src/game/ObjectMgr.h
@@ -701,10 +701,7 @@ class ObjectMgr
 
         // reserved names
         void LoadReservedPlayersNames();
-        bool IsReservedName(const std::string& name) const
-        {
-            return m_ReservedNames.find(name) != m_ReservedNames.end();
-        }
+        bool IsReservedName(const std::string& name) const;
 
         // name with valid structure and symbols
         static bool IsValidName( const std::string& name, bool create = false );
@@ -830,7 +827,7 @@ class ObjectMgr
         PetCreateSpellMap   mPetCreateSpell;
 
         //character reserved names
-        typedef std::set<std::string> ReservedNamesMap;
+        typedef std::set<std::wstring> ReservedNamesMap;
         ReservedNamesMap    m_ReservedNames;
 
         GraveYardMap        mGraveYardMap;
