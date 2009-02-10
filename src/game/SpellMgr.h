95819b685af757f1cff0bd8f0788530814a35fc1
VladimirMangos <vladimir@getmangos.com>
2009-02-10 07:40:03 +0300

    [7253] Check battleground status in GetSpellAllowedInLocationError for some spells for aura remove and spell casting cases. (cherry picked from commit 7d9720ce74ee17c5df5dd2b18d20ec2c03cf2449)

--- src/game/SpellMgr.h
+++ src/game/SpellMgr.h
@@ -323,7 +323,7 @@ bool IsPositiveTarget(uint32 targetA, uint32 targetB);
 bool IsSingleTargetSpell(SpellEntry const *spellInfo);
 bool IsSingleTargetSpells(SpellEntry const *spellInfo1, SpellEntry const *spellInfo2);
 
-uint8 GetSpellAllowedInLocationError(SpellEntry const *spellInfo,uint32 map_id,uint32 zone_id,uint32 area_id);
+uint8 GetSpellAllowedInLocationError(SpellEntry const *spellInfo,uint32 map_id,uint32 zone_id,uint32 area_id,uint32 bgInstanceId);
 
 inline bool IsAreaEffectTarget( Targets target )
 {
