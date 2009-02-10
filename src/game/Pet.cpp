a83270e4269c1619f0926220db80f80a76404fcb
VladimirMangos <vladimir@getmangos.com>
2009-02-10 06:25:39 +0300

    [7259] Prevent memory lost at pet spell reset. (cherry picked from commit a9a49af57bcc0abcea92f6bb4c992dabd18fa31b)

--- src/game/Pet.cpp
+++ src/game/Pet.cpp
@@ -1586,8 +1586,12 @@ void Pet::InitPetCreateSpells()
 {
     m_charmInfo->InitPetActionBar();
 
+    for (PetSpellMap::iterator i = m_spells.begin(); i != m_spells.end(); ++i)
+        delete i->second;
     m_spells.clear();
+
     int32 usedtrainpoints = 0;
+
     uint32 petspellid;
     PetCreateSpellEntry const* CreateSpells = objmgr.GetPetCreateSpellEntry(GetEntry());
     if(CreateSpells)
