ecadc840cae38d39a566b92e974c834903b3d7b3
arrai <array.of.intellect@gmail.com>
2009-03-19 23:33:56 +0100

    [7501] Fixed crash caused by .distance command

--- src/game/Level3.cpp
+++ src/game/Level3.cpp
@@ -3155,7 +3155,7 @@ bool ChatHandler::HandleGuildDeleteCommand(const char* args)
 
 bool ChatHandler::HandleGetDistanceCommand(const char* args)
 {
-    WorldObject* obj;
+    WorldObject* obj = NULL;
 
     if (*args)
     {
