10593df7458f5b37795baa3650cda7cf9c968b5a
Ramses_II <Ramses_II@getmangos.com>
2009-03-28 04:53:30 +0300

    [7559] Implement function Player::RewardPlayerAndGroupAtEvent for reward (quest objective only) player and group at special kill-like events.
    Use it for Spell::EffectKillCredit and some dummy code.
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    (cherry picked from commit d5d7feb8dc57aee0e1487510177aa542a7a2912a)
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/Player.h
+++ src/game/Player.h
@@ -1656,6 +1656,7 @@ class MANGOS_DLL_SPEC Player : public Unit
 
         bool IsAtGroupRewardDistance(WorldObject const* pRewardSource) const;
         bool RewardPlayerAndGroupAtKill(Unit* pVictim);
+        void RewardPlayerAndGroupAtEvent(uint32 creature_id,WorldObject* pRewardSource);
 
         ReputationMgr&       GetReputationMgr()       { return m_reputationMgr; }
         ReputationMgr const& GetReputationMgr() const { return m_reputationMgr; }
