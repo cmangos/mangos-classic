f5873d6e111cb579a5b4cf96b24cbd5715ced2c3
rastikzzz <rastikzzz@getmangos.com>
2009-04-06 14:42:34 +0400

    [7626] Fixed: Typo in InitStatsForLevel
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    (cherry picked from commit eaaf9d7fed9272687059a9e4d1af805a552319be)
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -2326,7 +2326,7 @@ void Player::InitStatsForLevel(bool reapplyMods)
     SetUInt32Value(PLAYER_FIELD_MOD_TARGET_PHYSICAL_RESISTANCE,0);
     for(int i = 0; i < MAX_SPELL_SCHOOL; ++i)
     {
-        SetFloatValue(UNIT_FIELD_POWER_COST_MODIFIER+i,0.0f);
+        SetUInt32Value(UNIT_FIELD_POWER_COST_MODIFIER+i,0);
         SetFloatValue(UNIT_FIELD_POWER_COST_MULTIPLIER+i,0.0f);
     }
     // Init data for form but skip reapply item mods for form
