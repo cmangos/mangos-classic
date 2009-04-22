f229a36bfb715f2a88a2cb47cf6c1866d0c43af7
NoFantasy <NoFantasy@getmangos.com>
2009-04-22 15:36:38 +0400

    [7695] Fixed typo & -> &&
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    (cherry picked from commit 93b6d510271ddc82b790ddb678b9890efb6f6bcc)

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7000,7 +7000,7 @@ bool Unit::AttackStop(bool targetSwitch /*=false*/)
 
 void Unit::CombatStop(bool cast)
 {
-    if(cast& IsNonMeleeSpellCasted(false))
+    if (cast && IsNonMeleeSpellCasted(false))
         InterruptNonMeleeSpells(false);
 
     AttackStop();
