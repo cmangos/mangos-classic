3fafcb87ef108cfc58a7ef4621c319caeba3158b
NoFantasy <NoFantasy@getmangos.com>
2009-06-01 05:28:37 +0400

    [7933] Call goober GO event script not only at open spell cast, also really call camera GO event scripts.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    (cherry picked from commit 1fd70e5cc9c22313197fcfce6294f810654e5021)
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/GameObject.cpp
+++ src/game/GameObject.cpp
@@ -955,6 +955,9 @@ void GameObject::Use(Unit* user)
 
                 // possible quest objective for active quests
                 player->CastedCreatureOrGO(info->id, GetGUID(), 0);
+
+                if (info->goober.eventId)
+                    sWorld.ScriptsStart(sEventScripts, info->goober.eventId, player, this);
             }
 
             // cast this spell later if provided
@@ -975,6 +978,9 @@ void GameObject::Use(Unit* user)
 
             if (info->camera.cinematicId)
                 player->SendCinematicStart(info->camera.cinematicId);
+
+            if (info->camera.eventID)
+                sWorld.ScriptsStart(sEventScripts, info->camera.eventID, player, this);
 
             return;
         }
