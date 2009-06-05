eff546800abde5a95b0341dd415504d3e0ad781d
VladimirMangos <vladimir@getmangos.com>
2009-06-05 05:18:40 +0400

    Code cleanups and fixes from master for vmap_extractor_v2 that not affect fucntionality
    * Replace tabs by spaces and other cleanups in vmap_extractor_v2
    * Memory leaks, unclear data access ways and strange typecasts.
    * Better error output.
    * Binary rebuilded (note: it can't be used for 0.13 and higher vmap extraction)

--- contrib/vmap_extractor_v2/vmapextract/model.cpp
+++ contrib/vmap_extractor_v2/vmapextract/model.cpp
@@ -1,10 +1,6 @@
-//#include "common.h"
 #include "model.h"
-//#include "world.h"
 #include <cassert>
 #include <algorithm>
-
-//int globalTime = 0;
 
 Model::Model(std::string &filename) : filename(filename)
 {
@@ -16,7 +12,7 @@ bool Model::open()
 
     ok = !f.isEof();
 
-    if (!ok) 
+    if (!ok)
     {
         f.close();
         printf("Error loading model %s\n", filename.c_str());
@@ -24,28 +20,19 @@ bool Model::open()
     }
 
     memcpy(&header, f.getBuffer(), sizeof(ModelHeader));
-    if(header.nBoundingTriangles > 0) {
-
-#if 0
-        animated = isAnimated(f);
-        if(animated)
-        {
-            f.close();
-            return false;
-        }
-#endif
-        trans = 1.0f;
+    if(header.nBoundingTriangles > 0)
+    {
         origVertices = (ModelVertex*)(f.getBuffer() + header.ofsVertices);
 
         vertices = new Vec3D[header.nVertices];
         normals = new Vec3D[header.nVertices];
 
-        for (size_t i=0; i<header.nVertices; i++) 
+        for (size_t i=0; i<header.nVertices; i++)
         {
             origVertices[i].pos = fixCoordSystem(origVertices[i].pos);
             origVertices[i].normal = fixCoordSystem(origVertices[i].normal);
             vertices[i] = origVertices[i].pos;
-            normals[i] = origVertices[i].normal.normalize();	
+            normals[i] = origVertices[i].normal.normalize();
         }
 
         ModelView *view = (ModelView*)(f.getBuffer() + header.ofsViews);
@@ -55,94 +42,23 @@ bool Model::open()
 
         nIndices = view->nTris;
         indices = new uint16[nIndices];
-        for (size_t i = 0; i<nIndices; i++) 
+        for (size_t i = 0; i<nIndices; i++)
         {
             indices[i] = indexLookup[triangles[i]];
         }
         f.close();
-    } else {
+    } 
+    else 
+    {
         //printf("not included %s\n", filename.c_str());
+        f.close();
         return false;
     }
     return true;
-
-}
-
-bool Model::isAnimated(MPQFile &f)
-{
-    // see if we have any animated bones
-    ModelBoneDef *bo = (ModelBoneDef*)(f.getBuffer() + header.ofsBones);
-
-    animGeometry = false;
-    animBones = false;
-    ind = false;
-
-    ModelVertex *verts = (ModelVertex*)(f.getBuffer() + header.ofsVertices);
-    for (size_t i=0; i<header.nVertices && !animGeometry; i++) {
-        for (size_t b=0; b<4; b++) {
-            if (verts[i].weights[b]>0) {
-                ModelBoneDef &bb = bo[verts[i].bones[b]];
-                if (bb.translation.type || bb.rotation.type || bb.scaling.type || (bb.flags&8)) {
-                    if (bb.flags&8) {
-                        // if we have billboarding, the model will need per-instance animation
-                        ind = true;
-                    }
-                    animGeometry = true;
-                    break;
-                }
-            }
-        }
-    }
-
-    if (animGeometry) animBones = true;
-    else {
-        for (size_t i=0; i<header.nBones; i++) {
-            ModelBoneDef &bb = bo[i];
-            if (bb.translation.type || bb.rotation.type || bb.scaling.type) {
-                animBones = true;
-                break;
-            }
-        }
-    }
-
-    animTextures = header.nTexAnims > 0;
-
-    bool animMisc = header.nCameras>0 || // why waste time, pretty much all models with cameras need animation
-        header.nLights>0 || // same here
-        header.nParticleEmitters>0 ||
-        header.nRibbonEmitters>0;
-
-    if (animMisc) animBones = true;
-
-    // animated colors
-    if (header.nColors) {
-        ModelColorDef *cols = (ModelColorDef*)(f.getBuffer() + header.ofsColors);
-        for (size_t i=0; i<header.nColors; i++) {
-            if (cols[i].color.type!=0 || cols[i].opacity.type!=0) {
-                animMisc = true;
-                break;
-            }
-        }
-    }
-
-    // animated opacity
-    if (header.nTransparency && !animMisc) {
-        ModelTransDef *trs = (ModelTransDef*)(f.getBuffer() + header.ofsTransparency);
-        for (size_t i=0; i<header.nTransparency; i++) {
-            if (trs[i].trans.type!=0) {
-                animMisc = true;
-                break;
-            }
-        }
-    }
-
-    // guess not...
-    return animGeometry || animTextures || animMisc;
 }
 
 bool Model::ConvertToVMAPModel(char * outfilename)
 {
-
     int N[] = {0x00000000};
     FILE * output=fopen(outfilename,"wb");
     if(!output)
@@ -162,23 +78,23 @@ bool Model::ConvertToVMAPModel(char * outfilename)
     wsize = sizeof(branches) + sizeof(uint32) * branches;
     fwrite(&wsize, sizeof(int), 1, output);
     fwrite(&branches,sizeof(branches), 1, output);
-    uint32 nIdexes = (uint32) nIndices;
+    uint32 nIndexes = (uint32) nIndices;
     fwrite(&nIndices,sizeof(uint32), 1, output);
     fwrite("INDX",4, 1, output);
-    wsize = sizeof(uint32) + sizeof(unsigned short) * nIdexes;
+    wsize = sizeof(uint32) + sizeof(unsigned short) * nIndexes;
     fwrite(&wsize, sizeof(int), 1, output);
-    fwrite(&nIdexes, sizeof(uint32), 1, output);
-    if(nIdexes >0) 
+    fwrite(&nIndexes, sizeof(uint32), 1, output);
+    if(nIndexes >0)
     {
-        fwrite(indices, sizeof(unsigned short), nIdexes, output);
+        fwrite(indices, sizeof(unsigned short), nIndexes, output);
     }
     fwrite("VERT",4, 1, output);
     wsize = sizeof(int) + sizeof(float) * 3 * nVertices;
     fwrite(&wsize, sizeof(int), 1, output);
     fwrite(&nVertices, sizeof(int), 1, output);
-    if(nVertices >0) 
+    if(nVertices >0)
     {
-        for(int vpos=0; vpos <nVertices; ++vpos) 
+        for(int vpos=0; vpos <nVertices; ++vpos)
         {
             float sy = vertices[vpos].y;
             vertices[vpos].y = vertices[vpos].z;
@@ -226,26 +142,24 @@ ModelInstance::ModelInstance(MPQFile &f,const char* ModelInstName,const char*Map
     sprintf(tempname, ".\\buildings\\%s", ModelInstName);
     FILE *input;
     input = fopen(tempname, "r+b");
-    if(!input)
-    {
-        return;
-    }
-    fseek(input, 8, SEEK_SET); // get the correct no of vertices
-	int nVertices;
-	fread(&nVertices, sizeof (int), 1, input);
-	fclose(input);
-    if(nVertices == 0)
-    {
-        return;
-    }
 
-    if(pDirfile) 
+    if(!input)
+        return;
+
+    fseek(input, 8, SEEK_SET); // get the correct no of vertices
+    int nVertices;
+    fread(&nVertices, sizeof (int), 1, input);
+    fclose(input);
+
+    if(nVertices == 0)
+        return;
+
+    if(pDirfile)
     {
         int realx1 = (int) ((float) pos.x / 533.333333f);
         int realy1 = (int) ((float) pos.z / 533.333333f);
         int realx2 = (int) ((float) pos.x / 533.333333f);
         int realy2 = (int) ((float) pos.z / 533.333333f);
-
 
         fprintf(pDirfile,"%s/%s %f,%f,%f_%f,%f,%f %f %d %d %d,%d %d\n",
             MapName,
@@ -256,6 +170,6 @@ ModelInstance::ModelInstance(MPQFile &f,const char* ModelInstName,const char*Map
             nVertices,
             realx1, realy1,
             realx2, realy2
-            );				
+            );
     }
 }
--- contrib/vmap_extractor_v2/vmapextract/model.h
+++ contrib/vmap_extractor_v2/vmapextract/model.h
@@ -4,7 +4,6 @@
 #include "vec3d.h"
 #include "mpq.h"
 #include "modelheaders.h"
-//#include "quaternion.h"
 #include <vector>
 
 class Model;
@@ -12,65 +11,40 @@ class WMOInstance;
 
 Vec3D fixCoordSystem(Vec3D v);
 
-
-
-
 class Model
-{ 
+{
 public:
-	ModelHeader header;
-	ModelAnimation *anims;
-	int *globalSequences;
-	
-public:
-	bool animGeometry,animTextures,animBones;
-	bool animated;
+    ModelHeader header;
+    ModelVertex *origVertices;
+    Vec3D *vertices, *normals;
+    uint16 *indices;
+    size_t nIndices;
 
-	bool isAnimated(MPQFile &f);
-	ModelVertex *origVertices;
-	Vec3D *vertices, *normals;
-	uint16 *indices;
-	size_t nIndices;
-	
-	bool open();
-	bool ConvertToVMAPModel(char * outfilename);
+    bool open();
+    bool ConvertToVMAPModel(char * outfilename);
 
-public:
+    bool ok;
 
-	bool ok;
-	bool ind;
+    Model(std::string &filename);
+    ~Model();
 
-	float rad;
-	float trans;
-	bool animcalc;
-	int anim, animtime;
-
-	Model(std::string &filename);
-	~Model();
-	
 private:
-	std::string filename;
-	char outfilename;
+    std::string filename;
+    char outfilename;
 };
 
 class ModelInstance 
 {
 public:
-	Model *model;
+    Model *model;
 
-	int id;
+    int id;
+    Vec3D pos, rot;
+    unsigned int d1, scale;
+    float w,sc;
 
-	Vec3D pos, rot;
-	unsigned int d1, scale;
-
-	float frot,w,sc;
-
-	int light;
-	Vec3D ldir;
-	Vec3D lcol;
-
-	ModelInstance() {}
-	ModelInstance(MPQFile &f,const char* ModelInstName,const char*MapName, FILE *pDirfile);
+    ModelInstance() {}
+    ModelInstance(MPQFile &f,const char* ModelInstName,const char*MapName, FILE *pDirfile);
 
 };
 
