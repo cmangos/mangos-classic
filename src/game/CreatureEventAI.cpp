90ba3af02c235ae0b5cca80bc1563e8c2eac3c17
VladimirMangos <vladimir@getmangos.com>
2009-06-15 12:16:10 +0400

    Use proper aura stack size code for pre-0.13 code.

--- src/game/CreatureEventAI.cpp
+++ src/game/CreatureEventAI.cpp
@@ -297,8 +297,7 @@ bool CreatureEventAI::ProcessEvent(CreatureEventAIHolder& pHolder, Unit* pAction
         {
             //Note: checked only aura for effect 0, if need check aura for effect 1/2 then
             // possible way: pack in event.buffed.amount 2 uint16 (ammount+effectIdx)
-            Aura* aura = m_creature->GetAura(event.buffed.spellId,0);
-            if(!aura || aura->GetStackAmount() < event.buffed.amount)
+            if(m_creature->GetAuras().count(Unit::spellEffectPair(event.buffed.spellId,0)) < event.buffed.amount)
                 return false;
 
             //Repeat Timers
@@ -313,8 +312,7 @@ bool CreatureEventAI::ProcessEvent(CreatureEventAIHolder& pHolder, Unit* pAction
 
             //Note: checked only aura for effect 0, if need check aura for effect 1/2 then
             // possible way: pack in event.buffed.amount 2 uint16 (ammount+effectIdx)
-            Aura* aura = pActionInvoker->GetAura(event.buffed.spellId,0);
-            if(!aura || aura->GetStackAmount() < event.buffed.amount)
+            if(pActionInvoker->GetAuras().count(Unit::spellEffectPair(event.buffed.spellId,0)) < event.buffed.amount)
                 return false;
 
             //Repeat Timers
