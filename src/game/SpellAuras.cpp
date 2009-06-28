c8f5834f9cf4e69c2c7b484fecf8e7458d159c0d
ApoC <apoc@nymfe.net>
2009-06-28 21:00:49 +0200

    Allow NPCs to apply taunt.
    Signed-off-by: ApoC <apoc@nymfe.net>
    (cherry picked from commit c2a83d348261fa96286493225e3d2c8504169639)

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -3605,7 +3605,7 @@ void Aura::HandleModTaunt(bool apply, bool Real)
 
     Unit* caster = GetCaster();
 
-    if(!caster || !caster->isAlive() || caster->GetTypeId() != TYPEID_PLAYER)
+    if(!caster || !caster->isAlive())
         return;
 
     if(apply)
