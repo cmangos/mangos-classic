d1a8c64d4160aead9da3fc3ca68ffcd8e76f6060
VladimirMangos <vladimir@getmangos.com>
2009-07-11 05:42:24 +0400

    [7971] Better DB data checks at pet loading and stable use.
    (cherry picked from commit b8ce292c7048fb8c4e4a06971c3859efe2421f28)

--- src/game/Pet.cpp
+++ src/game/Pet.cpp
@@ -175,8 +175,19 @@ bool Pet::LoadPetFromDB( Player* owner, uint32 petentry, uint32 petnumber, bool 
         return false;
     }
 
-    uint32 pet_number = fields[0].GetUInt32();
+    PetType pet_type = PetType(fields[22].GetUInt8());
+    if(pet_type==HUNTER_PET)
+    {
+        CreatureInfo const* creatureInfo = objmgr.GetCreatureTemplate(petentry);
+        if(!creatureInfo || !creatureInfo->isTameable())
+        {
+            delete result;
+            return false;
+        }
+    }
 
+    uint32 pet_number = fields[0].GetUInt32();
+    
     if (current && owner->IsPetNeedBeTemporaryUnsummoned())
     {
         owner->SetTemporaryUnsummonedPetNumber(pet_number);
@@ -205,7 +216,7 @@ bool Pet::LoadPetFromDB( Player* owner, uint32 petentry, uint32 petnumber, bool 
         return false;
     }
 
-    setPetType(PetType(fields[22].GetUInt8()));
+    setPetType(pet_type);
     setFaction(owner->getFaction());
     SetUInt32Value(UNIT_CREATED_BY_SPELL, summon_spell_id);
 
