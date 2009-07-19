e2d3cdb3994fb9f91a3636b57cc2a0e460a39578
NoFantasy <nofantasy@nf.no>
2009-07-19 21:13:58 +0400

    [8208] Add part support for spell 33525.
    Also let spell 33652 trigger when aura from 33572 reach full effect.
    
    Signed-off-by: NoFantasy <nofantasy@nf.no>
    
    (backported from commit d50307b)
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -1882,6 +1882,10 @@ void Aura::TriggerSpell()
                 caster->CastCustomSpell(target, trigger_spell_id, &m_modifier.m_amount, NULL, NULL, true, NULL, this);
                 return;
             }
+            // Ground Slam
+            case 33525:
+                target->CastSpell(target, trigger_spell_id, true);
+                return;
         }
     }
     // All ok cast by default case
@@ -3711,11 +3715,22 @@ void Aura::HandleAuraModIncreaseSwimSpeed(bool /*apply*/, bool Real)
     m_target->UpdateSpeed(MOVE_SWIM, true);
 }
 
-void Aura::HandleAuraModDecreaseSpeed(bool /*apply*/, bool Real)
+void Aura::HandleAuraModDecreaseSpeed(bool apply, bool Real)
 {
     // all applied/removed only at real aura add/remove
     if(!Real)
         return;
+
+    if (apply)
+    {
+        // Gronn Lord's Grasp, becomes stoned
+        if (GetId() == 33572)
+        {
+            if (m_target->GetAuras().count(Unit::spellEffectPair(GetId(),GetEffIndex())) >= 5 &&
+                !m_target->HasAura(33652))
+                m_target->CastSpell(m_target, 33652, true);
+        }
+    }
 
     m_target->UpdateSpeed(MOVE_RUN, true);
     m_target->UpdateSpeed(MOVE_SWIM, true);
