c52a5aee48979fc42535dbfcadf6ee7618427408
VladimirMangos <vladimir@getmangos.com>
2009-08-30 15:07:13 +0400

    Implement check DBs versions (required_* fields) at mangosd/realmd loading.
    * git_id updated to generate revision_sql.h file with required_* fields strings.
    * mangosd/realmd changed to include header and check this strings at startup.
    * mangosd/realmd will terminated if related strings not match in DB content.
      In most cases this meaning that not all expected sql updates applied.
      Current required_* field stored in DB output in error to help find what last sql updates applied.
    
    IMPORTNAT NOTE for mangos devs: please update used git_id before adding next commits with sql updates!
    
    (backported from commit e24b0a2)

---
+++ src/shared/revision_sql.h
@@ -0,0 +1,6 @@
+#ifndef __REVISION_SQL_H__
+#define __REVISION_SQL_H__
+ #define REVISION_DB_CHARACTERS "required_061_7067_03_characters_character_spell"
+ #define REVISION_DB_MANGOS "required_062______01_mangos_spell_learn_spell"
+ #define REVISION_DB_REALMD "required_058_7546_02_realmd_uptime"
+#endif // __REVISION_SQL_H__
