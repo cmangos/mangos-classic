2d6f536e7321f59f03d0f8cbb46f3fb63d2464c3
balrok <balrok@getmangos.com>
2009-10-08 01:38:43 +0200

    fixed problem with spiritguides
    the 2584-aura wasn't applied and so the player
    never could abort the resurrection
    through that he could run around everywhere but got
    still resurrected

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -2101,7 +2101,7 @@ void Spell::EffectApplyAura(uint32 i)
             return;
 
     // ghost spell check, allow apply any auras at player loading in ghost mode (will be cleanup after load)
-    if( !unitTarget->isAlive() && m_spellInfo->Id != 20584 && m_spellInfo->Id != 8326 &&
+    if( !unitTarget->isAlive() && m_spellInfo->Id != 20584 && m_spellInfo->Id != 8326 && m_spellInfo->Id != 2584 &&
         (unitTarget->GetTypeId()!=TYPEID_PLAYER || !((Player*)unitTarget)->GetSession()->PlayerLoading()) )
         return;
 
