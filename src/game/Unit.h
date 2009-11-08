dba947a65655dfd96ede71ffe4136ac5daa57e78
Yehonal <hw.2@hotmail.it>
2009-11-08 11:12:20 +0100

    +fix npcflags to pre-bc state

--- src/game/Unit.h
+++ src/game/Unit.h
@@ -512,33 +512,33 @@ enum UnitFlags2
 
 /// Non Player Character flags
 enum NPCFlags
-{
+{   //[-ZERO]
     UNIT_NPC_FLAG_NONE                  = 0x00000000,
     UNIT_NPC_FLAG_GOSSIP                = 0x00000001,       // 100%
     UNIT_NPC_FLAG_QUESTGIVER            = 0x00000002,       // guessed, probably ok
-    UNIT_NPC_FLAG_UNK1                  = 0x00000004,
-    UNIT_NPC_FLAG_UNK2                  = 0x00000008,
+    //UNIT_NPC_FLAG_UNK1                  = 0x00000004,
+    //UNIT_NPC_FLAG_UNK2                  = 0x00000008,
     UNIT_NPC_FLAG_TRAINER               = 0x00000010,       // 100%
-    UNIT_NPC_FLAG_TRAINER_CLASS         = 0x00000020,       // 100%
-    UNIT_NPC_FLAG_TRAINER_PROFESSION    = 0x00000040,       // 100%
-    UNIT_NPC_FLAG_VENDOR                = 0x00000080,       // 100%
-    UNIT_NPC_FLAG_VENDOR_AMMO           = 0x00000100,       // 100%, general goods vendor
-    UNIT_NPC_FLAG_VENDOR_FOOD           = 0x00000200,       // 100%
-    UNIT_NPC_FLAG_VENDOR_POISON         = 0x00000400,       // guessed
-    UNIT_NPC_FLAG_VENDOR_REAGENT        = 0x00000800,       // 100%
-    UNIT_NPC_FLAG_REPAIR                = 0x00001000,       // 100%
-    UNIT_NPC_FLAG_FLIGHTMASTER          = 0x00002000,       // 100%
-    UNIT_NPC_FLAG_SPIRITHEALER          = 0x00004000,       // guessed
+    //UNIT_NPC_FLAG_TRAINER_CLASS         = 0x00000020,       // 100%
+    //UNIT_NPC_FLAG_TRAINER_PROFESSION    = 0x00000040,       // 100%
+    UNIT_NPC_FLAG_VENDOR                = 0x00000004,       // 100%
+    //UNIT_NPC_FLAG_VENDOR_AMMO           = 0x00000100,       // 100%, general goods vendor || BC
+    //UNIT_NPC_FLAG_VENDOR_FOOD           = 0x00000200,       // 100% || BC
+    //UNIT_NPC_FLAG_VENDOR_POISON         = 0x00000400,       // guessed || BC
+    //UNIT_NPC_FLAG_VENDOR_REAGENT        = 0x00000800,       // 100% || BC
+    UNIT_NPC_FLAG_REPAIR                = 0x00004000,       // 100%
+    UNIT_NPC_FLAG_FLIGHTMASTER          = 0x00000008,       // 100%
+    UNIT_NPC_FLAG_SPIRITHEALER          = 0x00000020,       // guessed
     UNIT_NPC_FLAG_SPIRITGUIDE           = 0x00008000,       // guessed
-    UNIT_NPC_FLAG_INNKEEPER             = 0x00010000,       // 100%
-    UNIT_NPC_FLAG_BANKER                = 0x00020000,       // 100%
-    UNIT_NPC_FLAG_PETITIONER            = 0x00040000,       // 100% 0xC0000 = guild petitions, 0x40000 = arena team petitions
-    UNIT_NPC_FLAG_TABARDDESIGNER        = 0x00080000,       // 100%
-    UNIT_NPC_FLAG_BATTLEMASTER          = 0x00100000,       // 100%
-    UNIT_NPC_FLAG_AUCTIONEER            = 0x00200000,       // 100%
-    UNIT_NPC_FLAG_STABLEMASTER          = 0x00400000,       // 100%
-    UNIT_NPC_FLAG_SPELLCLICK            = 0x01000000,       // cause client to send 1015 opcode (spell click)
-    UNIT_NPC_FLAG_GUARD                 = 0x10000000,       // custom flag for guards
+    UNIT_NPC_FLAG_INNKEEPER             = 0x00000080,       // 100%
+    UNIT_NPC_FLAG_BANKER                = 0x00000100,       // 100%
+    UNIT_NPC_FLAG_PETITIONER            = 0x00000200,       // 100% 0xC0000 = guild petitions
+    UNIT_NPC_FLAG_TABARDDESIGNER        = 0x00000400,       // 100%
+    UNIT_NPC_FLAG_BATTLEMASTER          = 0x00000800,       // 100%
+    UNIT_NPC_FLAG_AUCTIONEER            = 0x00001000,       // 100%
+    UNIT_NPC_FLAG_STABLEMASTER          = 0x00002000,       // 100%
+    UNIT_NPC_FLAG_GUARD                 = 0x00000040,       // custom flag for guards
+    UNIT_NPC_FLAG_OUTDOORPVP            = 0x20000000,       // custom flag for outdoor pvp creatures || Custom flag
 };
 
 // used in SMSG_MONSTER_MOVE
