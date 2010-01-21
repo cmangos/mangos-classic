ce87bbb5480aa45a7daa87f988515b1e00479d04
Yehonal <hw.2@hotmail.it>
2010-01-21 13:44:28 +0100

    [z0102] added missing revision changing

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_z0082_xxx_01_characters_character_social"
- #define REVISION_DB_MANGOS "required_z0096_xxx_01_mangos"
+ #define REVISION_DB_MANGOS "required_z0101_xxx_01_mangos_item_template"
  #define REVISION_DB_REALMD "required_9010_01_realmd_realmlist"
 #endif // __REVISION_SQL_H__
