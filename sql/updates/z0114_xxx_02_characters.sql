88a2b77db4cef3238cacb2ceec9689cb10e7372f
Yehonal <hw.2@hotmail.it>
2010-01-27 12:07:11 +0100

    [z0124] fixed 2 sql updates

--- sql/updates/z0114_xxx_02_characters.sql
+++ sql/updates/z0114_xxx_02_characters.sql
@@ -1,4 +1,4 @@
-ALTER TABLE db_version CHANGE COLUMN required_z0114_xxx_01_character_kill required_z0114_xxx_02_characters bit;
+ALTER TABLE character_db_version CHANGE COLUMN required_z0114_xxx_01_character_kill required_z0114_xxx_02_characters bit;
 
 
 ALTER TABLE characters CHANGE COLUMN honor_rating stored_honor_rating FLOAT NOT NULL DEFAULT '0';
