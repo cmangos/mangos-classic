b6ba1c20ec0cf9d61c1ad01f7882066a92a88e3a
VladimirMangos <vladimir@getmangos.com>
2010-02-03 00:09:06 +0300

    [z0146] Prevent weapon skill grow at player's pet hit.
    (based on master commit 4cf4e87)
    
    (based on commit 061b4a3)
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6231,6 +6231,14 @@ Unit *Unit::GetCharmer() const
     return NULL;
 }
 
+bool Unit::IsCharmerOrOwnerPlayerOrPlayerItself() const
+{
+    if (GetTypeId()==TYPEID_PLAYER)
+        return true;
+
+    return IS_PLAYER_GUID(GetCharmerOrOwnerGUID());
+}
+
 Player* Unit::GetCharmerOrOwnerPlayerOrPlayerItself()
 {
     uint64 guid = GetCharmerOrOwnerGUID();
