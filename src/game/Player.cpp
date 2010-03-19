b519113c9f32cab7a8999da4cf7d92a8b0bf6f76
VladimirMangos <vladimir@getmangos.com>
2010-03-19 10:37:59 +0300

    [z0307] Reset UNIT_FIELD_AURASTATE before aura load or health/etc set.
    This resolve problem when some aura state not remove aftre reload and original aura source lost.
    
    (backported from master commit 3564cb5)
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    (based on commit 1b12998)
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -2372,6 +2372,9 @@ void Player::InitStatsForLevel(bool reapplyMods)
 
     SetUInt32Value(PLAYER_NEXT_LEVEL_XP, sObjectMgr.GetXPForLevel(getLevel()));
 
+    // reset before any aura state sources (health set/aura apply)
+    SetUInt32Value(UNIT_FIELD_AURASTATE, 0);
+
     UpdateSkillsForLevel ();
 
     // set default cast time multiplier
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "0306"
+ #define REVISION_NR "0307"
 #endif // __REVISION_NR_H__
