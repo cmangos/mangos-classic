2cfc5d8ef5ca1ae1e8388a5a8ad389d84ca8efbe
VladimirMangos <vladimir@getmangos.com>
2010-06-07 23:28:39 +0400

    [z0585] Implement CONDITION_QUESTAVAILABLE.
    Also do some constatification of quest status check functions
    and fix one unsafe use std::map operator[].
    Never never use std::map operator [] for _read_ access.
    Good chance corrupt related structure state ;)
    Not in found case but in general.
    
    (based on master commit 6eb863f)
    
    (based on commit 2451745)

--- src/game/ObjectMgr.cpp
+++ src/game/ObjectMgr.cpp
@@ -6881,6 +6881,13 @@ bool PlayerCondition::Meets(Player const * player) const
                 return data->CheckConditionCriteriaMeet(player, value1, value2);
             return false;
         }
+        case CONDITION_QUESTAVAILABLE:
+        {
+            if (Quest const* quest = sObjectMgr.GetQuestTemplate(value1))
+                return player->CanTakeQuest(quest, false);
+            else
+                false;
+        }
         default:
             return false;
     }
@@ -6989,6 +6996,7 @@ bool PlayerCondition::IsValid(ConditionType condition, uint32 value1, uint32 val
         }
         case CONDITION_QUESTREWARDED:
         case CONDITION_QUESTTAKEN:
+        case CONDITION_QUESTAVAILABLE:
         {
             Quest const *Quest = sObjectMgr.GetQuestTemplate(value1);
             if (!Quest)
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "0584"
+ #define REVISION_NR "0585"
 #endif // __REVISION_NR_H__
