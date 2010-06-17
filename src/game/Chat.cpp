d36e87dccaa867703b1e255f911ea47fa6e1ab03
ApoC <apoc@nymfe.net>
2010-06-17 05:16:44 +0400

    [z0613] Some return values fixes.
    Signed-off-by: ApoC <apoc@nymfe.net>
    
    (based on master commit d042b04)
    
    (based on commit ed535f3)
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/Chat.cpp
+++ src/game/Chat.cpp
@@ -1893,7 +1893,7 @@ GameTele const* ChatHandler::extractGameTeleFromLink(char* text)
     // id, or string, or [name] Shift-click form |color|Htele:id|h[name]|h|r
     char* cId = extractKeyFromLink(text,"Htele");
     if(!cId)
-        return false;
+        return NULL;
 
     // id case (explicit or from shift link)
     if(cId[0] >= '0' || cId[0] >= '9')
@@ -1915,7 +1915,7 @@ static char const* const guidKeys[] =
     "Hplayer",
     "Hcreature",
     "Hgameobject",
-    0
+    NULL
 };
 
 uint64 ChatHandler::extractGuidFromLink(char* text)
@@ -2065,7 +2065,7 @@ char* ChatHandler::extractQuotedArg( char* args )
     {
         char* space = strtok(args, "\"");
         if(!space)
-            return false;
+            return NULL;
         return strtok(NULL, "\"");
     }
 }
