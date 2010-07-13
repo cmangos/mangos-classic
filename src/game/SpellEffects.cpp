f0388016452bd25dbe9a6acf70738fcd6381bf89
DiSlord <dislord@nomail.com>
2010-07-13 04:09:41 +0400

    [z0698] Small cleanup approprite only for backport from commit.
    Signed-off-by: DiSlord <dislord@nomail.com>
    
    (based on master commit 2a8d947)
    
    (based on commit 7e61fc9)
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -911,11 +911,6 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
         {
             switch(m_spellInfo->Id)
             {
-                case 31231:                                 // Cheat Death
-                {
-                    m_caster->CastSpell(m_caster,45182,true);
-                    return;
-                }
                 case 5938:                                  // Shiv
                 {
                     if (m_caster->GetTypeId() != TYPEID_PLAYER)
@@ -949,6 +944,12 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
                     }
 
                     m_caster->CastSpell(unitTarget, 5940, true);
+                    return;
+                }
+
+                case 31231:                                 // Cheat Death
+                {
+                    m_caster->CastSpell(m_caster,45182,true);
                     return;
                 }
             }
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "0697"
+ #define REVISION_NR "0698"
 #endif // __REVISION_NR_H__
