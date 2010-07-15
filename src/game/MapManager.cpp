d8046d173d07d9f318370436db39c8801e9c2af0
VladimirMangos <vladimir@getmangos.com>
2010-07-15 06:13:59 +0400

    [z0714] Use for Transport::Update arg uint32 type as for other world objects
    (based on master commit 1f87b28)
    
    (based on commit ab3e380)

--- src/game/MapManager.cpp
+++ src/game/MapManager.cpp
@@ -246,7 +246,7 @@ MapManager::Update(uint32 diff)
     }
 
     for (TransportSet::iterator iter = m_Transports.begin(); iter != m_Transports.end(); ++iter)
-        (*iter)->Update(i_timer.GetCurrent());
+        (*iter)->Update((uint32)i_timer.GetCurrent());
 
     i_timer.SetCurrent(0);
 }
