e4b8da9f4d90bccb5a72d416f3945416b19b5016
VladimirMangos <vladimir@getmangos.com>
2010-09-01 04:40:39 +0400

    [z0831] Load most spell chains data from SkillLineAbility.dbc
    * Output error for redundent data in table
    * Drop detected data from `spell_chain`
    
    (based on master commit 3cd7ef7)
    (based on master commit 1fc52ea)
    (based on master commit 5aac34b)
    
    (based on commit 2686a20)
    
    SQL part redone for MaNGOSZero

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_z0801_s0114_01_characters_character_ticket"
- #define REVISION_DB_MANGOS "required_z0830_s0148_01_mangos_spell_chain"
+ #define REVISION_DB_MANGOS "required_z0831_s0150_01_mangos_spell_chain"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__
