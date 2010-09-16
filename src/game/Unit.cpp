3dd138a42cb57de91ac4f2d733a0c5553327cce7
VladimirMangos <vladimir@getmangos.com>
2010-09-17 02:37:03 +0400

    [z0847] Fixed unexpected area aura stacking from same caster at spellmods reapply.
    In fact it general problem possible with area auras if it re-casted
    non-standard way from code. But known only test case for mangos-0.12 branch.
    
    Thanks to ciphercom for reportand original research of problem source.
    
    Manually reapply master commit d790cad. Need re-backported after aura holders backport.
    
    (based on commit ea7aaba)

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -3200,7 +3200,7 @@ bool Unit::AddAura(Aura *Aur)
     if (i != m_Auras.end())
     {
         // passive and persistent auras can stack with themselves any number of times
-        if (!Aur->IsPassive() && !Aur->IsPersistent())
+        if (!Aur->IsPassive() && !Aur->IsPersistent() || Aur->IsAreaAura())
         {
             for(AuraMap::iterator i2 = m_Auras.lower_bound(spair); i2 != m_Auras.upper_bound(spair); ++i2)
             {
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "0846"
+ #define REVISION_NR "0847"
 #endif // __REVISION_NR_H__
