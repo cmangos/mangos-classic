d40770a82630595e4fdf7fb109b8b1383232f086
VladimirMangos <vladimir@getmangos.com>
2010-10-12 06:02:55 +0400

    [z0939] Redirect movement packet in proper way to controlled target.
    * In creature case ignore fall/etc damage
    * In player case apply fall damage/etc to controlled player, apply other movement effect to it also.
    
    (based on master commit 88335c3)
    
    Re-apply one lost line.
    
    (based on commit 10c68dc)

--- src/game/MovementHandler.cpp
+++ src/game/MovementHandler.cpp
@@ -266,7 +266,7 @@ void WorldSession::HandleMovementOpcodes( WorldPacket & recv_data )
                     // unmount before boarding
                     plMover->RemoveSpellsCausingAura(SPELL_AURA_MOUNTED);
 
-                    GetPlayer()->m_transport = (*iter);
+                    plMover->m_transport = (*iter);
                     (*iter)->AddPassenger(plMover);
                     break;
                 }
