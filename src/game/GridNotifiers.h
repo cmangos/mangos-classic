50571002453d91881d7c2af1af4a1d246259381a
SilverIce <slifeleaf@gmail.com>
2010-11-02 12:11:16 +0300

    [z0985] Simplified trade distance checks, GROUP_UPDATE_FLAG_POSITION flag handling moved out of Map
    note: need avoid Map::PlayerRelocation calls now and use Player::SetPosition instead
    
    (based on master commit 28d9aba)
    
    (based on commit f0816db)

--- src/game/GridNotifiers.h
+++ src/game/GridNotifiers.h
@@ -148,7 +148,6 @@ namespace MaNGOS
         Player &i_player;
         PlayerRelocationNotifier(Player &pl) : i_player(pl) {}
         template<class T> void Visit(GridRefManager<T> &) {}
-        void Visit(PlayerMapType &);
         void Visit(CreatureMapType &);
     };
 
@@ -1083,7 +1082,6 @@ namespace MaNGOS
 
     #ifndef WIN32
     template<> void PlayerRelocationNotifier::Visit<Creature>(CreatureMapType &);
-    template<> void PlayerRelocationNotifier::Visit<Player>(PlayerMapType &);
     template<> void CreatureRelocationNotifier::Visit<Player>(PlayerMapType &);
     template<> void CreatureRelocationNotifier::Visit<Creature>(CreatureMapType &);
     template<> inline void DynamicObjectUpdater::Visit<Creature>(CreatureMapType &);
