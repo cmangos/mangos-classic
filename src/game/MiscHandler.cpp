e4e2a275ad7fd3d9e90120a551fc48f325009a6b
tomrus88 <tomrus88@gmail.com>
2010-12-10 07:17:49 +0300

    [z1080] Small code style backport from not backportable commit.
    (based on master commit 0a8c2a1)
    
    (based on commit b89e9e6)

--- src/game/MiscHandler.cpp
+++ src/game/MiscHandler.cpp
@@ -758,7 +758,7 @@ void WorldSession::HandleAreaTriggerOpcode(WorldPacket & recv_data)
         }
     }
 
-    GetPlayer()->TeleportTo(at->target_mapId,at->target_X,at->target_Y,at->target_Z,at->target_Orientation,TELE_TO_NOT_LEAVE_TRANSPORT);
+    GetPlayer()->TeleportTo(at->target_mapId, at->target_X, at->target_Y, at->target_Z, at->target_Orientation, TELE_TO_NOT_LEAVE_TRANSPORT);
 }
 
 void WorldSession::HandleUpdateAccountData(WorldPacket & recv_data)
