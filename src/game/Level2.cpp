d57bcda93663115413cc3e78ce4de0b06bbea0f6
VladimirMangos <vladimir@getmangos.com>
2010-12-18 20:34:01 +0300

    [z1113] Pet with same level as owner must have xp = 0
    Thanks to Click for original patch and problem description.
    
    (based on master commit e1351dd)
    
    (based on commit 5db797d)

--- src/game/Level2.cpp
+++ src/game/Level2.cpp
@@ -1559,14 +1559,7 @@ bool ChatHandler::HandleNpcChangeLevelCommand(char* args)
     }
 
     if (pCreature->IsPet())
-    {
-        if (((Pet*)pCreature)->getPetType()==HUNTER_PET)
-        {
-            pCreature->SetUInt32Value(UNIT_FIELD_PETNEXTLEVELEXP, sObjectMgr.GetXPForPetLevel(lvl));
-            pCreature->SetUInt32Value(UNIT_FIELD_PETEXPERIENCE, 0);
-        }
         ((Pet*)pCreature)->GivePetLevel(lvl);
-    }
     else
     {
         pCreature->SetMaxHealth(100 + 30*lvl);
