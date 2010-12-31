2e0d61bda4e96f7585920b012acfe015633dd215
insider42 <insider42@github.com>
2010-12-31 18:06:40 +0300

    [z1180] Allow SPELL_AURA_MOD_PACIFY auras to affect creatures
    Signed-off-by: zergtmn <zerg@myisp.com>
    
    (based on master commit 54b1513)
    
    (based on commit ca24a29)

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -4593,13 +4593,10 @@ void Aura::HandleAuraUntrackable(bool apply, bool /*Real*/)
 
 void Aura::HandleAuraModPacify(bool apply, bool /*Real*/)
 {
-    if(m_target->GetTypeId() != TYPEID_PLAYER)
-        return;
-
-    if(apply)
-        m_target->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PACIFIED);
+    if (apply)
+        GetTarget()->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PACIFIED);
     else
-        m_target->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PACIFIED);
+        GetTarget()->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PACIFIED);
 }
 
 void Aura::HandleAuraModPacifyAndSilence(bool apply, bool Real)
