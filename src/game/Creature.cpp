592334155cb757e75ed3eb980c17a6fdb22dcde6
Schmoozerd <schmoozerd@getmangos.com>
2011-01-27 04:32:24 +0300

    [z1268] Implement CREATURE_FLAG_EXTRA_AGGRO_ZONE.
    This let control zone wide aggro in DB data instead
    need special script code.
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    (based on master commit daf34e6)
    (based on master commit 7fbb152)
    
    (based on commit 11ba270)
    (based on commit 693310a)

--- src/game/Creature.cpp
+++ src/game/Creature.cpp
@@ -1908,7 +1908,7 @@ void Creature::SetInCombatWithZone()
             if (pPlayer->isGameMaster())
                 continue;
 
-            if (pPlayer->isAlive())
+            if (pPlayer->isAlive() && !IsFriendlyTo(pPlayer))
             {
                 pPlayer->SetInCombatWith(this);
                 AddThreat(pPlayer);
--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7111,11 +7111,17 @@ void Unit::SetInCombatState(bool PvP, Unit* enemy)
         // should probably be removed for the attacked (+ it's party/group) only, not global
         RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_OOC_NOT_ATTACKABLE);
 
-        if (((Creature*)this)->AI())
-            ((Creature*)this)->AI()->EnterCombat(enemy);
+        Creature* pCreature = (Creature*)this;
+
+        if (pCreature->AI())
+            pCreature->AI()->EnterCombat(enemy);
+
+        // Some bosses are set into combat with zone
+        if (GetMap()->IsDungeon() && (pCreature->GetCreatureInfo()->flags_extra & CREATURE_FLAG_EXTRA_AGGRO_ZONE) && enemy && enemy->IsControlledByPlayer())
+            pCreature->SetInCombatWithZone();
 
         if (InstanceData* mapInstance = GetInstanceData())
-            mapInstance->OnCreatureEnterCombat((Creature*)this);
+            mapInstance->OnCreatureEnterCombat(pCreature);
     }
 }
 
