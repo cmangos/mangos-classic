40e21e0331d09eadba3bc35ef7fccd413548256e
VladimirMangos <vladimir@getmangos.com>
2011-02-20 04:48:09 +0300

    [z1385] Implemented pool system related commands.
    .lookup pool $pooldescpart - list of pools (anywhere) with substring in description
    .pool list   - list of pools with spawn in current map (only work in instances)
                   Non-instanceable maps share pool system state os useless attempt get all pols at all continents.
    .pool spawns - list current creatures/objects listed in pools and spawned (added to grid data, not meaning show in world).
    .pool spawns #pool_id - list current creatures/objects listed in #pool_id and spawned (added to grid data, not meaning show in world).
    .pool #pool_id - pool information and full list creatures/gameobjects included in pool
    
    (based on master commit a8c6b4d)
    (based on master commit 956dfce)
    (based on master commit 6c3f2c0)
    (based on master commit 086d9e4)
    (based on master commit f9d96ec)
    (based on master commit 2edfdff)
    
    (based on commit ffd09f1)

--- src/game/Chat.h
+++ src/game/Chat.h
@@ -270,6 +270,7 @@ class ChatHandler
         bool HandleLookupPlayerIpCommand(char* args);
         bool HandleLookupPlayerAccountCommand(char* args);
         bool HandleLookupPlayerEmailCommand(char* args);
+        bool HandleLookupPoolCommand(char* args);
         bool HandleLookupQuestCommand(char* args);
         bool HandleLookupSkillCommand(char* args);
         bool HandleLookupSpellCommand(char* args);
@@ -328,6 +329,10 @@ class ChatHandler
 
         bool HandlePDumpLoadCommand(char* args);
         bool HandlePDumpWriteCommand(char* args);
+
+        bool HandlePoolListCommand(char* args);
+        bool HandlePoolSpawnsCommand(char* args);
+        bool HandlePoolInfoCommand(char* args);
 
         bool HandleQuestAddCommand(char* args);
         bool HandleQuestRemoveCommand(char* args);
@@ -584,6 +589,7 @@ class ChatHandler
         void ShowQuestListHelper(uint32 questId, int32 loc_idx, Player* target = NULL);
         bool ShowPlayerListHelper(QueryResult* result, uint32* limit = NULL, bool title = true, bool error = true);
         void ShowSpellListHelper(Player* target, SpellEntry const* spellInfo, LocaleConstant loc);
+        void ShowPoolListHelper(uint16 pool_id);
         void ShowTicket(GMTicket const* ticket);
         void ShowTriggerListHelper(AreaTriggerEntry const * atEntry);
         void ShowTriggerTargetListHelper(uint32 id, AreaTrigger const* at, bool subpart = false);
