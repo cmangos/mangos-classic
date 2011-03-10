89ba5ff69dac7ea63fddd4accc01bd434de0899d
nos4r2zod <nos4r2zod@getmangos.com>
2011-03-10 16:41:32 +0300

    [z1423] Move DotFactor+GetSpellCastTimeForBonus call to CalculateDefaultCoefficient
    * This coeff used if not spell_bonus_data provided for spell
    * Continue prev. commit line expected that creature casts not affected by DotFactor
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    (based on master commit b136054)
    
    (based on commit 1c3d90d)

--- src/game/SpellMgr.h
+++ src/game/SpellMgr.h
@@ -90,6 +90,7 @@ SpellSpecific GetSpellSpecific(uint32 spellId);
 inline float GetSpellRadius(SpellRadiusEntry const *radius) { return (radius ? radius->Radius : 0); }
 uint32 GetSpellCastTime(SpellEntry const* spellInfo, Spell const* spell = NULL);
 uint32 GetSpellCastTimeForBonus( SpellEntry const *spellProto, DamageEffectType damagetype );
+float CalculateDefaultCoefficient(SpellEntry const *spellProto, DamageEffectType const damagetype);
 inline float GetSpellMinRange(SpellRangeEntry const *range) { return (range ? range->minRange : 0); }
 inline float GetSpellMaxRange(SpellRangeEntry const *range) { return (range ? range->maxRange : 0); }
 inline uint32 GetSpellRecoveryTime(SpellEntry const *spellInfo) { return spellInfo->RecoveryTime > spellInfo->CategoryRecoveryTime ? spellInfo->RecoveryTime : spellInfo->CategoryRecoveryTime; }
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "1422"
+ #define REVISION_NR "1423"
 #endif // __REVISION_NR_H__
