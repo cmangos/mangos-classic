084e31bec9bd2c873bcfa6c3881bec2e915651d4
NoFantasy <nofantasy@nf.no>
2011-04-17 08:46:23 +0400

    [z1474] Move code for initialize player model data and scale to same function, InitDisplayIds
    Signed-off-by: NoFantasy <nofantasy@nf.no>
    
    (based on commit 1d39bbf)
    
    Some redundent lines not removed from prev. commit backport
    
    (based on commit 8c91e3d)

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -13479,9 +13479,6 @@ bool Player::LoadFromDB( uint32 guid, SqlQueryHolder *holder )
 
     _LoadIntoDataField(fields[51].GetString(), PLAYER_EXPLORED_ZONES_1, PLAYER_EXPLORED_ZONES_SIZE);
 
-    SetFloatValue(UNIT_FIELD_BOUNDINGRADIUS, DEFAULT_WORLD_OBJECT_SIZE);
-    SetFloatValue(UNIT_FIELD_COMBATREACH, 1.5f);
-
     InitDisplayIds();                                       // model, scale and model data
 
     uint32 money = fields[8].GetUInt32();
