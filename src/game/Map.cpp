9576389d764bb6f3dfe6a6e0a17f1d80a97f81a4
Ambal <pogrebniak@gala.net>
2011-04-26 03:17:02 +0400

    [z1563] Small code cleanups not applied at firs backport time (based on commit 6ea13fc)
    (based on commit f003cc5)
    
    Also fix non-forced speed change packet structure used at spawn, death and attack stop.
    So this must fix some cases strange speed for mobs.

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -463,7 +463,6 @@ void Map::Update(const uint32 &t_diff)
         Player* plr = m_mapRefIter->getSource();
         if(plr && plr->IsInWorld())
         {
-            //plr->Update(t_diff);
             WorldSession * pSession = plr->GetSession();
             MapSessionFilter updater(pSession);
 
--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6772,8 +6772,7 @@ void Unit::SetSpeedRate(UnitMoveType mtype, float rate, bool forced)
         }
 
         data << GetPackGUID();
-        data << uint32(0);                                  //movement flags
-        data << uint8(0);                                   //unk
+        data << uint32(MOVEFLAG_NONE);                      // movement flags
         data << uint32(WorldTimer::getMSTime());
         data << float(GetPositionX());
         data << float(GetPositionY());
