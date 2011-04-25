bc2c5e077a21665ea89f21802bb56c853a753e33
michalpolko <michalpolko@getmangos.com>
2011-04-25 05:51:45 +0400

    [z1553] Implement charges counting for magnet target auras.
    Also implement skip spell effects redirecting if magnet target can't be
    targeted by this spell effect base at spell effect target data requirements.
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    (based on master commit f7fb2fd)
    
    (based on commit 329079d)

--- src/game/Unit.h
+++ src/game/Unit.h
@@ -1578,7 +1578,7 @@ class MANGOS_DLL_SPEC Unit : public WorldObject
         void ModifyAuraState(AuraState flag, bool apply);
         bool HasAuraState(AuraState flag) const { return HasFlag(UNIT_FIELD_AURASTATE, 1<<(flag-1)); }
         void UnsummonAllTotems();
-        Unit* SelectMagnetTarget(Unit *victim, SpellEntry const *spellInfo = NULL);
+        Unit* SelectMagnetTarget(Unit *victim, Spell* spell = NULL, SpellEffectIndex eff = EFFECT_INDEX_0);
 
         int32 SpellBonusWithCoeffs(SpellEntry const *spellProto, int32 total, int32 benefit, int32 ap_benefit, DamageEffectType damagetype, bool donePart);
         int32 SpellBaseDamageBonusDone(SpellSchoolMask schoolMask);
