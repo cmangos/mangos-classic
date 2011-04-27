17b246735345092ed3c8f54504181d051e1b1a1c
NoFantasy <nofantasy@nf.no>
2011-04-27 14:34:37 +0400

    [z1601] Add dummy aura effect of spell 6606
    Signed-off-by: NoFantasy <nofantasy@nf.no>
    
    (based on master commit 48111f9)
    
    (based on commit c7aac5e)

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -1325,6 +1325,21 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
         {
             switch(GetId())
             {
+                case 6606:                                  // Self Visual - Sleep Until Cancelled (DND)
+                {
+                    if (apply)
+                    {
+                        target->SetStandState(UNIT_STAND_STATE_SLEEP);
+                        target->addUnitState(UNIT_STAT_ROOT);
+                    }
+                    else
+                    {
+                        target->clearUnitState(UNIT_STAT_ROOT);
+                        target->SetStandState(UNIT_STAND_STATE_STAND);
+                    }
+
+                    return;
+                }
                 case 24658:                                 // Unstable Power
                 {
                     if (apply)
