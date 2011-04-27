93a759b943d74f473b98c97c2648270e05515798
Ambal <pogrebniak@gala.net>
2011-04-27 04:29:58 +0400

    [z1596] NearUsedPosDo searcher should add only units with correct coordinates. Possibly can help with freezes when 'DetectPosCollision = 1'.
    Signed-off-by: Ambal <pogrebniak@gala.net>
    
    (based on master commit a79ca96)
    
    (based on commit e1fed58)

--- src/game/Object.cpp
+++ src/game/Object.cpp
@@ -1444,9 +1444,9 @@ namespace MaNGOS
                 {
                     x = c->GetPositionX();
                     y = c->GetPositionY();
-                }
 
-                add(c,x,y);
+                    add(c,x,y);
+                }
             }
 
             template<class T>
