6b5124b4f7a1c2a92bd59b3bbf159097b05d8bf0
VladimirMangos <vladimir@getmangos.com>
2011-05-15 05:40:51 +0400

    [z1660] Provide access to some script data (names, count) for script DLL
    (based on master commit 2756bc5)
    
    (based on commit a83c75b)

--- src/game/ScriptMgr.cpp
+++ src/game/ScriptMgr.cpp
@@ -1187,3 +1187,14 @@ uint32 GetScriptId(const char *name)
 {
     return sScriptMgr.GetScriptId(name);
 }
+
+char const* GetScriptName(uint32 id)
+{
+    return sScriptMgr.GetScriptName(id);
+}
+
+uint32 GetScriptIdsCount()
+{
+    return sScriptMgr.GetScriptIdsCount();
+}
+
