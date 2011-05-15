7f73be964082e24e3c705c7a53b59ec9c386d3bd
Den <Den@ru-mangos.ru>
2011-05-15 20:53:04 +0400

    [z1669] Cleanup codestyle in WorldSocketMgr.cpp & WorldSocketMgr.h
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    (based on master commit 1d8de3b)
    
    (based on commit bdcfc58)

--- src/game/WorldSocketMgr.h
+++ src/game/WorldSocketMgr.h
@@ -39,43 +39,43 @@ class ACE_Event_Handler;
 /// Manages all sockets connected to peers and network threads
 class WorldSocketMgr
 {
-public:
-  friend class WorldSocket;
-  friend class ACE_Singleton<WorldSocketMgr,ACE_Thread_Mutex>;
+    public:
+        friend class WorldSocket;
+        friend class ACE_Singleton<WorldSocketMgr, ACE_Thread_Mutex>;
 
-  /// Start network, listen at address:port .
-  int StartNetwork (ACE_UINT16 port, std::string& address);
+        /// Start network, listen at address:port .
+        int StartNetwork(ACE_UINT16 port, std::string& address);
 
-  /// Stops all network threads, It will wait for all running threads .
-  void StopNetwork ();
+        /// Stops all network threads, It will wait for all running threads .
+        void StopNetwork();
 
-  /// Wait untill all network threads have "joined" .
-  void Wait ();
+        /// Wait untill all network threads have "joined" .
+        void Wait();
 
-  /// Make this class singleton .
-  static WorldSocketMgr* Instance ();
+        /// Make this class singleton .
+        static WorldSocketMgr* Instance();
 
-private:
-  int OnSocketOpen(WorldSocket* sock);
+    private:
+        int OnSocketOpen(WorldSocket* sock);
+        int StartReactiveIO(ACE_UINT16 port, const char* address);
 
-  int StartReactiveIO(ACE_UINT16 port, const char* address);
+        WorldSocketMgr();
+        virtual ~WorldSocketMgr();
 
-private:
-  WorldSocketMgr ();
-  virtual ~WorldSocketMgr ();
+        ReactorRunnable* m_NetThreads;
+        size_t m_NetThreadsCount;
 
-  ReactorRunnable* m_NetThreads;
-  size_t m_NetThreadsCount;
+        int m_SockOutKBuff;
+        int m_SockOutUBuff;
+        bool m_UseNoDelay;
 
-  int m_SockOutKBuff;
-  int m_SockOutUBuff;
-  bool m_UseNoDelay;
+        std::string m_addr;
+        ACE_UINT16 m_port;
 
-  ACE_Event_Handler* m_Acceptor;
+        ACE_Event_Handler* m_Acceptor;
 };
 
-#define sWorldSocketMgr WorldSocketMgr::Instance ()
-
+#define sWorldSocketMgr WorldSocketMgr::Instance()
 
 #endif
 /// @}
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "1668"
+ #define REVISION_NR "1669"
 #endif // __REVISION_NR_H__
