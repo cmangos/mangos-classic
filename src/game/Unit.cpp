1f56b4e91fb3ebcc0e7d425352ef6aa36012ddcd
mns <mns@getmangos.com>
2011-06-12 07:09:01 +0400

    [z1750] Really use DB value for creature walking speed after speed update.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    (based on master commit f33c298)
    
    (based on commit b8bb623)

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6644,7 +6644,7 @@ void Unit::UpdateSpeed(UnitMoveType mtype, bool forced, float ratio)
     switch(mtype)
     {
         case MOVE_WALK:
-            return;
+            break;
         case MOVE_RUN:
         {
             if (IsMounted()) // Use on mount auras
