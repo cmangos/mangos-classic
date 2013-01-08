2a791deac291604ebfc56debc0fdfd9696aea3af
Xfurry <xfurry@scriptdev2.com>
2013-01-08 23:35:16 +0200

    [z1817] Fix error in SQL file. Thanks cala for reporting

--- sql/updates/z1794_s1350_11716_05_characters_guild.sql
+++ sql/updates/z1794_s1350_11716_05_characters_guild.sql
@@ -1,5 +1,4 @@
 ALTER TABLE character_db_version CHANGE COLUMN required_z1794_s1350_11716_04_characters_auction required_z1794_s1350_11716_05_characters_auction bit;
 
 ALTER TABLE `guild`
-  CHANGE COLUMN `createdate` `createdate` bigint(20) unsigned NOT NULL default '0',
-  CHANGE COLUMN `BankMoney` `BankMoney` bigint(20) unsigned NOT NULL default '0';
+  CHANGE COLUMN `createdate` `createdate` bigint(20) unsigned NOT NULL default '0';
