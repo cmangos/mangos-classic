e033c5512d97754642168c0152df7a8c8a573e6f
stfx <stfx@hotmail.de>
2013-01-09 01:35:44 +0200

    [z1841] Use the same variable to store the message for AFK and DND
    Also make code in this area easier to read
    
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>
    
    (based on commit 6c01a2a)
    
    (cleaned version of 14245de)
    
    (based on commit [s1400] - 11851b7)

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -1512,40 +1512,36 @@ bool Player::BuildEnumData(QueryResult* result, WorldPacket* p_data)
     return true;
 }
 
-bool Player::ToggleAFK()
+void Player::ToggleAFK()
 {
     ToggleFlag(PLAYER_FLAGS, PLAYER_FLAGS_AFK);
 
-    bool state = HasFlag(PLAYER_FLAGS, PLAYER_FLAGS_AFK);
-
     // afk player not allowed in battleground
-    if (state && InBattleGround())
+    if (isAFK() && InBattleGround())
         LeaveBattleground();
-
-    return state;
 }
 
-bool Player::ToggleDND()
+void Player::ToggleDND()
 {
     ToggleFlag(PLAYER_FLAGS, PLAYER_FLAGS_DND);
-
-    return HasFlag(PLAYER_FLAGS, PLAYER_FLAGS_DND);
 }
 
 uint8 Player::chatTag() const
 {
     // it's bitmask
-    // 0x3 - gm
-    // 0x2 - dnd
     // 0x1 - afk
+    // 0x2 - dnd
+    // 0x3 - gm
+    
     if (isGMChat())
         return 3;
-    else if (isDND())
-        return 2;
+        
     if (isAFK())
         return 1;
-    else
-        return 0;
+    if (isDND())
+        return 2;
+
+    return 0;
 }
 
 bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientation, uint32 options)
@@ -16047,9 +16043,9 @@ void Player::Whisper(const std::string& text, uint32 language, ObjectGuid receiv
 
     // announce afk or dnd message
     if (rPlayer->isAFK())
-        ChatHandler(this).PSendSysMessage(LANG_PLAYER_AFK, rPlayer->GetName(), rPlayer->afkMsg.c_str());
+        ChatHandler(this).PSendSysMessage(LANG_PLAYER_AFK, rPlayer->GetName(), rPlayer->autoReplyMsg.c_str());
     else if (rPlayer->isDND())
-        ChatHandler(this).PSendSysMessage(LANG_PLAYER_DND, rPlayer->GetName(), rPlayer->dndMsg.c_str());
+        ChatHandler(this).PSendSysMessage(LANG_PLAYER_DND, rPlayer->GetName(), rPlayer->autoReplyMsg.c_str());
 }
 
 void Player::PetSpellInitialize()
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "1840"
+ #define REVISION_NR "1841"
 #endif // __REVISION_NR_H__
