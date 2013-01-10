6922f267c41f767294dd9e0ad8b4479f575f1af0
Schmoozerd <schmoozerd@scriptdev2.com>
2013-01-10 07:50:16 +0200

    [z1912] Remove old binaries for extraction
    (based on commit [s1494] - ae44617)

--- contrib/vmap_extract_assembler_bin/makevmaps_SIMPLE.bat
+++
@@ -1,5 +0,0 @@
-vmapExtractor3.exe
-md vmaps
-vmap_assembler.exe Buildings vmaps
-
-pause
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "1911"
+ #define REVISION_NR "1912"
 #endif // __REVISION_NR_H__
