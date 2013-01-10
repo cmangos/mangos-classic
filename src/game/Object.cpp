153a46756273836e2bf429e35dc005e72e5c02f2
faramir118 <faramir118@gmail.com>
2013-01-10 07:58:32 +0200

    [z1915] Improve GetNearPoint
    (cleaned version of c06e24f)
    
    (based on commit [s1497] - 59770e7)

--- src/game/Object.cpp
+++ src/game/Object.cpp
@@ -1537,7 +1537,7 @@ void WorldObject::GetNearPoint2D(float& x, float& y, float distance2d, float abs
 void WorldObject::GetNearPoint(WorldObject const* searcher, float& x, float& y, float& z, float searcher_bounding_radius, float distance2d, float absAngle) const
 {
     GetNearPoint2D(x, y, distance2d + searcher_bounding_radius, absAngle);
-    z = GetPositionZ();
+    const float init_z = z = GetPositionZ();
 
     // if detection disabled, return first point
     if (!sWorld.getConfig(CONFIG_BOOL_DETECT_POS_COLLISION))
@@ -1554,8 +1554,10 @@ void WorldObject::GetNearPoint(WorldObject const* searcher, float& x, float& y, 
     float first_y = y;
     bool first_los_conflict = false;                        // first point LOS problems
 
+    const float dist = distance2d + searcher_bounding_radius + GetObjectBoundingRadius();
+
     // prepare selector for work
-    ObjectPosSelector selector(GetPositionX(), GetPositionY(), distance2d + searcher_bounding_radius + GetObjectBoundingRadius(), searcher_bounding_radius);
+    ObjectPosSelector selector(GetPositionX(), GetPositionY(), dist, searcher_bounding_radius);
 
     // adding used positions around object
     {
@@ -1573,7 +1575,7 @@ void WorldObject::GetNearPoint(WorldObject const* searcher, float& x, float& y, 
         else
             UpdateGroundPositionZ(x, y, z);
 
-        if (IsWithinLOS(x, y, z))
+        if (fabs(init_z - z) < dist && IsWithinLOS(x, y, z))
             return;
 
         first_los_conflict = true;                          // first point have LOS problems
@@ -1595,7 +1597,7 @@ void WorldObject::GetNearPoint(WorldObject const* searcher, float& x, float& y, 
         else
             UpdateGroundPositionZ(x, y, z);
 
-        if (IsWithinLOS(x, y, z))
+        if (fabs(init_z - z) < dist && IsWithinLOS(x, y, z))
             return;
     }
 
@@ -1627,7 +1629,7 @@ void WorldObject::GetNearPoint(WorldObject const* searcher, float& x, float& y, 
         else
             UpdateGroundPositionZ(x, y, z);
 
-        if (IsWithinLOS(x, y, z))
+        if (fabs(init_z - z) < dist && IsWithinLOS(x, y, z))
             return;
     }
 
