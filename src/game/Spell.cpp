cda72439b9347d48c4762b629821eab53a750528
Schmoozerd <schmoozerd@scriptdev2.com>
2013-01-14 01:24:02 +0200

    [z1955] Fix logic bug in [z1950]
    Signed-off-by: Nighoo <nighoo@gmail.com>
    
    (based on commit [s1545] - 581ae90)

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -405,6 +405,7 @@ void Spell::FillTargetMap()
                 // Add further conditions here if required
             {
                 effToIndex[i] = j;                          // effect i has same targeting list as effect j
+                break;
             }
         }
 
