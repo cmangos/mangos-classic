69efb726db35b35a33fb2e13309163100619174b
Schmoozerd <schmoozerd@scriptdev2.com>
2013-01-14 00:21:54 +0200

    [z1940] Rewrite and cleanup DB Script Engine
    * Moved actual script processing code to ScriptMgr
    * Unifed and improved log output. Now table-name is passed to each script execution
    * Added the "buddy concept" to all commands (except the outdated direct field change commands):
    Now all commands support to search a creature (or go for some commands) in a radius, with which to do some stuff.
    See doc/script_commands.txt for more details and information
    
    Attention DB Devs:
    Current DB-Scripts are converted automatically as far as possible, but some old target selecting mechanics were not reasonable to do automated.
    For such cases the command is marked with data_flags & 0x10 (==16), which will throw DB-Errors to track them down faster
    
    Thanks to NeatElves and especially Grz3s for testing!
    
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>
    Signed-off-by: Nighoo <nighoo@gmail.com>
    
    (cleaned version of 6701840)
    
    (based on commit [s1528] - a4636fe)

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "1939"
+ #define REVISION_NR "1940"
 #endif // __REVISION_NR_H__
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_z1794_s1350_11716_09_characters_auction"
- #define REVISION_DB_MANGOS "required_z1922_s1504_11926_01_mangos_creature_template"
+ #define REVISION_DB_MANGOS "required_z1940_s1528_11940_07_mangos_spell_scripts"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__
