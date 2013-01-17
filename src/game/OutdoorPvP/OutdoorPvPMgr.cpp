25f74a66d69de72995611ef1248c9c98a134589b
stfx <stfx@hotmail.de>
2013-01-18 00:29:30 +0200

    [z2056] Add OutdoorPvPMgr class
    (based on commit 4b2bdc3)
    
    Signed-off-by: Nighoo <nighoo@overninethousand.de>
    
    (cleaned version of fbb5482)
    
    (based on commit [s1669] - 324b107)

--- src/game/OutdoorPvP/OutdoorPvPMgr.cpp
+++ src/game/OutdoorPvP/OutdoorPvPMgr.cpp
@@ -15,3 +15,122 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
+
+#include "OutdoorPvPMgr.h"
+#include "Policies/SingletonImp.h"
+#include "OutdoorPvP.h"
+#include "../GameObject.h"
+#include "../World.h"
+#include "Log.h"
+#include "OutdoorPvPEP.h"
+#include "OutdoorPvPSI.h"
+
+INSTANTIATE_SINGLETON_1(OutdoorPvPMgr);
+
+OutdoorPvPMgr::OutdoorPvPMgr()
+{
+    m_updateTimer.SetInterval(TIMER_OPVP_MGR_UPDATE);
+    memset(&m_scripts, 0, sizeof(m_scripts));
+}
+
+OutdoorPvPMgr::~OutdoorPvPMgr()
+{
+    for (uint8 i = 0; i < MAX_OPVP_ID; ++i)
+        delete m_scripts[i];
+}
+
+/**
+   Function which loads all outdoor pvp scripts
+ */
+void OutdoorPvPMgr::InitOutdoorPvP()
+{
+
+    sLog.outString();
+    sLog.outString(">> Loaded %u Outdoor PvP zones", MAX_OPVP_ID);
+}
+
+OutdoorPvP* OutdoorPvPMgr::GetScript(uint32 zoneId)
+{
+    switch (zoneId)
+    {
+        case ZONE_ID_SILITHUS:
+            return m_scripts[OPVP_ID_SI];
+        case ZONE_ID_EASTERN_PLAGUELANDS:
+            return m_scripts[OPVP_ID_EP];
+        default:
+            return NULL;
+    }
+}
+
+OutdoorPvP* OutdoorPvPMgr::GetScriptOfAffectedZone(uint32 zoneId)
+{
+    switch (zoneId)
+    {
+        case ZONE_ID_TEMPLE_OF_AQ:
+        case ZONE_ID_RUINS_OF_AQ:
+        case ZONE_ID_GATES_OF_AQ:
+            return m_scripts[OPVP_ID_SI];
+        case ZONE_ID_STRATHOLME:
+        case ZONE_ID_SCHOLOMANCE:
+            return m_scripts[OPVP_ID_EP];
+        default:
+            return NULL;
+    }
+}
+
+/**
+   Function that handles the players which enters a specific zone
+
+   @param   player to be handled in the event
+   @param   zone id used for the current outdoor pvp script
+ */
+void OutdoorPvPMgr::HandlePlayerEnterZone(Player* player, uint32 zoneId)
+{
+    if (OutdoorPvP* script = GetScript(zoneId))
+        script->HandlePlayerEnterZone(player, true);
+    else if (OutdoorPvP* script = GetScriptOfAffectedZone(zoneId))
+        script->HandlePlayerEnterZone(player, false);
+}
+
+/**
+   Function that handles the player who leaves a specific zone
+
+   @param   player to be handled in the event
+   @param   zone id used for the current outdoor pvp script
+ */
+void OutdoorPvPMgr::HandlePlayerLeaveZone(Player* player, uint32 zoneId)
+{
+    // teleport: called once from Player::CleanupsBeforeDelete, once from Player::UpdateZone
+    if (OutdoorPvP* script = GetScript(zoneId))
+        script->HandlePlayerLeaveZone(player, true);
+    else if (OutdoorPvP* script = GetScriptOfAffectedZone(zoneId))
+        script->HandlePlayerLeaveZone(player, false);
+}
+
+void OutdoorPvPMgr::Update(uint32 diff)
+{
+    m_updateTimer.Update(diff);
+    if (!m_updateTimer.Passed())
+        return;
+
+    for (uint8 i = 0; i < MAX_OPVP_ID; ++i)
+        (*m_scripts[i]).Update(m_updateTimer.GetCurrent());
+
+    m_updateTimer.Reset();
+}
+
+/**
+   Function that gets the capture point slider value
+
+   @param   capture point entry
+ */
+int8 OutdoorPvPMgr::GetCapturePointSliderValue(uint32 entry)
+{
+    std::map<uint32, int8>::iterator itr = m_capturePointSlider.find(entry);
+
+    if (itr != m_capturePointSlider.end())
+        return itr->second;
+
+    // return default value if we can't find any
+    return CAPTURE_SLIDER_NEUTRAL;
+}
--- src/game/OutdoorPvP/OutdoorPvPMgr.h
+++ src/game/OutdoorPvP/OutdoorPvPMgr.h
@@ -19,4 +19,77 @@
 #ifndef WORLD_PVP_MGR_H
 #define WORLD_PVP_MGR_H
 
+#include "Common.h"
+#include "Policies/Singleton.h"
+#include "Timer.h"
+
+enum
+{
+    TIMER_OPVP_MGR_UPDATE           = MINUTE * IN_MILLISECONDS // 1 minute is enough for us but this might change with wintergrasp support
+};
+
+enum OutdoorPvPTypes
+{
+    OPVP_ID_SI = 0,
+    OPVP_ID_EP,
+
+    MAX_OPVP_ID
+};
+
+enum OutdoorPvPZones
+{
+    ZONE_ID_SILITHUS                = 1377,
+    ZONE_ID_TEMPLE_OF_AQ            = 3428,
+    ZONE_ID_RUINS_OF_AQ             = 3429,
+    ZONE_ID_GATES_OF_AQ             = 3478,
+
+    ZONE_ID_EASTERN_PLAGUELANDS     = 139,
+    ZONE_ID_STRATHOLME              = 2017,
+    ZONE_ID_SCHOLOMANCE             = 2057,
+};
+
+class Player;
+class GameObject;
+class Creature;
+class OutdoorPvP;
+
+class OutdoorPvPMgr
+{
+    public:
+        OutdoorPvPMgr();
+        ~OutdoorPvPMgr();
+
+        // load all outdoor pvp scripts
+        void InitOutdoorPvP();
+
+        // called when a player enters an outdoor pvp area
+        void HandlePlayerEnterZone(Player* player, uint32 zoneId);
+
+        // called when player leaves an outdoor pvp area
+        void HandlePlayerLeaveZone(Player* player, uint32 zoneId);
+
+        // return assigned outdoor pvp script
+        OutdoorPvP* GetScript(uint32 zoneId);
+
+        void Update(uint32 diff);
+
+        // Handle capture point stuff
+        int8 GetCapturePointSliderValue(uint32 entry);
+        void SetCapturePointSlider(uint32 entry, int8 value) { m_capturePointSlider[entry] = value; }
+
+    private:
+        // return assigned outdoor pvp script
+        OutdoorPvP* GetScriptOfAffectedZone(uint32 zoneId);
+
+        // contains all outdoor pvp scripts
+        OutdoorPvP* m_scripts[MAX_OPVP_ID];
+
+        std::map < uint32 /*capture point entry*/, int8 /*slider value*/ > m_capturePointSlider;
+
+        // update interval
+        ShortIntervalTimer m_updateTimer;
+};
+
+#define sOutdoorPvPMgr MaNGOS::Singleton<OutdoorPvPMgr>::Instance()
+
 #endif
