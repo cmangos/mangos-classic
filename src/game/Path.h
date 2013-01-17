44f4415964ddb743f2de481985437b1d35acd991
Schmoozerd <schmoozerd@scriptdev2.com>
2013-01-17 10:48:25 +0200

    Various Cleanups (game P)
    (based on commit f87c14e)
    
    (cleaned version of e2c0f0f)
    
    (based on commit 73838e0)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/game/Path.h
+++ src/game/Path.h
@@ -30,7 +30,7 @@ struct PathNode
     float x, y, z;
 };
 
-template<typename PathElem, typename PathNode = PathElem>
+template < typename PathElem, typename PathNode = PathElem >
 class Path
 {
     public:
