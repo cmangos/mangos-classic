38806b5849bd5be711da2c92dcacad704e20b53e
Reamer <philipp.dallig@web.de>
2013-01-21 22:43:26 +0200

    [z2114] Add EVENT_T_TIMER to EventAI. This is a generic timer that ticks out of combat and in combat
    Signed-off-by: Schmoozerd <schmoozerd@cmangos>
    
    (based on commit bae9646)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>
    
    (based on commit [s1738] - 1796f4a)

--- src/game/CreatureEventAI.cpp
+++ src/game/CreatureEventAI.cpp
@@ -133,7 +133,7 @@ bool CreatureEventAI::ProcessEvent(CreatureEventAIHolder& pHolder, Unit* pAction
     // Check event conditions based on the event type, also reset events
     switch (event.event_type)
     {
-        case EVENT_T_TIMER:
+        case EVENT_T_TIMER_IN_COMBAT:
             if (!m_creature->isInCombat())
                 return false;
 
@@ -144,6 +144,10 @@ bool CreatureEventAI::ProcessEvent(CreatureEventAIHolder& pHolder, Unit* pAction
             if (m_creature->isInCombat() || m_creature->IsInEvadeMode())
                 return false;
 
+            // Repeat Timers
+            pHolder.UpdateRepeatTimer(m_creature, event.timer.repeatMin, event.timer.repeatMax);
+            break;
+        case EVENT_T_TIMER_GENERIC:
             // Repeat Timers
             pHolder.UpdateRepeatTimer(m_creature, event.timer.repeatMin, event.timer.repeatMax);
             break;
@@ -861,6 +865,14 @@ void CreatureEventAI::JustRespawned()
     if (m_bEmptyList)
         return;
 
+    // Reset generic timer
+    for (CreatureEventAIList::iterator i = m_CreatureEventAIList.begin(); i != m_CreatureEventAIList.end(); ++i)
+    {
+        if (i->Event.event_type == EVENT_T_TIMER_GENERIC)
+            if (i->UpdateRepeatTimer(m_creature, i->Event.timer.initialMin, i->Event.timer.initialMax))
+                i->Enabled = true;
+    }
+
     // Handle Spawned Events
     for (CreatureEventAIList::iterator i = m_CreatureEventAIList.begin(); i != m_CreatureEventAIList.end(); ++i)
         if (SpawnedEventConditionsCheck((*i).Event))
@@ -1021,7 +1033,7 @@ void CreatureEventAI::EnterCombat(Unit* enemy)
                     ProcessEvent(*i, enemy);
                     break;
                     // Reset all in combat timers
-                case EVENT_T_TIMER:
+                case EVENT_T_TIMER_IN_COMBAT:
                     if ((*i).UpdateRepeatTimer(m_creature, event.timer.initialMin, event.timer.initialMax))
                         (*i).Enabled = true;
                     break;
@@ -1162,9 +1174,10 @@ void CreatureEventAI::UpdateAI(const uint32 diff)
                 switch ((*i).Event.event_type)
                 {
                     case EVENT_T_TIMER_OOC:
+                    case EVENT_T_TIMER_GENERIC:
                         ProcessEvent(*i);
                         break;
-                    case EVENT_T_TIMER:
+                    case EVENT_T_TIMER_IN_COMBAT:
                     case EVENT_T_MANA:
                     case EVENT_T_HP:
                     case EVENT_T_TARGET_HP:
