8c92b455c7fd3e0114a31d5315fad90ff40fed5b
Schmoozerd <schmoozerd@scriptdev2.com>
2013-01-22 23:27:19 +0200

    Fix a typo
    (based on commit f4a5f0f)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>
    
    (based on commit 0696e7b)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/game/SpellMgr.h
+++ src/game/SpellMgr.h
@@ -388,7 +388,7 @@ inline bool IsOnlySelfTargeting(SpellEntry const* spellInfo)
             default:
                 return false;
         }
-        switch (spellInfo->EffectImplicitTargetA[i])
+        switch (spellInfo->EffectImplicitTargetB[i])
         {
             case TARGET_SELF:
             case TARGET_NONE:
