e4016448d9f7a06567aad19025f26e52e365dec6
Cyberium <cyberium@techemail.com>
2013-01-24 08:50:09 +0200

    [z2226] Add moving possibility check before start spline movement in PointMovementGenerator.
    Thanks to Schmoozerd for his precious help and Tarwyn for pointing.
    
    (based on commit [12324] - 46ce7f2)
    
    (based on commit [s1863] - 27d469c)

--- src/game/PointMovementGenerator.cpp
+++ src/game/PointMovementGenerator.cpp
@@ -30,6 +30,9 @@
 template<class T>
 void PointMovementGenerator<T>::Initialize(T& unit)
 {
+    if (unit.hasUnitState(UNIT_STAT_CAN_NOT_REACT | UNIT_STAT_NOT_MOVE))
+        return;
+
     if (!unit.IsStopped())
         unit.StopMoving();
 
@@ -75,7 +78,9 @@ bool PointMovementGenerator<T>::Update(T& unit, const uint32& diff)
         return true;
     }
 
-    unit.addUnitState(UNIT_STAT_ROAMING_MOVE);
+    if (!unit.hasUnitState(UNIT_STAT_ROAMING_MOVE) && unit.movespline->Finalized())
+        Initialize(unit);
+
     return !unit.movespline->Finalized();
 }
 
