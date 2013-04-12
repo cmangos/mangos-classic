2ceef5abf1854705ac8e0fbf76e906443b9cafdc
Schmoozerd <schmoozerd@scriptdev2.com>
2013-04-13 00:05:22 +0300

    [z2322] Prevent annoying client freezes
    It appears it is possible for the client freezes if movement of stunned mobs is sent (combined flags of SPLINE_ENABLED and SPLINE_ROOTED).
    This fix cannot be considered proper, but there should be no sideeffects.
    
    HUGE thanks for kid10 figuring the source of some of these freezes by manually testing individual commits.
    Unexpected source(*) is [12225] (ec6772948572f304f97e956c83cb34b2)
    
    (*) Well, the thing that made these freezes visual... - I consider these source to be older and much better hidden!
    
    (based on commit [12443] - 1d04658)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>
    
    (based on commit [s1963] - 6e96889)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2353,9 +2353,8 @@ void Aura::HandleAuraModStun(bool apply, bool Real)
         {
             ((Player*)target)->m_movementInfo.SetMovementFlags(MOVEFLAG_NONE);
             target->SetStandState(UNIT_STAND_STATE_STAND);// in 1.5 client
+            target->SetRoot(true);
         }
-
-        target->SetRoot(true);
     }
     else
     {
