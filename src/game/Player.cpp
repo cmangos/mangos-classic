cmangos/mangos-classic

commit 2eff3e82abc6a32d64b3cdf80736f56ee3d960fa

[z0490] add new case for SMSG_INIT_WORLD_STATES * it isn't the best way to fix this opcode , but now shouldn't be any problem with strange loop sounds changing zones

src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "0489"
+ #define REVISION_NR "0490"
 #endif // __REVISION_NR_H__

commit 63b463790aee001b3e5bac6614ad660580da7ac1

[z2352] Fix sending initial world states
Caused by commit 2eff3e8

src/game/Player.cpp
@@ -7596,7 +7596,7 @@ static WorldStatePair SI_world_states[] =                   // Silithus
 {
     { 2313, 0 },                                            // WORLD_STATE_SI_GATHERED_A
     { 2314, 0 },                                            // WORLD_STATE_SI_GATHERED_H
-    { 2317, 0 },                                            // WORLD_STATE_SI_SILITHYST_MAX
+    { 2317, 0 }                                             // WORLD_STATE_SI_SILITHYST_MAX
 };
 
 static WorldStatePair EP_world_states[] =                   // Eastern Plaguelands
@@ -7704,15 +7704,15 @@ void Player::SendInitWorldStates(uint32 zoneid)
 
         switch (zoneid)
         {
-            case 1:
-            case 11:
-            case 12:
-            case 38:
-            case 40:
-            case 51:
-            case 1519:
-            case 1537:
-            case 2257:
+            case 1:                                         // Dun Morogh
+            case 11:                                        // Wetlands
+            case 12:                                        // Elwynn Forest
+            case 38:                                        // Loch Modan
+            case 40:                                        // Westfall
+            case 51:                                        // Searing Gorge
+            case 1519:                                      // Stormwind City
+            case 1537:                                      // Ironforge
+            case 2257:                                      // Deeprun Tram
                 break;
             case 139:                                       // Eastern Plaguelands
                 if (OutdoorPvP* outdoorPvP = sOutdoorPvPMgr.GetScript(zoneid))
@@ -7751,6 +7751,9 @@ void Player::SendInitWorldStates(uint32 zoneid)
                 FillInitialWorldState(data, count, 0x915, 0x0); // 2325 10
                 break;
         }
+
+        data.put<uint16>(count_pos, count);                 // set actual world state amount
+
         GetSession()->SendPacket(&data);
     }
 }

src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "2351"
+ #define REVISION_NR "2352"
 #endif // __REVISION_NR_H__