00ef5f6aac08bcfb8e39fa0ee6af08e85deae6ae
Schmoozerd <schmoozerd@scriptdev2.com>
2013-06-03 23:52:00 +0200

    [z2360] Stop movement of dead npcs
    Close cmangos/issues#213
    Also thanks to pyromaniac for posting this bug on the forums (and a few users for confirming)
    
    (based on commit [12489] - 204838e)
    
    Signed-off-by: Schmoozerd <schmoozerd@cmangos.net>
    
    (based on commit [s2004] - b3a2f66)
    
    Also close cmangos/issues#216
    
    Signed-off-by: Schmoozerd <schmoozerd@cmangos.net>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6934,9 +6934,9 @@ void Unit::SetDeathState(DeathState s)
         RemoveGuardians();
         UnsummonAllTotems();
 
+        StopMoving();
         i_motionMaster.Clear(false, true);
         i_motionMaster.MoveIdle();
-        StopMoving();
 
         ModifyAuraState(AURA_STATE_HEALTHLESS_20_PERCENT, false);
         // remove aurastates allowing special moves
