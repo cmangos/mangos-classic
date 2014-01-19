e3f3e8b559c90013131c4d8b981e5e698c855253
Patman64 <patman256@yahoo.com>
2014-01-19 20:18:14 +0100

    [z2422] Add a configurable delay between when a creature respawns and when it can aggro due to movement.
    (based on commit [12583] - b5e19ef)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>
    
    (based on commit [s2075] - f75668c)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/game/Creature.cpp
+++ src/game/Creature.cpp
@@ -136,7 +136,7 @@ Creature::Creature(CreatureSubtype subtype) : Unit(),
     lootForPickPocketed(false), lootForBody(false), lootForSkin(false),
     m_groupLootTimer(0), m_groupLootId(0),
     m_lootMoney(0), m_lootGroupRecipientId(0),
-    m_corpseDecayTimer(0), m_respawnTime(0), m_respawnDelay(25), m_corpseDelay(60), m_respawnradius(5.0f),
+    m_corpseDecayTimer(0), m_respawnTime(0), m_respawnDelay(25), m_corpseDelay(60), m_respawnradius(5.0f), m_respawnAggroDelay(0),
     m_subtype(subtype), m_defaultMovementType(IDLE_MOTION_TYPE), m_equipmentId(0),
     m_AlreadyCallAssistance(false), m_AlreadySearchedAssistance(false),
     m_regenHealth(true), m_AI_locked(false), m_isDeadByDefault(false),
@@ -442,6 +442,7 @@ void Creature::Update(uint32 update_diff, uint32 diff)
             {
                 DEBUG_FILTER_LOG(LOG_FILTER_AI_AND_MOVEGENSS, "Respawning...");
                 m_respawnTime = 0;
+                m_respawnAggroDelay = sWorld.getConfig(CONFIG_UINT32_CREATURE_RESPAWN_AGGRO_DELAY);
                 lootForPickPocketed = false;
                 lootForBody         = false;
                 lootForSkin         = false;
@@ -509,6 +510,11 @@ void Creature::Update(uint32 update_diff, uint32 diff)
         }
         case ALIVE:
         {
+            if (m_respawnAggroDelay <= update_diff)
+                m_respawnAggroDelay = 0;
+            else
+                m_respawnAggroDelay -= update_diff;
+
             if (m_isDeadByDefault)
             {
                 if (m_corpseDecayTimer <= update_diff)
@@ -1776,6 +1782,9 @@ bool Creature::CanInitiateAttack()
         return false;
 
     if (isPassiveToHostile())
+        return false;
+
+    if (m_respawnAggroDelay != 0)
         return false;
 
     return true;
--- src/game/World.cpp
+++ src/game/World.cpp
@@ -697,6 +697,7 @@ void World::LoadConfigSettings(bool reload)
     setConfigMinMax(CONFIG_FLOAT_GHOST_RUN_SPEED_BG,      "Death.Ghost.RunSpeed.Battleground", 1.0f, 0.1f, 10.0f);
 
     setConfig(CONFIG_FLOAT_THREAT_RADIUS, "ThreatRadius", 100.0f);
+    setConfigMin(CONFIG_UINT32_CREATURE_RESPAWN_AGGRO_DELAY, "CreatureRespawnAggroDelay", 5000, 0);
 
     setConfig(CONFIG_BOOL_BATTLEGROUND_CAST_DESERTER,                  "Battleground.CastDeserter", true);
     setConfigMinMax(CONFIG_UINT32_BATTLEGROUND_QUEUE_ANNOUNCER_JOIN,   "Battleground.QueueAnnouncer.Join", 0, 0, 2);
