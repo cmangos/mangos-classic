5c2d2cc3529cf562b29893d7577ddcabfbb36239
Undergarun <undergarun@gmail.com>
2014-01-19 21:12:54 +0100

    [z2430] Fix LANG_ADDON use on Guild Channels and avoid a possible crash.
    Part of @rsa work too.
    
    (based on commit [12594] - adcd491)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>
    
    (based on commit [s2085] - e20c2fc)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/game/Guild.cpp
+++ src/game/Guild.cpp
@@ -546,7 +546,7 @@ void Guild::BroadcastToGuild(WorldSession* session, const std::string& msg, uint
     if (session && session->GetPlayer() && HasRankRight(session->GetPlayer()->GetRank(), GR_RIGHT_GCHATSPEAK))
     {
         WorldPacket data;
-        ChatHandler::BuildChatPacket(data, CHAT_MSG_GUILD, msg.c_str(), LANG_UNIVERSAL, session->GetPlayer()->GetChatTag(), session->GetPlayer()->GetObjectGuid());
+        ChatHandler::BuildChatPacket(data, CHAT_MSG_GUILD, msg.c_str(), Language(language), session->GetPlayer()->GetChatTag(), session->GetPlayer()->GetObjectGuid(), session->GetPlayer()->GetName());
 
         for (MemberList::const_iterator itr = members.begin(); itr != members.end(); ++itr)
         {
@@ -565,7 +565,7 @@ void Guild::BroadcastToOfficers(WorldSession* session, const std::string& msg, u
         for (MemberList::const_iterator itr = members.begin(); itr != members.end(); ++itr)
         {
             WorldPacket data;
-            ChatHandler::BuildChatPacket(data, CHAT_MSG_OFFICER, msg.c_str(), LANG_UNIVERSAL, session->GetPlayer()->GetChatTag(), session->GetPlayer()->GetObjectGuid());
+            ChatHandler::BuildChatPacket(data, CHAT_MSG_OFFICER, msg.c_str(), Language(language), session->GetPlayer()->GetChatTag(), session->GetPlayer()->GetObjectGuid(), session->GetPlayer()->GetName());
 
             Player* pl = ObjectAccessor::FindPlayer(ObjectGuid(HIGHGUID_PLAYER, itr->first));
 
