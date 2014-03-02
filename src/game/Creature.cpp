d27850d841fcc88d6304bdb612dc47dde3fa4728
Patman64 <patman256@yahoo.com>
2014-03-01 23:50:18 -0500

    Add CREATURE_FLAG_EXTRA_NO_CALL_ASSIST.
    This is used to prevent creatures from pulling too many nearby monsters,
    for example during certain scripted events.
    
    A good example is Mr. Smite in the Deadmines. There are two guards at the
    bottom of his bridge which trigger aggro for him, but if he is missing
    this flag he will also pull nearby mobs on the ship, which is not expected
    behavior.

--- src/game/Creature.cpp
+++ src/game/Creature.cpp
@@ -1718,6 +1718,10 @@ void Creature::CallAssistance()
     if (!m_AlreadyCallAssistance && getVictim() && !isCharmed())
     {
         SetNoCallAssistance(true);
+
+        if (GetCreatureInfo()->flags_extra & CREATURE_FLAG_EXTRA_NO_CALL_ASSIST)
+            return;
+
         AI()->SendAIEventAround(AI_EVENT_CALL_ASSISTANCE, getVictim(), sWorld.getConfig(CONFIG_UINT32_CREATURE_FAMILY_ASSISTANCE_DELAY), sWorld.getConfig(CONFIG_FLOAT_CREATURE_FAMILY_ASSISTANCE_RADIUS));
     }
 }
