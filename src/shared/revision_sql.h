99d6671c9a9824561c062db3c6a59faece749fd5
Sidsukana <pgsilent@gmail.com>
2014-03-20 21:18:28 +0100

    [z2442] Fix spell 6346 (Fear Ward)

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_z2410_s2063_12562_01_characters_various_tables"
- #define REVISION_DB_MANGOS "required_z2410_s2063_12562_01_mangos_playercreateinfo_action"
+ #define REVISION_DB_MANGOS "required_z2442_xxxx_01_mangos_spell_proc_event"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__
