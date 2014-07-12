04504b7c62e1ee8f2237e9f43c799af299c50e29
stracamp <wowcmp@openmailbox.org>
2014-07-12 02:51:13 +0200

    [z2522] Fix vmap_extractor on case sensitive filesystems

--- contrib/vmap_extractor/vmapextract/vmapexport.cpp
+++ contrib/vmap_extractor/vmapextract/vmapexport.cpp
@@ -317,18 +317,17 @@ bool fillArchiveNameVector(std::vector<std::string>& pArchiveNames)
 
     // open expansion and common files
     printf("Opening data files from data directory.\n");
-    sprintf(path, "%sterrain.mpq", input_path);
+    sprintf(path, "%sterrain.MPQ", input_path);
     pArchiveNames.push_back(path);
-    sprintf(path, "%smodel.mpq", input_path);
+    sprintf(path, "%smodel.MPQ", input_path);
     pArchiveNames.push_back(path);
+    sprintf(path, "%stexture.MPQ", input_path);
     pArchiveNames.push_back(path);
-    sprintf(path, "%stexture.mpq", input_path);
+    sprintf(path, "%swmo.MPQ", input_path);
     pArchiveNames.push_back(path);
-    sprintf(path, "%swmo.mpq", input_path);
+    sprintf(path, "%sbase.MPQ", input_path);
     pArchiveNames.push_back(path);
-    sprintf(path, "%sbase.mpq", input_path);
-    pArchiveNames.push_back(path);
-    sprintf(path, "%smisc.mpq", input_path);
+    sprintf(path, "%smisc.MPQ", input_path);
 
     // now, scan for the patch levels in the core dir
     printf("Scanning patch levels from data directory.\n");
