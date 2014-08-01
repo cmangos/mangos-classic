d2890dd8226ba2164a758d26c07e1296f103864a
Cyberium <cybermomo@techemail.com>
2014-08-01 13:30:28 +0200

    [z2508] Restore movemap generator compil and change mmap files versions.
    (based on commit [12694] - b40a3ea)
    
    Signed-off-by: Peter Camastral <petercamastral@gmx.ch>

--- src/game/MoveMapSharedDefines.h
+++ src/game/MoveMapSharedDefines.h
@@ -23,7 +23,7 @@
 #include "../recastnavigation/Detour/Include/DetourNavMesh.h"
 
 #define MMAP_MAGIC 0x4d4d4150   // 'MMAP'
-#define MMAP_VERSION 3
+#define MMAP_VERSION 4
 
 struct MmapTileHeader
 {
