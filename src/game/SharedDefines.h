d461bb2b0e22ac68747a99feb4fd91bcf8fccb6c
Cyberium <cybermomo@techemail.com>
2014-08-04 21:53:11 +0200

    [z2480] Simple fix for Stealth is removed on fall damage problem Surely could be done better way...
    Can be backported but need a little more work to make it work on TBC/Classic
    
    (based on commit [12638] - 2f8474a)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>
    
    (based on commit [s2121] - 676d7ec)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/game/SharedDefines.h
+++ src/game/SharedDefines.h
@@ -1016,7 +1016,8 @@ enum DamageEffectType
     DOT                     = 2,
     HEAL                    = 3,
     NODAMAGE                = 4,                            //< used also in case when damage applied to health but not applied to spell channelInterruptFlags/etc
-    SELF_DAMAGE             = 5
+    SELF_DAMAGE_ROGUE_FALL  = 5,                            //< used to avoid rogue loosing stealth on falling damage
+    SELF_DAMAGE             = 6
 };
 
 enum GameobjectTypes
