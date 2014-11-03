5de9577571776b0442913929323278b85ad5d755
Cala <calaftp@free.fr>
2014-11-03 23:06:51 +0100

    [z2534] Add missing SQL update in previous commit
    Thanks evil.at.wow for pointing.

--- sql/mangos.sql
+++ sql/mangos.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
-  `required_z2530_sxxxx_xxxxx_01_mangos_gameobject_template` bit(1) DEFAULT NULL
+  `required_z2533_sxxxx_xxxxx_01_mangos_spell_chain` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
 
 --
