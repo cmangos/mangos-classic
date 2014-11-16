95f6c4202a46028ed6337006f87223a15e010d20
Schmoozerd <schmoozerd@cmangos.net>
2014-11-16 20:26:06 +0100

    [z2578] Implement CREATURE_FLAG_EXTRA_ACTIVE
    Setting this flag 0x1000 to creature_template.ExtraFlags will result in
    the npc being loaded and set to active when the map is created.
    
    In case of continents this will happen on server startup.
    
    Note to DB-Devs: Use this flag with care, it will create more memory and
    cpu usage for users.
    
    Note to users: If you have a bottleneck and do not care about i.e. mobs
    with huge paths, just remove this flag from the npcs
    
    (based on commit [12784] - 00d86f0)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>
    
    (based on commit [s2224] - 02a1afa)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/game/Creature.h
+++ src/game/Creature.h
@@ -55,6 +55,7 @@ enum CreatureFlagsExtra
     CREATURE_EXTRA_FLAG_AGGRO_ZONE      = 0x00000200,       // creature sets itself in combat with zone on aggro
     CREATURE_EXTRA_FLAG_GUARD           = 0x00000400,       // creature is a guard
     CREATURE_EXTRA_FLAG_NO_CALL_ASSIST  = 0x00000800,       // creature shouldn't call for assistance on aggro
+    CREATURE_EXTRA_FLAG_ACTIVE          = 0x00001000,       // creature is active object. Grid of this creature will be loaded and creature set as active
 };
 
 // GCC have alternative #pragma pack(N) syntax and old gcc version not support pack(push,N), also any gcc version not support it at some platform
