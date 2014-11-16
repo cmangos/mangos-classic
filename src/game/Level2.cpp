a269a8e254fc7218e4cec6a80247b191d82403dd
Schmoozerd <schmoozerd@cmangos.net>
2014-11-16 18:52:03 +0100

    [z2540] Fix a crashing typo related to .npc unfollow command
    Thanks to Lillecarl for pointing.
    Close cmangos/issues#445
    
    (based on commit [12709] - dd15878)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>
    
    (based on commit [s2178] - 0db3702)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/game/Level2.cpp
+++ src/game/Level2.cpp
@@ -2030,7 +2030,7 @@ bool ChatHandler::HandleNpcUnFollowCommand(char* /*args*/)
     if (creature->GetMotionMaster()->empty() ||
             creature->GetMotionMaster()->GetCurrentMovementGeneratorType() != FOLLOW_MOTION_TYPE)
     {
-        PSendSysMessage(LANG_CREATURE_NOT_FOLLOW_YOU);
+        PSendSysMessage(LANG_CREATURE_NOT_FOLLOW_YOU, creature->GetName());
         SetSentErrorMessage(true);
         return false;
     }
@@ -2040,7 +2040,7 @@ bool ChatHandler::HandleNpcUnFollowCommand(char* /*args*/)
 
     if (mgen->GetTarget() != player)
     {
-        PSendSysMessage(LANG_CREATURE_NOT_FOLLOW_YOU);
+        PSendSysMessage(LANG_CREATURE_NOT_FOLLOW_YOU, creature->GetName());
         SetSentErrorMessage(true);
         return false;
     }
