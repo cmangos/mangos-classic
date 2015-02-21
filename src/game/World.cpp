98961988a583c80f3be52c6533505af4727af34c
stfx <stfx@hotmail.de>
2015-02-21 23:40:27 +0100

    [z2593] Fix a few more static code analysis warnings
    (based on commit [12794] - 9acdf91)
    
    Signed-off-by: evil-at-wow <evil.at.wow@gmail.com>
    
    (based on commit [s2235] - 166b35b)
    
    Signed-off-by: Cala <calaftp@free.fr>

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -137,8 +137,7 @@ World::~World()
     VMAP::VMapFactory::clear();
     MMAP::MMapFactory::clear();
 
-    if (m_configForceLoadMapIds)
-        delete m_configForceLoadMapIds;
+    delete m_configForceLoadMapIds;
 
     // TODO free addSessQueue
 }
