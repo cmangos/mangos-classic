f54f1efd5e2dc8ecb00fad448378639fc717d501
Cyberium <cybermomo@techemail.com>
2015-03-07 18:38:30 +0100

    [z2617] Implement holes handling on map. Now GetHeight correctly returns no height when a hole is detected. This is very useful in vmap object because in some cases we had a bad value returned by map data wich makes it hard to choose the right height.
    (based on commit [12818] - 5bbd180)
    
    Signed-off-by: evil-at-wow <evil.at.wow@gmail.com>
    
    (based on commit [s2259] - 0acda59)
    
    Signed-off-by: Cala <calaftp@free.fr>

--- src/game/GridMap.cpp
+++ src/game/GridMap.cpp
@@ -36,6 +36,9 @@ char const* MAP_AREA_MAGIC    = "AREA";
 char const* MAP_HEIGHT_MAGIC  = "MHGT";
 char const* MAP_LIQUID_MAGIC  = "MLIQ";
 
+static uint16 holetab_h[4] = { 0x1111, 0x2222, 0x4444, 0x8888 };
+static uint16 holetab_v[4] = { 0x000F, 0x00F0, 0x0F00, 0xF000 };
+
 GridMap::GridMap()
 {
     m_flags = 0;
@@ -49,6 +52,7 @@ GridMap::GridMap()
     m_gridGetHeight = &GridMap::getHeightFromFlat;
     m_V9 = NULL;
     m_V8 = NULL;
+    memset(m_holes, 0, sizeof(m_holes));
 
     // Liquid data
     m_liquidType    = 0;
@@ -86,6 +90,14 @@ bool GridMap::loadData(char* filename)
         if (header.areaMapOffset && !loadAreaData(in, header.areaMapOffset, header.areaMapSize))
         {
             sLog.outError("Error loading map area data\n");
+            fclose(in);
+            return false;
+        }
+
+        // loadup holes data
+        if (header.holesOffset && !loadHolesData(in, header.holesOffset, header.holesSize))
+        {
+            sLog.outError("Error loading map holes data\n");
             fclose(in);
             return false;
         }
@@ -195,6 +207,16 @@ bool GridMap::loadHeightData(FILE* in, uint32 offset, uint32 /*size*/)
     return true;
 }
 
+bool GridMap::loadHolesData(FILE* in, uint32 offset, uint32 size)
+{
+    if (fseek(in, offset, SEEK_SET) != 0)
+        return false;
+
+    if (fread(&m_holes, sizeof(m_holes), 1, in) != 1)
+        return false;
+    return true;
+}
+
 bool GridMap::loadGridMapLiquidData(FILE* in, uint32 offset, uint32 /*size*/)
 {
     GridMapLiquidHeader header;
@@ -245,10 +267,22 @@ float GridMap::getHeightFromFlat(float /*x*/, float /*y*/) const
     return m_gridHeight;
 }
 
+bool GridMap::isHole(int row, int col) const
+{
+    int cellRow = row / 8;     // 8 squares per cell
+    int cellCol = col / 8;
+    int holeRow = row % 8 / 2;
+    int holeCol = (col - (cellCol * 8)) / 2;
+
+    uint16 hole = m_holes[cellRow][cellCol];
+
+    return (hole & holetab_h[holeCol] & holetab_v[holeRow]) != 0;
+}
+
 float GridMap::getHeightFromFloat(float x, float y) const
 {
     if (!m_V8 || !m_V9)
-        return m_gridHeight;
+        return INVALID_HEIGHT_VALUE;
 
     x = MAP_RESOLUTION * (32 - x / SIZE_OF_GRIDS);
     y = MAP_RESOLUTION * (32 - y / SIZE_OF_GRIDS);
@@ -259,6 +293,9 @@ float GridMap::getHeightFromFloat(float x, float y) const
     y -= y_int;
     x_int &= (MAP_RESOLUTION - 1);
     y_int &= (MAP_RESOLUTION - 1);
+
+    if (isHole(x_int, y_int))
+        return INVALID_HEIGHT_VALUE;
 
     // Height stored as: h5 - its v8 grid, h1-h4 - its v9 grid
     // +--------------> X
