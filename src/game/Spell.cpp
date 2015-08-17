a9e75060582b429b5b45048d0dbf91fb51ade1a2
leewalkergm <leewalkergm@gmail.com>
2015-08-17 18:06:31 +0100

    Slightly cleaner code -- limited to only disarm trap spell

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -4674,7 +4674,7 @@ SpellCastResult Spell::CheckCast(bool strict)
                             !((Player*)m_caster)->CanUseBattleGroundObject())
                         return SPELL_FAILED_TRY_AGAIN;
 
-					if (((Player*)m_caster)->GetDistance(go->GetPositionX(), go->GetPositionY(), go->GetPositionZ()) > 5)
+					if (m_spellInfo->Id == 1842 && ((Player*)m_caster)->GetDistance(go) > 5)
 						return SPELL_FAILED_OUT_OF_RANGE;
 
                     lockId = go->GetGOInfo()->GetLockId();
