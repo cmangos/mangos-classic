3b5212fb134e535c1c8d1ae3cdb53f8f9d3ee13e
stfx <stfx@hotmail.de>
2015-09-03 00:16:49 +0200

    Cleanup code in FindPlatform.cmake
    (based on cmangos/mangos-wotlk@159c57d)
    
    Signed-off-by: stfx <stfx@hotmail.de>
    
    (based on cmangos/mangos-tbc@95ebdc5)
    
    Signed-off-by: stfx <stfx@hotmail.de>

--- cmake/FindPlatform.cmake
+++ cmake/FindPlatform.cmake
@@ -1,23 +1,14 @@
-# Already set, so only add definition
-# IF(PLATFORM)
-#   IF(PLATFORM MATCHES X64 AND WIN32)
-#     ADD_DEFINITIONS("-D_WIN64")
-#   ENDIF()
-# ELSE()
-  # default to x86 platform.  We'll check for X64 in a bit
-  SET(PLATFORM X86)
-
-  # This definition is necessary to work around a bug with Intellisense described
-  # here: http://tinyurl.com/2cb428.  Syntax highlighting is important for proper
-  # debugger functionality.
-
-  IF(CMAKE_SIZEOF_VOID_P MATCHES 8)
-      MESSAGE(STATUS "Detected 64-bit platform.")
-      IF(WIN32)
-        ADD_DEFINITIONS("-D_WIN64")
-      ENDIF()
-      SET(PLATFORM X64)
-  ELSE()
-      MESSAGE(STATUS "Detected 32-bit platform.")
-  ENDIF()
-# ENDIF()
+# check what platform we're on (64-bit or 32-bit), and create a simpler test than CMAKE_SIZEOF_VOID_P
+if(CMAKE_SIZEOF_VOID_P MATCHES 8)
+    set(PLATFORM X64)
+    # This definition is necessary to work around a bug with Intellisense described
+    # here: http://tinyurl.com/2cb428.  Syntax highlighting is important for proper
+    # debugger functionality.
+    if(WIN32)
+        add_definitions("-D_WIN64")
+    endif()
+    message(STATUS "Detected 64-bit platform")
+else()
+    set(PLATFORM X86)
+    message(STATUS "Detected 32-bit platform")
+endif()
