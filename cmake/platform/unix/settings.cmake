b4542ce6fb02bfb0cf7ea2bab9cefebe6be50229
stfx <stfx@hotmail.de>
2015-09-03 00:17:03 +0200

    Add separate cmake files for gcc and clang
    Based on TrinityCore cmake implementation
    
    (based on cmangos/mangos-wotlk@ab6b482)
    
    Signed-off-by: stfx <stfx@hotmail.de>
    
    (based on cmangos/mangos-tbc@fc591c9)
    
    Signed-off-by: stfx <stfx@hotmail.de>

--- cmake/platform/unix/settings.cmake
+++ cmake/platform/unix/settings.cmake
@@ -11,3 +11,12 @@ add_custom_target(uninstall
   "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake"
 )
 message(STATUS "UNIX: Created uninstall target")
+
+message(STATUS "UNIX: Detected compiler: ${CMAKE_C_COMPILER}")
+if(CMAKE_C_COMPILER MATCHES "gcc" OR CMAKE_C_COMPILER_ID STREQUAL "GNU")
+  include(${CMAKE_SOURCE_DIR}/cmake/compiler/gcc/settings.cmake)
+elseif(CMAKE_C_COMPILER MATCHES "clang")
+  include(${CMAKE_SOURCE_DIR}/cmake/compiler/clang/settings.cmake)
+else()
+add_definitions(-D_BUILD_DIRECTIVE='"${CMAKE_BUILD_TYPE}"')
+endif()
