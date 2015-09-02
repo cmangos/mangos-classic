ed7b42b99098cd1201b5e9aa706af03d74e3be4e
stfx <stfx@hotmail.de>
2015-09-03 00:17:08 +0200

    Add separate cmake file to show configuration options
    [ci skip]
    
    (based on cmangos/mangos-wotlk@7e9bf06)
    
    Signed-off-by: stfx <stfx@hotmail.de>
    
    (based on cmangos/mangos-tbc@9b4a6cf)
    
    Signed-off-by: stfx <stfx@hotmail.de>

---
+++ cmake/showoptions.cmake
@@ -0,0 +1,50 @@
+# output generic information about the core
+message(STATUS "CMaNGOS-Core revision : ${GIT_REVISION}")
+message(STATUS "Install server to     : ${CMAKE_INSTALL_PREFIX}")
+
+# Show infomation about the options selected during configuration
+if(DEFINED INCLUDE_BINDINGS_DIR AND INCLUDE_BINDINGS_DIR)
+  message(STATUS "Build script library  : Yes (using ${INCLUDE_BINDINGS_DIR})")
+else()
+  message(STATUS "Build script library  : No")
+endif()
+
+# if(CLI)
+#   message(STATUS "Build with CLI        : Yes (default)")
+#   add_definitions(-DENABLE_CLI)
+# else()
+#   message(STATUS "Build with CLI        : No")
+# endif()
+
+# if(RA)
+#   message(STATUS "* Build with RA       : Yes")
+#   add_definitions(-DENABLE_RA)
+# else(RA)
+#   message(STATUS "* Build with RA       : No  (default)")
+# endif(RA)
+
+if(PCH)
+  message(STATUS "Use PCH               : Yes")
+else()
+  message(STATUS "Use PCH               : No")
+endif()
+
+if(DEBUG)
+  message(STATUS "Build in debug-mode   : Yes")
+else()
+  message(STATUS "Build in debug-mode   : No  (default)")
+endif()
+
+# if(SQL)
+#   message(STATUS "Install SQL-files     : Yes")
+# else()
+#   message(STATUS "Install SQL-files     : No  (default)")
+# endif()
+
+# if(TOOLS)
+#   message(STATUS "Build map/vmap tools  : Yes")
+# else()
+#   message(STATUS "Build map/vmap tools  : No  (default)")
+# endif()
+
+message("")
