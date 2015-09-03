f58a2e1bf31f751291dc4c06872a43a9a09610e4
stfx <stfx@hotmail.de>
2015-09-03 11:34:00 +0200

    Fix sql update file

--- sql/updates/z2680_01_mangos_battleground_template.sql
+++ sql/updates/z2680_01_mangos_battleground_template.sql
@@ -2,6 +2,5 @@ ALTER TABLE db_version CHANGE COLUMN required_z2678_03_mangos required_z2680_01_
 
 ALTER TABLE `battleground_template` ADD `StartMaxDist` float NOT NULL DEFAULT 0 AFTER `HordeStartO`;
 
-UPDATE `battleground_template` SET `StartMaxDist`=200 WHERE `id`=30; -- IC
 UPDATE `battleground_template` SET `StartMaxDist`=100 WHERE `id`=1; -- AV
-UPDATE `battleground_template` SET `StartMaxDist`=75 WHERE `id` IN (2,3,7); -- WS, AB, EY
+UPDATE `battleground_template` SET `StartMaxDist`=75 WHERE `id` IN (2,3); -- WS, AB
