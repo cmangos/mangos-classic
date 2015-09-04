f93785b0dffe9f065e9a1ed3af98cbc49c08e070
stfx <stfx@hotmail.de>
2015-09-04 14:21:22 +0200

    Fix recent sql updates
    Thanks to @Tobschinski

--- sql/updates/0.12.3/z2678_01_realmd.sql
+++ sql/updates/0.12.3/z2678_01_realmd.sql
@@ -1,2 +1,2 @@
-ALTER TABLE realmd_db_version CHANGE COLUMN required_z2678_01_realmd required_z2678_01_realmd bit;
+ALTER TABLE realmd_db_version CHANGE COLUMN required_10008_01_realmd_realmd_db_version required_z2678_01_realmd bit;
 
--- sql/updates/0.12.3/z2679_03_characters_guild_member.sql
+++ sql/updates/0.12.3/z2679_03_characters_guild_member.sql
@@ -1,4 +1,4 @@
-ALTER TABLE character_db_version CHANGE COLUMN required_z2679_02_characters_guild_bank_tab required_z2679_03_characters_guild_member bit;
+ALTER TABLE character_db_version CHANGE COLUMN required_z2679_01_characters_guild required_z2679_03_characters_guild_member bit;
 
 ALTER TABLE `guild_member` CHANGE `pnote` `pnote` varchar(31) NOT NULL DEFAULT '';
 ALTER TABLE `guild_member` CHANGE `offnote` `offnote` varchar(31) NOT NULL DEFAULT '';
