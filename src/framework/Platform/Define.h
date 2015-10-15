e53da8a1b90b84f96a49d9eed994f7ec07afb806
Cyberium <cybermomo@techemail.com>
2015-10-15 11:28:39 +0200

    Fix for some Mac OSX but can broke other APPLE config. We need more input here

--- src/framework/Platform/Define.h
+++ src/framework/Platform/Define.h
@@ -57,7 +57,8 @@ typedef ACE_SHLIB_HANDLE MANGOS_LIBRARY_HANDLE;
 #else // PLATFORM != PLATFORM_WINDOWS
 #  define MANGOS_EXPORT export
 #  if defined(__APPLE_CC__) && defined(BIG_ENDIAN)
-#    define MANGOS_IMPORT __attribute__ ((longcall))
+#    define MANGOS_IMPORT
+//#    define MANGOS_IMPORT __attribute__ ((longcall)) Is this still needed for some system? If yes we need to find a defined macro to identify wheter to use longcall or not
 #  elif defined(__x86_64__)
 #    define MANGOS_IMPORT
 #  else
