e8aa9cbe1c335119f0e428ad06b1b48017cd444c
Cyberium <cybermomo@techemail.com>
2015-10-16 09:09:49 +0200

    Use more defined macro to attribute or not longcall Seem that longcall is needed only for power pc and i found these macro are perfectly fit to identify powerpc.
    For big endian problem we'll see after boost integration.

--- src/framework/Platform/Define.h
+++ src/framework/Platform/Define.h
@@ -56,9 +56,12 @@ typedef ACE_SHLIB_HANDLE MANGOS_LIBRARY_HANDLE;
 #  define MANGOS_IMPORT __cdecl
 #else // PLATFORM != PLATFORM_WINDOWS
 #  define MANGOS_EXPORT export
-#  if defined(__APPLE_CC__) && defined(BIG_ENDIAN)
-#    define MANGOS_IMPORT
-//#    define MANGOS_IMPORT __attribute__ ((longcall)) Is this still needed for some system? If yes we need to find a defined macro to identify wheter to use longcall or not
+#  if defined(__APPLE_CC__) && defined(BIG_ENDIAN) // TODO:: more work to do with byte order. Have to be rechecked after boost integration.
+#    if (defined (__ppc__) || defined (__powerpc__))
+#      define MANGOS_IMPORT __attribute__ ((longcall))
+#    else
+#      define MANGOS_IMPORT
+#    endif
 #  elif defined(__x86_64__)
 #    define MANGOS_IMPORT
 #  else
