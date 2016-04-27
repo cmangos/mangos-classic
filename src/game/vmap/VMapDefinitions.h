fa5aad7a670d797e9a573723f711990453a43f61
0x2AFF <schlosser.st@gmail.com>
2016-04-27 17:23:06 +0200

    Fix mmap-extractor Didn't compile on linux. Don't know if compilation worked on windows.

--- src/game/vmap/VMapDefinitions.h
+++ src/game/vmap/VMapDefinitions.h
@@ -38,7 +38,6 @@ namespace VMAP
 #define ERROR_LOG(...) sLog.outError(__VA_ARGS__);
 #elif defined MMAP_GENERATOR
 #include <assert.h>
-#define MANGOS_ASSERT(x) assert(x)
 #define DEBUG_LOG(...) 0
 #define DETAIL_LOG(...) 0
 #define LOG_FILTER_MAP_LOADING true
@@ -46,7 +45,6 @@ namespace VMAP
 #define ERROR_LOG(...) do{ printf("ERROR:"); printf(__VA_ARGS__); printf("\n"); } while(0)
 #else
 #include <assert.h>
-#define MANGOS_ASSERT(x) assert(x)
 #define DEBUG_LOG(...) do{ printf(__VA_ARGS__); printf("\n"); } while(0)
 #define DETAIL_LOG(...) do{ printf(__VA_ARGS__); printf("\n"); } while(0)
 #define LOG_FILTER_MAP_LOADING true
