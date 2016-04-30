b4d1b360c442149b4e2e7150ee3a68b9bede5743
Caydan <caydan@vanilla.re>
2016-04-30 23:27:44 +0200

    Game/Unit: preventing players from keep running when fear/mc was removed

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -8462,6 +8462,9 @@ void Unit::SetFeared(bool apply, ObjectGuid casterGuid, uint32 spellID, uint32 t
     {
         RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_FLEEING);
 
+        if (GetTypeId() == TYPEID_PLAYER)
+            StopMoving(true);
+
         GetMotionMaster()->MovementExpired(false);
         if (GetTypeId() == TYPEID_PLAYER)
             StopMoving(true);
