f12ff28c81640c77bd76dee63bb2232a51633fca
Laizerox <dpetmanson@gmail.com>
2016-05-08 18:04:04 +0300

    Fix player kicking
    Probably missing backport

--- src/game/WorldSession.cpp
+++ src/game/WorldSession.cpp
@@ -490,7 +490,7 @@ void WorldSession::LogoutPlayer(bool Save)
 /// Kick a player out of the World
 void WorldSession::KickPlayer()
 {
-    if (m_Socket->IsClosed())
+    if (!m_Socket->IsClosed())
         m_Socket->Close();
 }
 
