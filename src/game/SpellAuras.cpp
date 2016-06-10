29ede4ec99dbfeb4bac6ac208c7d8695a7702274
sidsukana <pgsilent@gmail.com>
2016-06-10 13:35:11 +0500

    Fix absorb amount of Power Word: Shield (10% pre-bc) http://wow.gamepedia.com/index.php?title=Power_Word:_Shield&oldid=282582

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -4238,8 +4238,8 @@ void Aura::HandleSchoolAbsorb(bool apply, bool Real)
                     // Power Word: Shield
                     if (spellProto->SpellFamilyFlags & uint64(0x0000000000000001))
                     {
-                        //+30% from +healing bonus
-                        DoneActualBenefit = caster->SpellBaseHealingBonusDone(GetSpellSchoolMask(spellProto)) * 0.3f;
+                        //+10% from +healing bonus
+                        DoneActualBenefit = caster->SpellBaseHealingBonusDone(GetSpellSchoolMask(spellProto)) * 0.1f;
                         break;
                     }
                     break;
