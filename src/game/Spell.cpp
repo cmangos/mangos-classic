cmangos/mangos-tbc

Core/Procs: Allow Lightning Shield to trigger procs.
https://report.nostalrius.org/plugins/tracker/?aid=4482

Signed-off-by: robinsch <robin.schriever.hude@web.de>

src/game/Spell.cpp
@@ -684,6 +684,11 @@ void Spell::prepareDataForTriggerSystem()
                 if (m_spellInfo->IsFitToFamilyMask(UI64LIT(0x0001000000200000)))
                     m_canTrigger = true;
                 break;
+            case SPELLFAMILY_SHAMAN:
+                // Lightning Shield
+                if (m_spellInfo->IsFitToFamilyMask(UI64LIT(0x0000000000000400)))
+                    m_canTrigger = true;
+                break;
             default:
                 break;
         }

cmangos/mangos-classic

src/game/Spell.cpp
@@ -657,6 +657,11 @@ void Spell::prepareDataForTriggerSystem()
                 if (m_spellInfo->IsFitToFamilyMask(uint64(0x0001000000200000)))
                     m_canTrigger = true;
                 break;
+            case SPELLFAMILY_SHAMAN:
+                // Lightning Shield
+                if (m_spellInfo->IsFitToFamilyMask(uint64(0x0000000000000400)))
+                    m_canTrigger = true;
+                break;
             default:
                 break;
         }