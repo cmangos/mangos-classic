c1669a4cc92b789e977866e97a37e8c99edc4e57
Phatcat <holsthein@live.dk>
2016-07-13 04:48:22 +0200

    Don't evade in dungeons

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7167,7 +7167,7 @@ bool Unit::SelectHostileTarget()
 
             // check if currently selected target is reachable
             // NOTE: path alrteady generated from AttackStart()
-            if (!GetMotionMaster()->GetCurrent()->IsReachable())
+            if (!(GetMotionMaster()->GetCurrent()->IsReachable() || m_creature->GetMap()->IsDungeon()))
             {
                 // remove all taunts
                 RemoveSpellsCausingAura(SPELL_AURA_MOD_TAUNT);
@@ -7176,6 +7176,7 @@ bool Unit::SelectHostileTarget()
                 {
                     // only one target in list, we have to evade after timer
                     // TODO: make timer - inside Creature class
+                    // WHY on EARTH would we not just do it here? -.-
                     ((Creature*)this)->AI()->EnterEvadeMode();
                 }
                 else
