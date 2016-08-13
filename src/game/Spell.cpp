bbda45a4e7da1d5acaa9d334c037d6a8c072bea6
tbsp <kmds3iugt>
2016-08-13 12:47:16 -0700

    Fixed water walking not being castable on warriors, priests in shadow form, or stealthed rogues.
    Based on: https://github.com/cmangos/mangos-tbc/commit/57d7276353f9e75359565ae11a54f09de7cb2f44
    
    Signed-off-by: Phatcat <holsthein@live.dk>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -5052,8 +5052,8 @@ SpellCastResult Spell::CheckCast(bool strict)
                 {
                     Player const* player = static_cast<Player const*>(expectedTarget);
 
-                    // Player is not allowed to cast water walk on shapeshifted/mounted player
-                    if (player->GetShapeshiftForm() != FORM_NONE || player->IsMounted())
+                    // Player is not allowed to cast water walk on shapeshifted/mounted player 
+                    if (player->IsInDisallowedMountForm() || player->IsMounted())
                         return SPELL_FAILED_BAD_TARGETS;
                 }
             }
