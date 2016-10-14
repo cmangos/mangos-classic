a8e8fa2e1567f947bea6b7fc2c5ed80e8571b708
Ono <warlockbugs@users.noreply.github.com>
2016-10-14 06:12:57 +0300

    Fix no stats for group members out of visility range on log in
    * Fixes group members having level 0 and invalid stats until within visibility range.
    * Also fixes inability to summon these group members via meeting stone aka "Target is too low level" fix.

--- src/game/CharacterHandler.cpp
+++ src/game/CharacterHandler.cpp
@@ -592,7 +592,11 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder* holder)
 
     // announce group about member online (must be after add to player list to receive announce to self)
     if (Group* group = pCurrChar->GetGroup())
+    {
         group->SendUpdate();
+        pCurrChar->SetGroupUpdateFlag(GROUP_UPDATE_FULL);
+        group->UpdatePlayerOutOfRange(pCurrChar);
+    }
 
     // friend status
     sSocialMgr.SendFriendStatus(pCurrChar, FRIEND_ONLINE, pCurrChar->GetObjectGuid(), true);
