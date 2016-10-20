88ead427738eeb7fa4131a959a00330af023a569
Cyberium <cybermomo@techemail.com>
2016-10-20 09:55:54 +0200

    Implement reviving pet even if its not spawned.
    Before it was impossible to resurect a dead pet when it was not spawned in map.
    Now if the hunter have the pet saved in db it will be possible to resurect it and summon it same time.
    
    Work inspired by @kilerwife work -> https://github.com/cmangos/mangos-tbc/pull/131/commits/f328de71cdeb9c8090ccbb30c5d609e997ee09b1ssibilit

--- src/game/Pet.h
+++ src/game/Pet.h
@@ -169,7 +169,7 @@ class MANGOS_DLL_SPEC Pet : public Creature
 
         bool Create(uint32 guidlow, CreatureCreatePos& cPos, CreatureInfo const* cinfo, uint32 pet_number);
         bool CreateBaseAtCreature(Creature* creature);
-        bool LoadPetFromDB(Player* owner, uint32 petentry = 0, uint32 petnumber = 0, bool current = false);
+        bool LoadPetFromDB(Player* owner, uint32 petentry = 0, uint32 petnumber = 0, bool current = false, uint32 healthPercentage = 0);
         void SavePetToDB(PetSaveMode mode);
         void Unsummon(PetSaveMode mode, Unit* owner = nullptr);
         static void DeleteFromDB(uint32 guidlow, bool separate_transaction = true);
