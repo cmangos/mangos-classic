4be2bbb548dc0981e33b769050540b222c961984
Phatcat <holsthein@live.dk>
2016-10-30 21:11:37 +0100

    Fix quest rewards appearing twice in chat
    ref: https://github.com/cmangos/issues/issues/1054
    and:
    https://github.com/cmangos/issues/issues/1045#issuecomment-242015294

--- src/game/Player.h
+++ src/game/Player.h
@@ -1098,7 +1098,7 @@ class MANGOS_DLL_SPEC Player : public Unit
             Item* mainItem = GetItemByPos(INVENTORY_SLOT_BAG_0, EQUIPMENT_SLOT_MAINHAND);
             return mainItem && mainItem->GetProto()->InventoryType == INVTYPE_2HWEAPON;
         }
-        void SendNewItem(Item* item, uint32 count, bool received, bool created, bool broadcast = false);
+        void SendNewItem(Item* item, uint32 count, bool received, bool created, bool broadcast = false, bool showInChat = true);
         bool BuyItemFromVendor(ObjectGuid vendorGuid, uint32 item, uint8 count, uint8 bag, uint8 slot);
 
         float GetReputationPriceDiscount(Creature const* pCreature) const;
