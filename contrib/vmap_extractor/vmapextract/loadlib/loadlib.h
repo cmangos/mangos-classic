7f15528cfc745f38ac23eb4041bb756bdd44234a
Carl Hjerpe <github@lillecarl.com>
2016-11-16 13:14:34 +0100

    Fix OSX building and implement Travis.
    Default Mac OpenSSL directory to home-brew installation path if nothing else is specified. (+9 squashed commits)
    Squashed commits:
    [6d2ce1c] Update CMakeLists.txt
    [263d9ba] Update loadlib.h
    
    Copied from CMaNGOS defs
    [eb0b9f9] Update loadlib.h
    [1f510c7] Update loadlib.h
    [2461169] Update .travis.yml
    [ecc939d] Update .travis.yml
    [b88d7b1] Update .travis.yml
    [3cd9472] Update .travis.yml
    [3f0b716] Update .travis.yml
    [ff219c0] Update .travis.yml

--- contrib/vmap_extractor/vmapextract/loadlib/loadlib.h
+++ contrib/vmap_extractor/vmapextract/loadlib/loadlib.h
@@ -19,31 +19,16 @@
 #ifndef LOAD_LIB_H
 #define LOAD_LIB_H
 
-#ifdef WIN32
-typedef __int64            int64;
-typedef __int32            int32;
-typedef __int16            int16;
-typedef __int8             int8;
-typedef unsigned __int64   uint64;
-typedef unsigned __int32   uint32;
-typedef unsigned __int16   uint16;
-typedef unsigned __int8    uint8;
-#else
-#include <stdint.h>
-#ifndef uint64_t
-  #ifndef _WIN32
-    #include <linux/types.h>
-  #endif
-#endif
-typedef int64_t            int64;
-typedef int32_t            int32;
-typedef int16_t            int16;
-typedef int8_t             int8;
-typedef uint64_t           uint64;
-typedef uint32_t           uint32;
-typedef uint16_t           uint16;
-typedef uint8_t            uint8;
-#endif
+#include <cstdint>
+
+typedef std::int64_t int64;
+typedef std::int32_t int32;
+typedef std::int16_t int16;
+typedef std::int8_t int8;
+typedef std::uint64_t uint64;
+typedef std::uint32_t uint32;
+typedef std::uint16_t uint16;
+typedef std::uint8_t uint8;
 
 #define FILE_FORMAT_VERSION    18
 
