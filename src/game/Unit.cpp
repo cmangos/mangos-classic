861c754659ac99592d802555a9706809e8b22827
robinsch <robin.schriever.hude@web.de>
2016-11-30 22:35:43 +0100

    Minimum speed rate is 0.01f
    Signed-off-by: robinsch <robin.schriever.hude@web.de>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7295,8 +7295,7 @@ struct SetSpeedRateHelper
 
 void Unit::SetSpeedRate(UnitMoveType mtype, float rate, bool forced)
 {
-    if (rate < 0)
-        rate = 0.0f;
+    rate = std::max(rate, 0.01f);
 
     // Update speed only on change
     if (m_speed_rate[mtype] != rate)
