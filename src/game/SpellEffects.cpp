829ed73fc55958b1cd580b9676f747eb748e8e51
killerwife <killerwife@gmail.com>
2016-12-28 11:51:41 +0100

    Remove redundant line

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -3390,9 +3390,6 @@ void Spell::EffectSummonObjectWild(SpellEffectIndex eff_idx)
 
     pGameObj->SummonLinkedTrapIfAny();
 
-    if (gameobject_id == 177746)
-        pGameObj->Use(m_caster);
-
     // Notify Summoner
     if (m_originalCaster && (m_originalCaster != m_caster) && (m_originalCaster->AI()))
         m_originalCaster->AI()->JustSummoned(pGameObj);
