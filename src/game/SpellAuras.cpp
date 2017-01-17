3baabe9e9ccfc2fdd6ff1264139b4f2b0f603b86
robinsch <robin.schriever.hude@web.de>
2017-01-17 17:27:07 +0100

    Fixed death bug when a player died while affected by Last Stance
    Fix https://github.com/cmangos/issues/issues/1238
    Fix https://github.com/cmangos/issues/issues/462
    close https://github.com/cmangos/mangos-classic/pull/248

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -3273,10 +3273,14 @@ void Aura::HandleAuraModIncreaseHealth(bool apply, bool Real)
                 }
                 else
                 {
-                    if (int32(target->GetHealth()) > m_modifier.m_amount)
-                        target->ModifyHealth(-m_modifier.m_amount);
-                    else
-                        target->SetHealth(1);
+                    if (m_removeMode != AURA_REMOVE_BY_DEATH)
+                    {
+                        if (int32(target->GetHealth()) > m_modifier.m_amount)
+                            target->ModifyHealth(-m_modifier.m_amount);
+                        else
+                            target->SetHealth(1);
+                    }
+
                     target->HandleStatModifier(UNIT_MOD_HEALTH, TOTAL_VALUE, float(m_modifier.m_amount), apply);
                 }
             }
