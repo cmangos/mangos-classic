010715c867969f212a1e648f8dc3ce2ebbe767e4
killerwife <killerwife@gmail.com>
2017-01-23 19:33:58 +0100

    Fix formatting

--- src/game/Spells/UnitAuraProcHandler.cpp
+++ src/game/Spells/UnitAuraProcHandler.cpp
@@ -297,15 +297,11 @@ bool Unit::IsTriggeredAtSpellProcEvent(Unit* pVictim, SpellAuraHolder* holder, S
     }
     // Apply chance modifier aura
     if (Player* modOwner = GetSpellModOwner())
-    {
         modOwner->ApplySpellMod(spellProto->Id, SPELLMOD_CHANCE_OF_SUCCESS, chance);
-    }
 
     if (procSpell && procSpell->HasAttribute(SPELL_ATTR_EX3_TRIGGERED_CAN_TRIGGER_SPECIAL))
-    {
         if (!spellProto->HasAttribute(SPELL_ATTR_EX3_CAN_PROC_FROM_TRIGGERED_SPECIAL))
             return false;
-    }
 
     return roll_chance_f(chance);
 }
