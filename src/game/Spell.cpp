cebd61b3211aa5c42d49002f744992f30dbf4bb6
Cyberium <cyberium@users.noreply.github.com>
2017-01-25 13:21:02 +0100

    Cancel global cooldown in all spell state before SPELL_STATE_PREPARING

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -2721,6 +2721,8 @@ void Spell::cancel()
     m_autoRepeat = false;
     switch (m_spellState)
     {
+        case SPELL_STATE_CREATED:
+        case SPELL_STATE_STARTING:
         case SPELL_STATE_PREPARING:
             CancelGlobalCooldown();
 
@@ -2752,9 +2754,7 @@ void Spell::cancel()
                 SendCastResult(SPELL_FAILED_INTERRUPTED);
         } break;
 
-        default:
-        {
-        } break;
+        case SPELL_STATE_FINISHED: break; // should not occur
     }
 
     finish(false);
