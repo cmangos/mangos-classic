ca30694d31e40bcefa4d525a774dce5ea38f2009
killerwife <killerwife@gmail.com>
2017-02-21 12:05:40 +0100

    Minor Warsong Gulch code consistency cleanup

--- src/game/BattleGround/BattleGroundWS.cpp
+++ src/game/BattleGround/BattleGroundWS.cpp
@@ -504,9 +504,9 @@ void BattleGroundWS::Reset()
     m_flagCarrierHorde.Clear();
 
     bool isBGWeekend = BattleGroundMgr::IsBGWeekend(GetTypeID());
-    m_ReputationCapture = (isBGWeekend) ? 25 : 15;
-    m_HonorWinKills = (isBGWeekend) ? 3 : 1;
-    m_HonorEndKills = (isBGWeekend) ? 4 : 2;
+    m_ReputationCapture = (isBGWeekend) ? WS_WEEKEND_FLAG_CAPTURE_REPUTATION : WS_NORMAL_FLAG_CAPTURE_REPUTATION;
+    m_HonorWinKills = (isBGWeekend) ? WS_WEEKEND_WIN_KILLS : WS_NORMAL_WIN_KILLS;
+    m_HonorEndKills = (isBGWeekend) ? WS_WEEKEND_MAP_COMPLETE_KILLS : WS_NORMAL_MAP_COMPLETE_KILLS;
 }
 
 void BattleGroundWS::EndBattleGround(Team winner)
