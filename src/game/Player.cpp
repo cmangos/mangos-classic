747183f8f9c5a4c08a4e4f43889c3d8c611f4406
Ono <warlockbugs@users.noreply.github.com>
2017-04-29 00:37:45 +0300

    Clear PVP option flag on logout
    Client no longer thinks that PvP flag is on when it clearly isn't.
    The flag itself is not used properly yet, to be improved in the future.

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -2515,7 +2515,7 @@ void Player::InitStatsForLevel(bool reapplyMods)
     SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PVP_ATTACKABLE);    // must be set
 
     // cleanup player flags (will be re-applied if need at aura load), to avoid have ghost flag without ghost aura, for example.
-    RemoveFlag(PLAYER_FLAGS, PLAYER_FLAGS_AFK | PLAYER_FLAGS_DND | PLAYER_FLAGS_GM | PLAYER_FLAGS_GHOST | PLAYER_FLAGS_FFA_PVP);
+    RemoveFlag(PLAYER_FLAGS, PLAYER_FLAGS_AFK | PLAYER_FLAGS_DND | PLAYER_FLAGS_GM | PLAYER_FLAGS_GHOST | PLAYER_FLAGS_IN_PVP | PLAYER_FLAGS_FFA_PVP);
 
     // one form stealth modified bytes
     RemoveByteFlag(UNIT_FIELD_BYTES_1, 3, UNIT_BYTE1_FLAG_ALL);
