9d8371929608f5e3691d51ed2ab17a384fdd74b8
Ono <warlockbugs@users.noreply.github.com>
2017-05-17 00:45:19 +0300

    Experimental MinGW support: Implemented
    Support for MinGW through CMake has arrived!
    
    Check out a new wiki page with notes about MinGW toolchains:
    https://github.com/cmangos/issues/wiki/Notes-on-MinGW-toolchains-for-developers

--- cmake/platform/win/settings.cmake
+++ cmake/platform/win/settings.cmake
@@ -1,3 +1,17 @@
 if ( MSVC )
   include("${ROOT_DIR}/cmake/compiler/msvc/settings.cmake")
+elseif ( MINGW )
+  include("${ROOT_DIR}/cmake/compiler/gcc/settings.cmake")
+  # On windows we generally want our binaries to be static
+  # Override default linker flags and we are good to go
+  set(CMAKE_EXE_LINKER_FLAGS "-static")
+  # remove -Wl,-Bdynamic
+  set(CMAKE_EXE_LINK_DYNAMIC_C_FLAGS)
+  set(CMAKE_EXE_LINK_DYNAMIC_CXX_FLAGS)
+  # remove -fPIC
+  set(CMAKE_SHARED_LIBRARY_C_FLAGS)
+  set(CMAKE_SHARED_LIBRARY_CXX_FLAGS)
+  # remove -rdynamic
+  set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS)
+  set(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS)
 endif()
