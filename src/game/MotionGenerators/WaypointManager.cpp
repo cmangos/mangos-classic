dd2c6a14aa559059cd9a764d1e3a3a28b5c47e02
killerwife <killerwife@gmail.com>
2017-05-17 19:06:44 +0200

    Fix debug log so it provides actual data

--- src/game/MotionGenerators/WaypointManager.cpp
+++ src/game/MotionGenerators/WaypointManager.cpp
@@ -234,7 +234,7 @@ void WaypointManager::Load()
                 ERROR_DB_STRICT_LOG("Table creature_movement has waypoint for creature guid %u (entry %u), but MovementType is not WAYPOINT_MOTION_TYPE(2). Make sure that this is actually used in a script!", *itr, cData->id);
 
                 if (cInfo->MovementType == WAYPOINT_MOTION_TYPE)
-                    sLog.outErrorDb("    creature_template for this entry has MovementType WAYPOINT_MOTION_TYPE(2), did you intend to use creature_movement_template ?");
+                    sLog.outErrorDb("Table creature_template for this entry(%u) guid(%u) has MovementType WAYPOINT_MOTION_TYPE(2), did you intend to use creature_movement_template ?", cData->id, *itr);
             }
         }
 
