9a7f85fc31a284ae5a898a9157e5604d76bb2c6f
Cala <calaftp@free.fr>
2017-05-30 09:18:19 +0200

    Playerbot: add cmake option for Playerbot build

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -6,6 +6,7 @@ option(BUILD_GAME_SERVER    "Build game server"                     ON)
 option(BUILD_LOGIN_SERVER   "Build login server"                    ON)
 option(BUILD_EXTRACTORS     "Build map/dbc/vmap/mmap extractors"    OFF)
 option(BUILD_SCRIPTDEV      "Build ScriptDev. (OFF Speedup build)"  ON)
+option(BUILD_PLAYERBOT      "Build Playerbot mod"                   OFF)
 
 # TODO: options that should be checked/created:
 #option(CLI                  "With CLI"                              ON)
@@ -27,6 +28,7 @@ message(STATUS
     BUILD_LOGIN_SERVER      Build login server (auth server)
     BUILD_EXTRACTORS        Build map/dbc/vmap/mmap extractor
     BUILD_SCRIPTDEV         Build scriptdev. (Disable it to speedup build in dev mode by not including scripts)
+    BUILD_PLAYERBOT         Build Playerbot mod
 
   To set an option simply type -D<OPTION>=<VALUE> after 'cmake <srcs>'.
   Also, you can specify the generator with -G. see 'cmake --help' for more details
