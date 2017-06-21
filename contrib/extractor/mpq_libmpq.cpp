cee9784eefa8c0f11482aa808ba8e8e1a002eebf
boxa <boxa@mail.ru>
2017-06-21 04:04:56 +0300

    Fix AppVeyour VC warning:'>': signed/unsigned mismatch

--- contrib/extractor/mpq_libmpq.cpp
+++ contrib/extractor/mpq_libmpq.cpp
@@ -99,7 +99,7 @@ size_t MPQFile::read(void* dest, size_t bytes)
 {
     if (eof) return 0;
 
-    size_t rpos = pointer + bytes;
+    libmpq__off_t rpos = pointer + bytes;
     if (rpos > size)
     {
         bytes = size - pointer;
