badeb0025a33cc201f7408379ec9d9ae82fac83a
Muehe <Muehe@users.noreply.github.com>
2017-06-24 00:01:22 +0200

    Fix ExtractResources script calling inexistant binary
    close https://github.com/cmangos/issues/issues/1382

--- contrib/extractor_scripts/ExtractResources.sh
+++ contrib/extractor_scripts/ExtractResources.sh
@@ -163,7 +163,7 @@ fi
 if [ "$USE_VMAPS" = "1" ]
 then
   echo "`date`: Start extraction of vmaps..." | tee -a $LOG_FILE
-  ./vmapExtractor | tee -a $DETAIL_LOG_FILE
+  ./vmap_extractor | tee -a $DETAIL_LOG_FILE
   echo "`date`: Extracting of vmaps finished" | tee -a $LOG_FILE
   mkdir vmaps
   echo "`date`: Start assembling of vmaps..." | tee -a $LOG_FILE
