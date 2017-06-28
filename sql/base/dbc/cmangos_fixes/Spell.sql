7916fe460b32851eadec1102aab5409a966665a0
killerwife <killerwife@gmail.com>
2017-06-28 13:21:27 +0200

    Remove AIF flag for NOT_SEATED from sleep spells Seems to be some sort of copypasted issue, only exists for 4 spells in wotlk and 3 in tbc/vanilla These spells dont use stand state for sitting but various other ones

--- sql/base/dbc/cmangos_fixes/Spell.sql
+++ sql/base/dbc/cmangos_fixes/Spell.sql
@@ -1,3 +1,7 @@
 -- Spell 24148 should have passive attribute. Without it, items with that spell enchant are declared as invalid when logging in a dead player
 -- Confirmed on later version of the spell
 UPDATE spell_template SET Attributes = Attributes | 0x040 WHERE Id = 24148;
+
+-- Removes aura interrupt flag by sitting for spells that do not make a creature sit, all seem to be copypasted for unknown reasons
+UPDATE spell_template SET AuraInterruptFlags=AuraInterruptFlags&~0x00040000 WHERE entry IN(6606,14915,16093);
+
