72b6848dd61265f33d80192f173680df7c8d731a
Ono <warlockbugs@users.noreply.github.com>
2017-06-30 23:49:38 +0300

    [z2700] Correct Paladin bonus coefficients for classic
    Source: https://web.archive.org/web/20070330234642/https://paladin-guide.blogspot.com/2005/12/stats.html

--- sql/base/mangos.sql
+++ sql/base/mangos.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
-  `required_z2699_01_mangos_spell_chain_totems` bit(1) DEFAULT NULL
+  `required_z2700_01_mangos_paladin_bonus_scaling` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Used DB version notes';
 
 --
@@ -10377,14 +10377,15 @@ INSERT INTO `spell_bonus_data` VALUES
 (116,   0.8143, 0,       0,     0,     'Mage - Frost Bolt'),
 (1463,  0.8053, 0,       0,     0,     'Mage - Mana Shield'),
 /* Paladin */
-(26573, 0,      0.04,    0,  0.04,     'Paladin - Consecration'),
-(879,   0.15,   0,       0.15,  0,     'Paladin - Exorcism'),
-(24275, 0.15,   0,       0.15,  0,     'Paladin - Hammer of Wrath'),
-(20925, 0.09,   0,       0.056, 0,     'Paladin - Holy Shield'),
-(2812,  0.07,   0,       0.07,  0,     'Paladin - Holy Wrath'),
-(20424, 0.25,   0,       0.16,  0,     'Paladin - Seal of Command Proc'),
-(20167, 0.25,   0,       0.16,  0,     'Paladin - Seal of Light Proc'),
-(25742, 0.07,   0,       0.039, 0,     'Paladin - Seal of Righteousness Dummy Proc'),
+(20911, 0,      0,       0,     0,     'Paladin - Blessing of Sanctuary'),
+(25997, 1,      0,       0,     0,     'Paladin - Eye for an Eye'),
+(25899, 0,      0,       0,     0,     'Paladin - Greater Blessing of Sanctuary'),
+(19968, 0.7143, 0,       0,     0,     'Paladin - Holy Light'),
+(20925, 0.05,   0,       0,     0,     'Paladin - Holy Shield'),
+(2812,  0.1905, 0,       0,     0,     'Paladin - Holy Wrath'),
+(20187, 0.5,    0,       0,     0,     'Paladin - Judgement of Righteousness'),
+(20424, 0.2,    0,       0,     0,     'Paladin - Seal of Command Proc'),
+(25742, 0.1,    0,       0,     0,     'Paladin - Seal of Righteousness Proc'),
 /* Priest */
 (2061,  0.6177, 0,       0,     0,     'Priest - Flash Heal'),
 (2060,  1.2353, 0,       0,     0,     'Priest - Greater Heal'),
@@ -11766,6 +11767,15 @@ INSERT INTO `spell_chain` VALUES
 /* Holy Wrath */
 (2812,0,2812,1,0),
 (10318,2812,2812,2,0),
+/* Judgement of Righteousness */
+(20187,0,20187,1,0),
+(20280,20187,20187,2,0),
+(20281,20280,20187,3,0),
+(20282,20281,20187,4,0),
+(20283,20282,20187,5,0),
+(20284,20283,20187,6,0),
+(20285,20284,20187,7,0),
+(20286,20285,20187,8,0),
 /* Redemption */
 (7328,0,7328,1,0),
 (10322,7328,7328,2,0),
@@ -11785,6 +11795,15 @@ INSERT INTO `spell_chain` VALUES
 (20291,20290,20154,7,0),
 (20292,20291,20154,8,0),
 (20293,20292,20154,9,0),
+/* Seal of Righteousness Proc */
+(25742,0,25742,1,0),
+(25740,25742,25742,2,0),
+(25739,25740,25742,3,0),
+(25738,25739,25742,4,0),
+(25737,25738,25742,5,0),
+(25736,25737,25742,6,0),
+(25735,25736,25742,7,0),
+(25713,25735,25742,8,0),
 /* Seal of Wisdom */
 (20166,0,20166,1,0),
 (20356,20166,20166,2,0),
