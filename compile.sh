2c35ef38524f9f6546bc936c42d817426eea0a2a
GUIDI MARIO <guidi.mario@gmail.com>
2017-07-06 15:08:11 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -10,7 +10,7 @@ if [ $CC = "gcc" ] ; then export CC=gcc-4.8 CXX=g++-4.8 ; fi
 mkdir _build
 mkdir _install
 cd _build
-cmake -DCMAKE_INSTALL_PREFIX=../_install -BUILD_PLAYERBOT=OFF -DBUILD_SCRIPTDEV=ON -DBUILD_GAME_SERVER=ON -BUILD_LOGIN_SERVER=ON -DPCH=OFF ..
+cmake -DCMAKE_INSTALL_PREFIX=../_install -BUILD_PLAYERBOT=OFF -DBUILD_SCRIPTDEV=OFF -DBUILD_GAME_SERVER=OFF -BUILD_LOGIN_SERVER=ON -DPCH=OFF ..
 make
 make install
 
