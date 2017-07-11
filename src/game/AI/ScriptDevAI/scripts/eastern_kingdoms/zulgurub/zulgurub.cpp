1ce9c03ced61c07530855bf0104ee796030cfebc
oppahansi <oppahansi@gmail.com>
2017-07-11 22:05:14 +0200

    Add Soulflayer AI
    During the research we found out, that the NPC Soulflayer 11359 has randomized abilities / ability sets.
    This conclusion comes from comparing several old sources with each other:
    
    Bradygames Bestiary: http://prntscr.com/frqhgp
    Bradygames Dungeon Companion: http://prntscr.com/frqhw5
    wowguru: http://web.archive.org/web/20060510172842/http://www.wowguru.com:80/db/mobs/soulflayer-id11359
    allakhazam: https://web.archive.org/web/20080421224028/http://wow.allakhazam.com:80/db/mob.html?wmob=11359
    allakhazam2: https://web.archive.org/web/20100807192134/http://wow.allakhazam.com:80/db/mob.html?wmob=11359
    wowhead: http://web.archive.org/web/20100423035459/http://www.wowhead.com:80/npc=11359
    
    As you can see the difference in abilities listed on each source compared to each other, aligns with the statement from Bradygame Dungeon Compaion source.
    This was the base for our conclusion.
    
    So we picked 7 abilities and randomized them on spawn:
    2 default abilities, Soul Tap and Lightning Breath,
    1 random CC ability, Fear or Knockdown
    1 random buff ability, Thrash, Frenzy or Enrage
    
    This means Soulflayer will have 4 abilities in total in each fight, from which if rolled Thrash is a passive one.
    
    Our timers are wildly guessed and only tested on a local server:
    What felt right and what would fit the descriptions in guides / sources.
    Unfortunately we couldnt find anything more related to that.

--- src/game/AI/ScriptDevAI/scripts/eastern_kingdoms/zulgurub/zulgurub.cpp
+++ src/game/AI/ScriptDevAI/scripts/eastern_kingdoms/zulgurub/zulgurub.cpp
@@ -26,6 +26,119 @@ EndScriptData */
  /* ContentData
 EndContentData */
 
+enum
+{
+    // npc_soulflayer
+    SPELL_SOUL_TAP               = 24619,
+    SPELL_LIGHTNING_BREATH       = 20543,
+    SPELL_THRASH                 = 12787,
+    SPELL_KNOCKDOWN              = 20276,
+    SPELL_FEAR                   = 22678,
+    SPELL_FRENZY                 = 28371,
+    SPELL_ENRAGE                 = 8269
+};
+
+/*######
+## npc_soulflayer
+######*/
+
+struct npc_soulflayerAI : public ScriptedAI
+{
+    npc_soulflayerAI(Creature* pCreature) : ScriptedAI(pCreature) { Reset(); }
+
+    uint32 m_uiSoulTapTimer;
+    uint32 m_uiLightingBreathTimer;
+    uint32 m_uiRandomCcAbility;
+    uint32 m_uiRandomCcAbilityTimer;
+    uint32 m_uiRandomBuffAbility;
+
+    const uint32 spell_list_cc[2] = { SPELL_FEAR, SPELL_KNOCKDOWN };
+    const uint32 spell_list_buff[3] = { SPELL_ENRAGE, SPELL_FRENZY, SPELL_THRASH };
+
+    void JustRespawned() override
+    {
+        // Set random time based CC ability on spawn (Fear or Knockdown)
+        m_uiRandomCcAbility = spell_list_cc[urand(0, 1)];
+
+        // Set a random buff HP based ability (Frenzy or Enrage) or Thrash on spawn
+        m_uiRandomBuffAbility = spell_list_buff[urand(0, 2)];
+
+        // Cast Thrash on spawn, since a passive ability
+        if (m_uiRandomBuffAbility == SPELL_THRASH)
+            DoCastSpellIfCan(m_creature, m_uiRandomBuffAbility);
+    }
+
+    void Reset() override
+    {
+        m_uiRandomCcAbilityTimer = urand(2000, 5000);
+        m_uiSoulTapTimer = urand(m_uiRandomCcAbilityTimer, 7000);
+        m_uiLightingBreathTimer = urand(m_uiSoulTapTimer, 9000);
+    }
+
+    void UpdateAI(const uint32 uiDiff) override
+    {
+        // Check if we have a valid target, otherwise do nothing
+        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
+            return;
+
+        // Check if we are casting / channelling, return to not interrupt process and pause CDs
+        if (m_creature->IsNonMeleeSpellCasted(false))
+            return;
+
+        // Soul Tap
+        if (m_uiSoulTapTimer < uiDiff)
+        {
+            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0, SPELL_SOUL_TAP, SELECT_TARGET_ANY_ENEMY))
+                if (DoCastSpellIfCan(pTarget, SPELL_SOUL_TAP) == CAST_OK)
+                    m_uiSoulTapTimer = urand(10000, 15000);
+        }
+        else
+            m_uiSoulTapTimer -= uiDiff;
+
+        // Lightning Breath
+        if (m_uiLightingBreathTimer < uiDiff)
+        {
+            if (DoCastSpellIfCan(m_creature->getVictim(), SPELL_LIGHTNING_BREATH) == CAST_OK)
+                m_uiLightingBreathTimer = urand(10000, 15000);
+        }
+        else
+            m_uiLightingBreathTimer -= uiDiff;
+
+        // Random time based CC ability
+        // Either Fear or Knockdown
+        if (m_uiRandomCcAbilityTimer < uiDiff)
+        {
+            if (DoCastSpellIfCan(m_creature->getVictim(), m_uiRandomCcAbility) == CAST_OK)
+            {
+                m_creature->getVictim()->DeleteThreatList();
+                m_uiRandomCcAbilityTimer = urand(8000, 10000);
+            }
+        }
+        else
+            m_uiRandomCcAbilityTimer -= uiDiff;
+
+        // Random HP based ability
+        // Either Frenzy or Enrage
+        if (m_uiRandomBuffAbility != SPELL_THRASH)
+            if (m_creature->GetHealthPercent() <= 30.0f)
+                if (!m_creature->HasAura(m_uiRandomBuffAbility))
+                    DoCastSpellIfCan(m_creature->getVictim(), m_uiRandomBuffAbility);
+
+        DoMeleeAttackIfReady();
+    }
+};
+
+CreatureAI* GetAI_npc_soulflayer(Creature* pCreature)
+{
+    return new npc_soulflayerAI(pCreature);
+}
+
 void AddSC_zulgurub()
 {
+    Script* pNewScript;
+
+    pNewScript = new Script();
+    pNewScript->Name = "npc_soulflayer";
+    pNewScript->GetAI = &GetAI_npc_soulflayer;
+    pNewScript->RegisterSelf();
 }
