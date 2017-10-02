cmangos/mangos-classic

commit 8c097572aa76891367ea6e4f94ba4f60b5ac7f48

[z2710] Add questgiver_greeting and locales to properly handle SMSG_QUESTGIVER_QUEST_LIST
TODO: Remove npc_gossip

src/game/Globals/ObjectMgr.cpp
@@ -4511,6 +4511,145 @@ void ObjectMgr::LoadGossipTextLocales()
     sLog.outString();
 }
 
+QuestgiverGreeting const* ObjectMgr::GetQuestgiverGreetingData(uint32 entry, uint32 type) const
+{
+    auto itr = m_questgiverGreetingMap[type].find(entry);
+    if (itr == m_questgiverGreetingMap[type].end()) return nullptr;
+    else return &itr->second;
+}
+
+void ObjectMgr::LoadQuestgiverGreeting()
+{
+    for (uint32 i = 0; i < QUESTGIVER_TYPE_MAX; i++) // Reload Case
+        m_questgiverGreetingMap[i].clear();
+
+    QueryResult* result = WorldDatabase.Query("SELECT Entry, Type, Text, EmoteId, EmoteDelay FROM questgiver_greeting");
+    int count = 0;
+    if (!result)
+    {
+        BarGoLink bar(1);
+        bar.step();
+
+        sLog.outString(">> Loaded %u questgiver greetings", count);
+        sLog.outString();
+        return;
+    }
+
+    BarGoLink bar(result->GetRowCount());
+
+    do
+    {
+        Field* fields = result->Fetch();
+        bar.step();
+
+        uint32 entry = fields[0].GetUInt32();
+        uint32 type = fields[1].GetUInt32();
+        uint32 emoteId = fields[3].GetUInt32();
+
+        switch (type)
+        {
+            case QUESTGIVER_CREATURE:
+                if (!sCreatureStorage.LookupEntry<CreatureInfo>(entry))
+                {
+                    sLog.outErrorEventAI("Table questgiver_greeting uses nonexistent creature entry %u. Skipping.", entry);
+                    continue;
+                }
+                break;
+            case QUESTGIVER_GAMEOBJECT:
+                if (!sGOStorage.LookupEntry<GameObjectInfo>(entry))
+                {
+                    sLog.outErrorEventAI("Table questgiver_greeting uses nonexistent gameobject entry %u. Skipping.", entry);
+                    continue;
+                }
+                break;
+            default:
+                sLog.outErrorEventAI("Table questgiver_greeting uses questgiver type %u. Skipping.", type);
+                continue;
+        }
+
+        if (!sEmotesStore.LookupEntry(emoteId))
+        {
+            sLog.outErrorEventAI("Table questgiver_greeting entry %u type %u uses invalid emote %u. Skipping.", entry, type, emoteId);
+            continue;
+        }
+
+        QuestgiverGreeting& var = m_questgiverGreetingMap[type][entry];
+        var.text = fields[2].GetString();
+        var.emoteId = emoteId;
+        var.emoteDelay = fields[4].GetUInt32();
+    }
+    while (result->NextRow());
+
+    delete result;
+
+    sLog.outString(">> Loaded " SIZEFMTD " questgiver greetings.", mNpcTextLocaleMap.size());
+    sLog.outString();
+}
+
+void ObjectMgr::LoadQuestgiverGreetingLocales()
+{
+    mNpcTextLocaleMap.clear();                              // need for reload case
+
+    QueryResult* result = WorldDatabase.Query("SELECT Entry, Type, Text_loc1, Text_loc2, Text_loc3, Text_loc4, Text_loc5, Text_loc6, Text_loc7, Text_loc8 FROM locales_questgiver_greeting");
+    int count = 0;
+
+    if (!result)
+    {
+        BarGoLink bar(1);
+        bar.step();
+        sLog.outString(">> Loaded 0 locales questgiver greetings");
+        return;
+    }
+
+    BarGoLink bar(result->GetRowCount());
+
+    do
+    {
+        Field* fields = result->Fetch();
+        bar.step();
+
+        uint32 entry = fields[0].GetUInt32();
+        uint32 type = fields[1].GetUInt32();
+
+        switch (type)
+        {
+            case QUESTGIVER_CREATURE:
+                if (!sCreatureStorage.LookupEntry<CreatureInfo>(entry))
+                {
+                    sLog.outErrorEventAI("Table questgiver_greeting uses nonexistent creature entry %u. Skipping.", entry);
+                    continue;
+                }
+                break;
+            case QUESTGIVER_GAMEOBJECT:
+                if (!sGOStorage.LookupEntry<GameObjectInfo>(entry))
+                {
+                    sLog.outErrorEventAI("Table questgiver_greeting uses nonexistent gameobject entry %u. Skipping.", entry);
+                    continue;
+                }
+                break;
+            default:
+                sLog.outErrorEventAI("Table questgiver_greeting uses questgiver type %u. Skipping.", type);
+                continue;
+        }
+
+        QuestgiverGreetingLocale& var = m_questgiverGreetingLocaleMap[type][entry];
+
+        for (int i = 1; i < MAX_LOCALE; ++i)
+        {
+            if (const char* text = fields[1 + i].GetString())
+                var.localeText.push_back(text);
+            else
+                var.localeText.push_back("");
+        }
+    }
+    while (result->NextRow());
+
+    delete result;
+
+    sLog.outString(">> Loaded " SIZEFMTD " locales questgiver greetings.", mNpcTextLocaleMap.size());
+    sLog.outString();
+}
+
 // not very fast function but it is called only once a day, or on starting-up
 /// @param serverUp true if the server is already running, false when the server is started
 void ObjectMgr::ReturnOrDeleteOldMails(bool serverUp)
@@ -8740,6 +8879,19 @@ void ObjectMgr::GetNpcTextLocaleStrings0(uint32 entry, int32 loc_idx, std::strin
             if (text1_0_Ptr)
                 if (nl->Text_1[0].size() > (size_t)loc_idx && !nl->Text_1[0][loc_idx].empty())
                     *text1_0_Ptr = nl->Text_1[0][loc_idx];
+        }
+    }
+}
+
+void ObjectMgr::GetQuestgiverGreetingLocales(uint32 entry, uint32 type, int32 loc_idx, std::string* titlePtr) const
+{
+    if (loc_idx >= 0)
+    {
+        if (QuestgiverGreetingLocale const* ql = GetQuestgiverGreetingLocale(entry, type))
+        {
+            if (titlePtr)
+                if (ql->localeText.size() > (size_t)loc_idx && !ql->localeText[loc_idx].empty())
+                    *titlePtr = ql->localeText[loc_idx];
         }
     }
 }

sql/base/mangos.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
-  `required_z2709_01_mangos_comments` bit(1) DEFAULT NULL
+  `required_z2710_01_mangos_questgiver_greeting` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Used DB version notes';
 
 --
@@ -3143,6 +3143,24 @@ LOCK TABLES `locales_quest` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `locales_questgiver_greeting`
+--
+
+DROP TABLE IF EXISTS `locales_questgiver_greeting`;
+CREATE TABLE `locales_questgiver_greeting` (
+   `Entry` INT(11) UNSIGNED NOT NULL COMMENT 'Entry of Questgiver',
+   `Type` INT(11) UNSIGNED NOT NULL COMMENT 'Type of entry',
+   `Text_loc1` LONGTEXT COMMENT 'Text of the greeting locale 1',
+   `Text_loc2` LONGTEXT COMMENT 'Text of the greeting locale 2',
+   `Text_loc3` LONGTEXT COMMENT 'Text of the greeting locale 3',
+   `Text_loc4` LONGTEXT COMMENT 'Text of the greeting locale 4',
+   `Text_loc5` LONGTEXT COMMENT 'Text of the greeting locale 5',
+   `Text_loc6` LONGTEXT COMMENT 'Text of the greeting locale 6',
+   `Text_loc7` LONGTEXT COMMENT 'Text of the greeting locale 7',
+   `Text_loc8` LONGTEXT COMMENT 'Text of the greeting locale 8',
+   PRIMARY KEY(`Entry`,`Type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='Quest and Gossip system';
+
 -- Table structure for table `mail_loot_template`
 --
 
@@ -9814,6 +9832,20 @@ LOCK TABLES `quest_template` WRITE;
 /*!40000 ALTER TABLE `quest_template` DISABLE KEYS */;
 /*!40000 ALTER TABLE `quest_template` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `questgiver_greeting`
+--
+
+DROP TABLE IF EXISTS `questgiver_greeting`;
+CREATE TABLE `questgiver_greeting` (
+   `Entry` INT(11) UNSIGNED NOT NULL COMMENT 'Entry of Questgiver',
+   `Type` INT(11) UNSIGNED NOT NULL COMMENT 'Type of entry',
+   `Text` LONGTEXT COMMENT 'Text of the greeting',
+   `EmoteId` INT(11) UNSIGNED NOT NULL COMMENT 'Emote ID of greeting',
+   `EmoteDelay` INT(11) UNSIGNED NOT NULL COMMENT 'Emote delay of the greeting',
+   PRIMARY KEY(`Entry`,`Type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='Quest and Gossip system';
 
 --
 -- Table structure for table `reference_loot_template`

sql/updates/mangos/z2710_01_mangos_questgiver_greeting.sql
@@ -1,0 +1,28 @@
+ALTER TABLE db_version CHANGE COLUMN required_z2709_01_mangos_comments required_z2710_01_mangos_questgiver_greeting bit;
+
+DROP TABLE IF EXISTS `questgiver_greeting`;
+CREATE TABLE `questgiver_greeting` (
+   `Entry` INT(11) UNSIGNED NOT NULL COMMENT 'Entry of Questgiver',
+   `Type` INT(11) UNSIGNED NOT NULL COMMENT 'Type of entry',
+   `Text` LONGTEXT COMMENT 'Text of the greeting',
+   `EmoteId` INT(11) UNSIGNED NOT NULL COMMENT 'Emote ID of greeting',
+   `EmoteDelay` INT(11) UNSIGNED NOT NULL COMMENT 'Emote delay of the greeting',
+   PRIMARY KEY(`Entry`,`Type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='Quest and Gossip system';
+
+DROP TABLE IF EXISTS `locales_questgiver_greeting`;
+CREATE TABLE `locales_questgiver_greeting` (
+   `Entry` INT(11) UNSIGNED NOT NULL COMMENT 'Entry of Questgiver',
+   `Type` INT(11) UNSIGNED NOT NULL COMMENT 'Type of entry',
+   `Text_loc1` LONGTEXT COMMENT 'Text of the greeting locale 1',
+   `Text_loc2` LONGTEXT COMMENT 'Text of the greeting locale 2',
+   `Text_loc3` LONGTEXT COMMENT 'Text of the greeting locale 3',
+   `Text_loc4` LONGTEXT COMMENT 'Text of the greeting locale 4',
+   `Text_loc5` LONGTEXT COMMENT 'Text of the greeting locale 5',
+   `Text_loc6` LONGTEXT COMMENT 'Text of the greeting locale 6',
+   `Text_loc7` LONGTEXT COMMENT 'Text of the greeting locale 7',
+   `Text_loc8` LONGTEXT COMMENT 'Text of the greeting locale 8',
+   PRIMARY KEY(`Entry`,`Type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='Quest and Gossip system';
+
+

src/game/Chat/Chat.cpp
@@ -546,6 +546,7 @@ ChatCommand* ChatHandler::getCommandTable()
         { "locales_page_text",           SEC_ADMINISTRATOR, true,  &ChatHandler::HandleReloadLocalesPageTextCommand,         "", nullptr },
         { "locales_points_of_interest",  SEC_ADMINISTRATOR, true,  &ChatHandler::HandleReloadLocalesPointsOfInterestCommand, "", nullptr },
         { "locales_quest",               SEC_ADMINISTRATOR, true,  &ChatHandler::HandleReloadLocalesQuestCommand,            "", nullptr },
+        { "locales_questgiver_greeting", SEC_ADMINISTRATOR, true,  &ChatHandler::HandleReloadQuestgiverGreetingLocalesCommand, "", nullptr },
         { "mail_loot_template",          SEC_ADMINISTRATOR, true,  &ChatHandler::HandleReloadLootTemplatesMailCommand,       "", nullptr },
         { "mangos_string",               SEC_ADMINISTRATOR, true,  &ChatHandler::HandleReloadMangosStringCommand,            "", nullptr },
         { "npc_gossip",                  SEC_ADMINISTRATOR, true,  &ChatHandler::HandleReloadNpcGossipCommand,               "", nullptr },
@@ -555,6 +556,7 @@ ChatCommand* ChatHandler::getCommandTable()
         { "page_text",                   SEC_ADMINISTRATOR, true,  &ChatHandler::HandleReloadPageTextsCommand,               "", nullptr },
         { "pickpocketing_loot_template", SEC_ADMINISTRATOR, true,  &ChatHandler::HandleReloadLootTemplatesPickpocketingCommand, "", nullptr},
         { "points_of_interest",          SEC_ADMINISTRATOR, true,  &ChatHandler::HandleReloadPointsOfInterestCommand,        "", nullptr },
+        { "questgiver_greeting",         SEC_ADMINISTRATOR, true,  &ChatHandler::HandleReloadQuestgiverGreetingCommand,      "", nullptr },
         { "quest_template",              SEC_ADMINISTRATOR, true,  &ChatHandler::HandleReloadQuestTemplateCommand,           "", nullptr },
         { "reference_loot_template",     SEC_ADMINISTRATOR, true,  &ChatHandler::HandleReloadLootTemplatesReferenceCommand,  "", nullptr },
         { "reserved_name",               SEC_ADMINISTRATOR, true,  &ChatHandler::HandleReloadReservedNameCommand,            "", nullptr },

src/game/Chat/Chat.h
@@ -419,6 +419,7 @@ class ChatHandler
         bool HandleReloadGameGraveyardZoneCommand(char* args);
         bool HandleReloadGameTeleCommand(char* args);
         bool HandleReloadGossipMenuCommand(char* args);
+        bool HandleReloadQuestgiverGreetingCommand(char* args);
         bool HandleReloadGOQuestRelationsCommand(char* args);
         bool HandleReloadGOQuestInvRelationsCommand(char* args);
         bool HandleReloadItemEnchantementsCommand(char* args);
@@ -431,6 +432,7 @@ class ChatHandler
         bool HandleReloadLocalesPageTextCommand(char* args);
         bool HandleReloadLocalesPointsOfInterestCommand(char* args);
         bool HandleReloadLocalesQuestCommand(char* args);
+        bool HandleReloadQuestgiverGreetingLocalesCommand(char* args);
         bool HandleReloadLootTemplatesCreatureCommand(char* args);
         bool HandleReloadLootTemplatesDisenchantCommand(char* args);
         bool HandleReloadLootTemplatesFishingCommand(char* args);

src/game/Chat/Level3.cpp
@@ -432,6 +432,20 @@ bool ChatHandler::HandleReloadGossipMenuCommand(char* /*args*/)
     return true;
 }
 
+bool ChatHandler::HandleReloadQuestgiverGreetingCommand(char* /*args*/)
+{
+    sObjectMgr.LoadQuestgiverGreeting();
+    SendGlobalSysMessage("DB table `questgiver_greeting` reloaded.");
+    return true;
+}
+
+bool ChatHandler::HandleReloadQuestgiverGreetingLocalesCommand(char* /*args*/)
+{
+    sObjectMgr.LoadQuestgiverGreetingLocales();
+    SendGlobalSysMessage("DB table `locales_questgiver_greeting` reloaded.");
+    return true;
+}
+
 bool ChatHandler::HandleReloadGOQuestRelationsCommand(char* /*args*/)
 {
     sLog.outString("Loading Quests Relations... (`gameobject_questrelation`)");

src/game/Entities/Player.cpp
@@ -11474,60 +11474,79 @@ void Player::SendPreparedQuest(ObjectGuid guid) const
 
     uint32 status = qmi0.m_qIcon;
 
-    // single element case
-    if (questMenu.MenuItemCount() == 1)
-    {
-        // Auto open -- maybe also should verify there is no greeting
-        uint32 quest_id = qmi0.m_qId;
-        Quest const* pQuest = sObjectMgr.GetQuestTemplate(quest_id);
+    uint32 type;
+    if (guid.IsCreature())
+        type = QUESTGIVER_CREATURE;
+    else if (guid.IsGameObject())
+        type = QUESTGIVER_GAMEOBJECT;
 
-        if (pQuest)
-        {
-            if (status == DIALOG_STATUS_REWARD_REP && !GetQuestRewardStatus(quest_id))
-                PlayerTalkClass->SendQuestGiverRequestItems(pQuest, guid, CanRewardQuest(pQuest, false), true);
-            else if (status == DIALOG_STATUS_INCOMPLETE)
-                PlayerTalkClass->SendQuestGiverRequestItems(pQuest, guid, false, true);
-            // Send completable on repeatable quest if player don't have quest
-            else if (pQuest->IsRepeatable())
-                PlayerTalkClass->SendQuestGiverRequestItems(pQuest, guid, CanCompleteRepeatableQuest(pQuest), true);
-            else
-                PlayerTalkClass->SendQuestGiverQuestDetails(pQuest, guid, true);
-        }
-    }
-    // multiply entries
-    else
+    if (QuestgiverGreeting const* data = sObjectMgr.GetQuestgiverGreetingData(guid.GetEntry(), type))
     {
         QEmote qe;
-        qe._Delay = 0;
-        qe._Emote = 0;
-        std::string title = "";
-
-        // need pet case for some quests
-        if (Creature* pCreature = GetMap()->GetAnyTypeCreature(guid))
+        qe._Delay = data->emoteDelay;
+        qe._Emote = data->emoteId;
+        std::string title = data->text;
+        int loc_idx = GetSession()->GetSessionDbLocaleIndex();
+        sObjectMgr.GetQuestgiverGreetingLocales(guid.GetEntry(), type, loc_idx, &title);
+        PlayerTalkClass->SendQuestGiverQuestList(qe, title, guid);
+    }
+    else
+    {
+        // single element case
+        if (questMenu.MenuItemCount() == 1)
         {
-            uint32 textid = GetGossipTextId(pCreature);
+            // Auto open -- maybe also should verify there is no greeting
+            uint32 quest_id = qmi0.m_qId;
+            Quest const* pQuest = sObjectMgr.GetQuestTemplate(quest_id);
 
-            GossipText const* gossiptext = sObjectMgr.GetGossipText(textid);
-            if (!gossiptext)
+            if (pQuest)
             {
-                qe._Delay = 0;                              // TEXTEMOTE_MESSAGE;              // zyg: player emote
-                qe._Emote = 0;                              // TEXTEMOTE_HELLO;                // zyg: NPC emote
-                title.clear();
-            }
-            else
-            {
-                qe = gossiptext->Options[0].Emotes[0];
-
-                int loc_idx = GetSession()->GetSessionDbLocaleIndex();
-
-                std::string title0 = gossiptext->Options[0].Text_0;
-                std::string title1 = gossiptext->Options[0].Text_1;
-                sObjectMgr.GetNpcTextLocaleStrings0(textid, loc_idx, &title0, &title1);
-
-                title = !title0.empty() ? title0 : title1;
+                if (status == DIALOG_STATUS_REWARD_REP && !GetQuestRewardStatus(quest_id))
+                    PlayerTalkClass->SendQuestGiverRequestItems(pQuest, guid, CanRewardQuest(pQuest, false), true);
+                else if (status == DIALOG_STATUS_INCOMPLETE)
+                    PlayerTalkClass->SendQuestGiverRequestItems(pQuest, guid, false, true);
+                // Send completable on repeatable quest if player don't have quest
+                else if (pQuest->IsRepeatable())
+                    PlayerTalkClass->SendQuestGiverRequestItems(pQuest, guid, CanCompleteRepeatableQuest(pQuest), true);
+                else
+                    PlayerTalkClass->SendQuestGiverQuestDetails(pQuest, guid, true);
             }
         }
-        PlayerTalkClass->SendQuestGiverQuestList(qe, title, guid);
+        // multiply entries
+        else
+        {
+            QEmote qe;
+            qe._Delay = 0;
+            qe._Emote = 0;
+            std::string title = "";
+
+            // need pet case for some quests
+            if (Creature* pCreature = GetMap()->GetAnyTypeCreature(guid))
+            {
+                uint32 textid = GetGossipTextId(pCreature);
+
+                GossipText const* gossiptext = sObjectMgr.GetGossipText(textid);
+                if (!gossiptext)
+                {
+                    qe._Delay = 0;                              // TEXTEMOTE_MESSAGE;              // zyg: player emote
+                    qe._Emote = 0;                              // TEXTEMOTE_HELLO;                // zyg: NPC emote
+                    title.clear();
+                }
+                else
+                {
+                    qe = gossiptext->Options[0].Emotes[0];
+
+                    int loc_idx = GetSession()->GetSessionDbLocaleIndex();
+
+                    std::string title0 = gossiptext->Options[0].Text_0;
+                    std::string title1 = gossiptext->Options[0].Text_1;
+                    sObjectMgr.GetNpcTextLocaleStrings0(textid, loc_idx, &title0, &title1);
+
+                    title = !title0.empty() ? title0 : title1;
+                }
+            }
+            PlayerTalkClass->SendQuestGiverQuestList(qe, title, guid);
+        }
     }
 }
 

src/game/Globals/ObjectMgr.h
@@ -278,6 +278,28 @@ struct GraveYardData
 typedef std::multimap < uint32 /*zoneId*/, GraveYardData > GraveYardMap;
 typedef std::pair<GraveYardMap::const_iterator, GraveYardMap::const_iterator> GraveYardMapBounds;
 
+struct QuestgiverGreeting
+{
+    std::string text;
+    uint32 emoteId;
+    uint32 emoteDelay;
+};
+
+struct QuestgiverGreetingLocale
+{
+    std::vector<std::string> localeText;
+};
+
+typedef std::map<uint32, QuestgiverGreeting> QuestgiverGreetingMap;
+typedef std::map<uint32, QuestgiverGreetingLocale> QuestgiverGreetingLocaleMap;
+
+enum
+{
+    QUESTGIVER_CREATURE = 0,
+    QUESTGIVER_GAMEOBJECT = 1,
+    QUESTGIVER_TYPE_MAX = 2,
+};
+
 enum ConditionType
 {
     //                                                      // value1       value2  for the Condition enumed
@@ -547,6 +569,8 @@ class ObjectMgr
 
         GossipText const* GetGossipText(uint32 Text_ID) const;
 
+        QuestgiverGreeting const* GetQuestgiverGreetingData(uint32 entry, uint32 type) const;
+
         WorldSafeLocsEntry const* GetClosestGraveYard(float x, float y, float z, uint32 MapId, Team team);
         bool AddGraveYardLink(uint32 id, uint32 zone, Team team, bool inDB = true);
         void SetGraveYardLinkTeam(uint32 id, uint32 zoneId, Team team);
@@ -650,6 +674,8 @@ class ObjectMgr
         void LoadItemLocales();
         void LoadQuestLocales();
         void LoadGossipTextLocales();
+        void LoadQuestgiverGreeting();
+        void LoadQuestgiverGreetingLocales();
         void LoadPageTextLocales();
         void LoadGossipMenuItemsLocales();
         void LoadPointOfInterestLocales();
@@ -824,6 +850,15 @@ class ObjectMgr
         typedef std::string NpcTextArray[MAX_GOSSIP_TEXT_OPTIONS];
         void GetNpcTextLocaleStringsAll(uint32 entry, int32 loc_idx, NpcTextArray* text0_Ptr, NpcTextArray* text1_Ptr) const;
         void GetNpcTextLocaleStrings0(uint32 entry, int32 loc_idx, std::string* text0_0_Ptr, std::string* text1_0_Ptr) const;
+
+        void GetQuestgiverGreetingLocales(uint32 entry, uint32 type, int32 loc_idx, std::string* titlePtr) const;
+
+        QuestgiverGreetingLocale const* GetQuestgiverGreetingLocale(uint32 entry, uint32 type) const
+        {
+            auto itr = m_questgiverGreetingLocaleMap[type].find(entry);
+            if (itr == m_questgiverGreetingLocaleMap[type].end()) return nullptr;
+            return &itr->second;
+        }
 
         PageTextLocale const* GetPageTextLocale(uint32 entry) const
         {
@@ -1171,6 +1206,9 @@ class ObjectMgr
         PointOfInterestLocaleMap mPointOfInterestLocaleMap;
         DungeonEncounterMap m_DungeonEncounters;
 
+        QuestgiverGreetingMap m_questgiverGreetingMap[QUESTGIVER_TYPE_MAX];
+        QuestgiverGreetingLocaleMap m_questgiverGreetingLocaleMap[QUESTGIVER_TYPE_MAX];
+        
         CacheNpcTextIdMap m_mCacheNpcTextIdMap;
         CacheVendorItemMap m_mCacheVendorTemplateItemMap;
         CacheVendorItemMap m_mCacheVendorItemMap;

commit 3d0d273bb686aa67f1bb2169a7efa3e1bc3d3358

Fix NPC text localization
NPC text locale strings were being erased in ObjectMgr::LoadQuestgiverGreeting due to a typo introduced in 8c09757.
This bug was spotted by @M2us.

src/game/Globals/ObjectMgr.cpp
@@ -4577,18 +4577,21 @@ void ObjectMgr::LoadQuestgiverGreeting()
         var.text = fields[2].GetString();
         var.emoteId = emoteId;
         var.emoteDelay = fields[4].GetUInt32();
+
+        ++count;
     }
     while (result->NextRow());
 
     delete result;
 
-    sLog.outString(">> Loaded " SIZEFMTD " questgiver greetings.", mNpcTextLocaleMap.size());
+    sLog.outString(">> Loaded %u questgiver greetings.", count);
     sLog.outString();
 }
 
 void ObjectMgr::LoadQuestgiverGreetingLocales()
 {
-    mNpcTextLocaleMap.clear();                              // need for reload case
+    for (uint32 i = 0; i < QUESTGIVER_TYPE_MAX; i++)        // need for reload case
+        m_questgiverGreetingLocaleMap[i].clear();
 
     QueryResult* result = WorldDatabase.Query("SELECT Entry, Type, Text_loc1, Text_loc2, Text_loc3, Text_loc4, Text_loc5, Text_loc6, Text_loc7, Text_loc8 FROM locales_questgiver_greeting");
     int count = 0;
@@ -4641,12 +4644,14 @@ void ObjectMgr::LoadQuestgiverGreetingLocales()
             else
                 var.localeText.push_back("");
         }
+
+        ++count;
     }
     while (result->NextRow());
 
     delete result;
 
-    sLog.outString(">> Loaded " SIZEFMTD " locales questgiver greetings.", mNpcTextLocaleMap.size());
+    sLog.outString(">> Loaded %u locales questgiver greetings.", count);
     sLog.outString();
 }
 