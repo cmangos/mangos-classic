7fe6fbd36502e3315191ddd14a5e35c06653ea66
killerwife <killerwife@gmail.com>
2017-10-05 20:18:43 +0200

    Fix formatting and comment in PetAI

--- src/game/AI/BaseAI/PetAI.cpp
+++ src/game/AI/BaseAI/PetAI.cpp
@@ -390,7 +390,7 @@ void PetAI::UpdateAllies()
     Unit* owner = m_unit->GetMaster();
     Group* pGroup = nullptr;
 
-    m_updateAlliesTimer = 10 * IN_MILLISECONDS;             // update friendly targets every 10 seconds, lesser checks increase performance
+    m_updateAlliesTimer = 10 * IN_MILLISECONDS;             // update friendly targets every 10 seconds, fewer checks increase performance
 
     if (!owner)
         return;
