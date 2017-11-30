6d37c6f3a3c060d8276ebab277464a75f82a39e1
Killerwife <killerwife@gmail.com>
2017-11-30 22:54:14 +0100

    PVP combat timer should be started for PVPFreeForAll as well

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -6988,7 +6988,7 @@ void Unit::Unmount(bool from_aura)
 void Unit::SetInCombatWith(Unit* enemy)
 {
     Unit* eOwner = enemy->GetBeneficiary();
-    if (eOwner->IsPvP())
+    if (eOwner->IsPvP() || eOwner->IsPvPFreeForAll())
     {
         SetInCombatState(true, enemy);
         return;
