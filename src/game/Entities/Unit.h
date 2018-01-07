46b8cbfd409467cbf835652027e4531db95ff50e
killerwife <killerwife@gmail.com>
2018-01-07 09:21:18 +0100

    Add some possibly crucial unks to enum

--- src/game/Entities/Unit.h
+++ src/game/Entities/Unit.h
@@ -56,11 +56,13 @@ enum SpellInterruptFlags
 
 enum SpellChannelInterruptFlags
 {
-    CHANNEL_FLAG_DAMAGE      = 0x0002,
-    CHANNEL_FLAG_MOVEMENT    = 0x0008,
-    CHANNEL_FLAG_TURNING     = 0x0010,
-    CHANNEL_FLAG_DAMAGE2     = 0x0080,
-    CHANNEL_FLAG_DELAY       = 0x4000
+    CHANNEL_FLAG_UNK        = 0x0001,
+    CHANNEL_FLAG_DAMAGE     = 0x0002,
+    CHANNEL_FLAG_MOVEMENT   = 0x0008,
+    CHANNEL_FLAG_TURNING    = 0x0010,
+    CHANNEL_FLAG_DAMAGE2    = 0x0080,
+    CHANNEL_FLAG_UNK2       = 0x1000,
+    CHANNEL_FLAG_DELAY      = 0x4000,
 };
 
 enum SpellAuraInterruptFlags
