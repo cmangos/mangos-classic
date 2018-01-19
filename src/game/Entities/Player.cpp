3f7e6c13644686e9d56ccb58401c1f92953229d9
Warlockbugs <warlockbugs@outlook.com>
2018-01-19 08:05:49 +0300

    Add UNIT_FLAG_POSSESSED to player unit flag cleanup list
    Don't let it run loose, should only be strictly associated with possess auras.

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -2563,8 +2563,9 @@ void Player::InitStatsForLevel(bool reapplyMods)
                UNIT_FLAG_IMMUNE_TO_PLAYER | UNIT_FLAG_IMMUNE_TO_NPC    | UNIT_FLAG_LOOTING          |
                UNIT_FLAG_PET_IN_COMBAT  | UNIT_FLAG_SILENCED     | UNIT_FLAG_PACIFIED         |
                UNIT_FLAG_STUNNED        | UNIT_FLAG_IN_COMBAT    | UNIT_FLAG_DISARMED         |
-               UNIT_FLAG_CONFUSED       | UNIT_FLAG_FLEEING      | UNIT_FLAG_NOT_SELECTABLE   |
-               UNIT_FLAG_SKINNABLE      | UNIT_FLAG_TAXI_FLIGHT);
+               UNIT_FLAG_CONFUSED       | UNIT_FLAG_FLEEING      | UNIT_FLAG_POSSESSED        |
+               UNIT_FLAG_NOT_SELECTABLE | UNIT_FLAG_SKINNABLE    |
+               UNIT_FLAG_TAXI_FLIGHT);
     SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PLAYER_CONTROLLED);    // must be set
 
     // cleanup player flags (will be re-applied if need at aura load), to avoid have ghost flag without ghost aura, for example.
