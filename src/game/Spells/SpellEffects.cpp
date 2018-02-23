5ba828f5cd0a03eb42aaadc8eecbc398f925a758
Killerwife <killerwife@gmail.com>
2018-02-23 09:12:45 +0100

    Prevent middleman judgement spells from rolling hit calc again
    Same problem as execute had.

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -4262,7 +4262,7 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
                     break;
                 }
 
-                m_caster->CastSpell(unitTarget, spellId2, TRIGGERED_OLD_TRIGGERED);
+                m_caster->CastSpell(unitTarget, spellId2, TRIGGERED_OLD_TRIGGERED | TRIGGERED_IGNORE_HIT_CALCULATION);
 
                 return;
             }
