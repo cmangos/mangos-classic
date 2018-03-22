dbe73246c730ffebd4488c48bfd745342c2a11ed
killerwife <killerwife@gmail.com>
2018-03-22 08:13:58 +0100

    Fix merge conflict from Stave of the Ancients commit

--- sql/scriptdev2/scriptdev2.sql
+++ sql/scriptdev2/scriptdev2.sql
@@ -100,11 +100,7 @@ UPDATE creature_template SET ScriptName='npc_prof_leather' WHERE entry IN (7866,
 -- UPDATE creature_template SET ScriptName='' WHERE npcflag!=npcflag|65536 AND ScriptName='npc_innkeeper';
 -- UPDATE creature_template SET ScriptName='npc_innkeeper' WHERE npcflag=npcflag|65536;
 UPDATE creature_template SET ScriptName='npc_redemption_target' WHERE entry IN (6172,6177,17542,17768);
-<<<<<<< HEAD
-=======
-UPDATE creature_template SET ScriptName='npc_burster_worm' WHERE entry IN (16844,16857,16968,21380,21849,22038,22466,22482,23285);
 UPDATE creature_template SET ScriptName='npc_the_cleaner' WHERE entry=14503;
->>>>>>> e1e796eb89... Implement q.7636 Stave of the Ancients
 
 /* SPELL */
 UPDATE creature_template SET ScriptName='spell_dummy_npc' WHERE entry IN (
