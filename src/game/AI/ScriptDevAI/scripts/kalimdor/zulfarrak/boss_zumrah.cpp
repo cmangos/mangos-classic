d6e6b0069cffcc2a2f26cfdafe13b5b5abb2eb23
Nekolepsy <nekolepsy@gmail.com>
2018-03-24 16:50:10 -0600

    Issue #1466 [classic]some Boss can't be attacked
    Allow Witch Doctor Zum'rah to be attacked

--- src/game/AI/ScriptDevAI/scripts/kalimdor/zulfarrak/boss_zumrah.cpp
+++ src/game/AI/ScriptDevAI/scripts/kalimdor/zulfarrak/boss_zumrah.cpp
@@ -101,6 +101,7 @@ struct boss_zumrahAI : public ScriptedAI
         if (!m_bHasTurnedHostile && pWho->GetTypeId() == TYPEID_PLAYER && m_creature->IsWithinDistInMap(pWho, 9.0f) && m_creature->IsWithinLOSInMap(pWho))
         {
             m_creature->SetFactionTemporary(FACTION_HOSTILE, TEMPFACTION_NONE);
+            m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_IMMUNE_TO_PLAYER);
             DoScriptText(SAY_INTRO, m_creature);
             m_bHasTurnedHostile = true;
             AttackStart(pWho);
