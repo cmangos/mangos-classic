798622588059727608349ffaf079a37770433929
Killerwife <killerwife@gmail.com>
2018-06-18 00:33:15 +0200

    Fix pets not being marked as loading during spell effect init

--- src/game/Entities/Pet.h
+++ src/game/Entities/Pet.h
@@ -172,6 +172,7 @@ class Pet : public Creature
         bool LoadPetFromDB(Player* owner, uint32 petentry = 0, uint32 petnumber = 0, bool current = false, uint32 healthPercentage = 0, bool permanentOnly = false);
         void SavePetToDB(PetSaveMode mode);
         bool isLoading() const { return m_loading; }
+        void SetLoading(bool state) { m_loading = state; }
         void Unsummon(PetSaveMode mode, Unit* owner = nullptr);
         static void DeleteFromDB(uint32 guidlow, bool separate_transaction = true);
         static void DeleteFromDB(Unit* owner, PetSaveMode slot);
