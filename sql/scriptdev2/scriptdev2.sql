746691ddc925a91e0294e7333258f47a30d8ab7b
killerwife <killerwife@gmail.com>
2018-06-25 19:24:23 +0200

    Fix SD2 SQL file after Dashel script commit

--- sql/scriptdev2/scriptdev2.sql
+++ sql/scriptdev2/scriptdev2.sql
@@ -1671,7 +1671,7 @@ INSERT INTO script_texts (entry,content_default,sound,type,language,emote,commen
 (-1001250,'%s is silenced by the venemous sting.',0,2,0,0,'npc_simone EMOTE_SILENCE'),
 (-1001251,'%s is stricken by a virulent poison.',0,2,0,0,'npc_artorius EMOTE_POISON'),
 (-1001252,'%s is immobilized.',0,2,0,0,'npc_solenor EMOTE_IMMOBILIZED'),
-(-1001253,'You dare interfere with this being''s testing? The battle must be fought alone! You shall all pay for this interference!',0,0,0,0,'the_cleaner SAY_CLEANER_AGGRO');
+(-1001253,'You dare interfere with this being''s testing? The battle must be fought alone! You shall all pay for this interference!',0,0,0,0,'the_cleaner SAY_CLEANER_AGGRO'),
 -- used in TBC core
 (-1001274,'Now you''re gonna get it good, $n!', '0', '0', '0', '0','dashel stonefist SAY_STONEFIST_1'),
 (-1001275,'Okay, okay! Enough fighting. No one else needs to get hurt.', '0', '0', '0', '0','dashel stonefist SAY_STONEFIST_2'),
