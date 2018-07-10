45fd15b2b1bfc001a1ec8cd4e8d479fa10d8b26f
killerwife <killerwife@gmail.com>
2018-07-10 13:22:02 +0200

    Add sd2 text ranges to get rid of conflicts between expansions in outdoors scripts

--- sql/scriptdev2/scriptdev2.sql
+++ sql/scriptdev2/scriptdev2.sql
@@ -1677,6 +1677,12 @@ INSERT INTO script_texts (entry,content_default,sound,type,language,emote,commen
 (-1001275,'Okay, okay! Enough fighting. No one else needs to get hurt.', '0', '0', '0', '0','dashel stonefist SAY_STONEFIST_2'),
 (-1001276,'It''s okay, boys. Back off. You''ve done enough. I''ll meet up with you later.', '0', '0', '0', '0','dashel stonefist SAY_STONEFIST_3');
 
+-- -1 010 000 Classic texts
+
+-- -1 015 000 TBC texts
+
+-- -1 020 000 WOTLK texts
+
 -- -1 033 000 SHADOWFANG KEEP
 INSERT INTO script_texts (entry,content_default,sound,type,language,emote,comment) VALUES
 (-1033000,'Follow me and I\'ll open the courtyard door for you.',0,0,7,1,'prisoner ashcrombe SAY_FREE_AS'),
