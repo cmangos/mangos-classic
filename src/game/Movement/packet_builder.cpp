83734d086db72990a1703c67be2cf455fc26da10
cyberium <cyberium@users.noreply.github.com>
2018-09-04 11:09:25 +0200

    Fix always true condition

--- src/game/Movement/packet_builder.cpp
+++ src/game/Movement/packet_builder.cpp
@@ -75,14 +75,14 @@ namespace Movement
     {
         Movement::SplineBase::ControlArray const& pathPoint = spline.getPoints(); // get ref of whole path points array
 
-        uint32 pathSize = spline.last() - spline.first() - 1; // -1 as we send destination first and last index is destination
+        int32 pathSize = spline.last() - spline.first() - 1; // -1 as we send destination first and last index is destination
         MANGOS_ASSERT(pathSize >= 0);                       // should never be less than 0
 
         Vector3 destination = pathPoint[spline.last()];     // destination of this path should be send right after path size
         data << pathSize;
         data << destination;
 
-        for (uint32 i = spline.first(); i < spline.first() + pathSize; i++) // from first real index (this array contain also special data)
+        for (int32 i = spline.first(); i < spline.first() + pathSize; i++) // from first real index (this array contain also special data)
         {
             Vector3 offset = destination - pathPoint[i];    // we have to send offset relative to destination instead of directly path point.
             data.appendPackXYZ(offset.x, offset.y, offset.z); // we have to pack x,y,z before send
