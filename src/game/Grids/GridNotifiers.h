52d7af5f94a2c9b5b6e3f1e48d62fed4c36734c5
Killerwife <killerwife@gmail.com>
2018-11-05 10:14:05 +0100

    Make unfriendly traps check for LOS

--- src/game/Grids/GridNotifiers.h
+++ src/game/Grids/GridNotifiers.h
@@ -875,7 +875,7 @@ namespace MaNGOS
             WorldObject const& GetFocusObject() const { return *i_obj; }
             bool operator()(Unit* u) const
             {
-                return u->isAlive() && i_obj->CanAttackSpell(u) && i_obj->IsWithinDistInMap(u, i_range);
+                return u->isAlive() && i_obj->CanAttackSpell(u) && i_obj->IsWithinDistInMap(u, i_range) && i_obj->IsWithinLOSInMap(u);
             }
         private:
             WorldObject const* i_obj;
