dc66acfcb4301d67de86873db1ddc760becf7d20
Cyberium <cybermomo@gmail.com>
2019-01-08 19:14:14 +0100

    Silent loading map error as it can be normal in some case
    Some map doesn't have map data. They are basicaly a big game object.
    You are still able to see those error if you enable LOG_FILTER_MAP_LOADING filter in mangosd.conf
    
    close https://github.com/cmangos/issues/issues/1819

--- src/game/Maps/GridMap.cpp
+++ src/game/Maps/GridMap.cpp
@@ -82,7 +82,11 @@ bool GridMap::loadData(char const* filename)
     // Not return error if file not found
     FILE* in = fopen(filename, "rb");
     if (!in)
-        return false;
+    {
+        DEBUG_FILTER_LOG(LOG_FILTER_MAP_LOADING, "Failled to found %s", filename);
+        // its a valid error only in case of no vmap files are available too
+        return true;
+    }
 
     fread(&header, sizeof(header), 1, in);
     if (header.mapMagic     == *((uint32 const*)(MAP_MAGIC)) &&
