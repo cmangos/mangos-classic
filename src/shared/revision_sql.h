d1ceb24a8518847929827a6f95bfde970eea7ce7
pengzx_cmangos <solomon0728@163.com>
2019-01-10 13:39:50 +0800

    Revert "Revert "Prevent Prowl to consume Clearcasting""
    This reverts commit ec316fbdf0267f58c5ff09f4695cdd7d4292f2fc.

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_z2716_01_realmd_totp"
  #define REVISION_DB_CHARACTERS "required_z2708_01_characters_account_instances_entered"
- #define REVISION_DB_MANGOS "required_z2717_01_mangos_spam_records_length"
+ #define REVISION_DB_MANGOS "required_z2718_01_mangos_spell_affect"
 #endif // __REVISION_SQL_H__
