80f7c074801f83c14fb7519f462123f07f5f2d65
pengzx_cmangos <solomon0728@163.com>
2019-01-15 13:30:44 +0800

    创建数据库用一个脚本实现

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#创建库
+uname="root"
+upasswd="root"
+cmd="mysql -u${uname} -p${upasswd} -s"
+#创建库
+
+#创建库
+printf "Creating classicmangos database \n"
+${cmd} -e "drop database if exists classicmangos"
+${cmd} -e "CREATE DATABASE classicmangos DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci"
+printf "Creating classiccharacters database \n"
+${cmd} -e "drop database if exists classiccharacters"
+${cmd} -e "CREATE DATABASE classiccharacters DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci"
+printf "Creating classicrealmd database \n"
+${cmd} -e "drop database if exists classicrealmd"
+${cmd} -e "CREATE DATABASE classicrealmd DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci"
+
+
+${cmd} -e "DROP USER cmangos@localhost";
+
+printf "Creating cmangos user for wow \n"
+${cmd} -e "CREATE USER cmangos@localhost IDENTIFIED BY 'cmangos'"
+
+printf "Creating SIUDCDAL...  for wow database with cmangos\n"
+${cmd} -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, LOCK TABLES, CREATE TEMPORARY TABLES, EXECUTE, ALTER ROUTINE, CREATE ROUTINE ON classicmangos.* TO \"cmangos\"@\"localhost\""
+${cmd} -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, LOCK TABLES, CREATE TEMPORARY TABLES, EXECUTE ON classiccharacters.* TO \"cmangos\"@\"localhost\""
+${cmd} -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, LOCK TABLES, CREATE TEMPORARY TABLES, EXECUTE ON classicrealmd.* TO cmangos@localhost"
+
+
+#创建基础数据
+#修改缓存大小256M(1024*1024*256),检查方法show global variables like 'max_allowed_packet';
+${cmd} -e "set global max_allowed_packet=268435456"
+#创建基础数据
+${cmd} --database=classicmangos  < sql/base/mangos.sql
+printf "importing classicmangos base datas \n"
+for sql_file in `ls sql/base/dbc/original_data/*.sql`; do ${cmd} --database=classicmangos < $sql_file ; done
+for sql_file in `ls sql/base/dbc/cmangos_fixes/*.sql`; do ${cmd} --database=classicmangos < $sql_file ; done
+#创建角色属性数据
+printf "import classiccharacters for wow \n"
+${cmd} classiccharacters < sql/base/characters.sql
+#创建登录数据库
+printf "import classiccharacters for wow \n"
+${cmd} classicrealmd < sql/base/realmd.sql
+printf "import finish！！！ \n"
