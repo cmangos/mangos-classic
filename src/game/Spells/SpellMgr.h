380d8d3c8db568299c5a9abc1be2f7c9c24c93f7
anonxs <anonxs@web.de>
2019-01-28 19:31:00 +0100

    [Spell] Deadly Poison 10022 should not be removed on Evade

--- src/game/Spells/SpellMgr.h
+++ src/game/Spells/SpellMgr.h
@@ -367,6 +367,7 @@ inline bool IsSpellRemovedOnEvade(SpellEntry const* spellInfo)
         case 8876:          // Thrash
         case 9205:          // Hate to Zero (Hate to Zero)
         case 9460:          // Corrosive Ooze
+        case 10022:         // Deadly Poison
         case 10095:         // Hate to Zero (Hate to Zero)
         case 11838:         // Hate to Zero (Hate to Zero)
         case 11919:         // Poison Proc
