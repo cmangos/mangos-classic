4d753115b13a554ebec51ba82ad008b56487bbc4
Thorbjørn H.H. Sindrup <holsthein@live.dk>
2019-02-05 11:00:54 +0100

    Nest player controlled pet attack angle under config
    Add PetAttackFromBehind as a bool config value and default it to 0 on classic, much like PetUnsummonAtMount
    
    no mini pet is ever using petAI, they always use GuardianAI
    
    Since mini_pets are not controlled and never can be since iscontrolled returns getpettype = summon_pet or hunter_pet, which a mini pet never will be, and they are immune to npc and player (set on creatoin in spellEffects) so they are never given petAI , therefore any minipet handling in petAI is redundant and can be removed.

--- src/game/World/World.cpp
+++ src/game/World/World.cpp
@@ -679,6 +679,7 @@ void World::LoadConfigSettings(bool reload)
     setConfig(CONFIG_UINT32_TIMERBAR_FIRE_MAX,        "TimerBar.Fire.Max", 1);
 
     setConfig(CONFIG_BOOL_PET_UNSUMMON_AT_MOUNT,      "PetUnsummonAtMount", false);
+    setConfig(CONFIG_BOOL_PET_ATTACK_FROM_BEHIND,     "PetAttackFromBehind", false);
 
     m_relocation_ai_notify_delay = sConfig.GetIntDefault("Visibility.AIRelocationNotifyDelay", 1000u);
     m_relocation_lower_limit_sq  = pow(sConfig.GetFloatDefault("Visibility.RelocationLowerLimit", 10), 2);
