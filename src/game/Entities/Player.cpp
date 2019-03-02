b1b9770bc44dfbdc8bc4e023b8d0cf96ce0db4fc
Warlockbugs <warlockbugs@outlook.com>
2019-03-02 17:27:40 +0300

    Improve liquid level handling in new environment flags
    Closes https://github.com/cmangos/issues/issues/1893

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -1070,8 +1070,8 @@ void Player::SetEnvironmentFlags(EnvironmentFlags flags, bool apply)
     }
 
     // Remove auras that need land or water
-    if (flags & ENVIRONMENT_FLAG_SHALLOW_LIQUID && (apply || IsInWater()))
-        RemoveAurasWithInterruptFlags(apply ? AURA_INTERRUPT_FLAG_NOT_UNDERWATER : AURA_INTERRUPT_FLAG_NOT_ABOVEWATER);
+    if (flags & ENVIRONMENT_FLAG_HIGH_LIQUID)
+        RemoveAurasWithInterruptFlags(apply ?  AURA_INTERRUPT_FLAG_NOT_ABOVEWATER : AURA_INTERRUPT_FLAG_NOT_UNDERWATER);
 
     // On moving in/out high sea area: affect fatigue timer
     if (flags & ENVIRONMENT_FLAG_HIGH_SEA)
@@ -18701,8 +18701,8 @@ void Player::UpdateTerainEnvironmentFlags(Map* m, float x, float y, float z)
     // In deep water: on, under, above surface level
     SetEnvironmentFlags(ENVIRONMENT_FLAG_HIGH_SEA, (liquid_status.type_flags & MAP_LIQUID_TYPE_DEEP_WATER));
 
-    // All liquid types: check too shallow level for swimming
-    SetEnvironmentFlags(ENVIRONMENT_FLAG_SHALLOW_LIQUID, ((res & LIQUID_MAP_IN_WATER) && !(res & LIQUID_MAP_UNDER_WATER) && (liquid_status.level < liquid_status.depth_level + 1.5f)));
+    // All liquid types: check if deep enough level for swimming
+    SetEnvironmentFlags(ENVIRONMENT_FLAG_HIGH_LIQUID, ((res & (LIQUID_MAP_UNDER_WATER | LIQUID_MAP_IN_WATER)) && liquid_status.level > (liquid_status.depth_level + 1.5f)));
 }
 
 bool ItemPosCount::isContainedIn(ItemPosCountVec const& vec) const
