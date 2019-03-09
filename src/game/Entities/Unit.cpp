c75e6971521734530c9cf3f6f952e93830182068
Warlockbugs <warlockbugs@outlook.com>
2019-03-09 17:19:39 +0300

    Magic Resistance implementation, mk. III: Fix spell penetration calc typo
    Fix wrong sign

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -3545,7 +3545,7 @@ float Unit::CalculateEffectiveMagicResistancePercent(const Unit* attacker, Spell
             int32 penetration = attacker->GetResistancePenetration(SpellSchools(school));
 
             // Modify by penetration, but can't go negative with it
-            int32 result = (amount + penetration);
+            int32 result = (amount - penetration);
 
             if (result < 0)
                 result = std::min(amount, 0);
