020c6be7751d217b9cbc083c76a9b2062cf0cf72
anonxs <anonxs@web.de>
2019-04-19 15:04:27 +0200

    [Spell] Bear Form (Shapeshift) 7090/31792 should not be removed on Evade

--- src/game/Spells/SpellMgr.h
+++ src/game/Spells/SpellMgr.h
@@ -363,6 +363,7 @@ inline bool IsSpellRemovedOnEvade(SpellEntry const* spellInfo)
         case 3235:          // Rancid Blood
         case 3417:          // Thrash
         case 6752:          // Weak Poison Proc
+        case 7090:          // Bear Form (Shapeshift)
         case 7276:          // Poison Proc
         case 8247:          // Wandering Plague
         case 8279:          // Stealth Detection
