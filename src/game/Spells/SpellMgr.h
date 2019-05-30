8bcb6e93b3fbb6d153fd42af4d4239749e7dc41b
Ben Dol <dolb90@gmail.com>
2019-05-31 07:22:27 +1200

    Fix duplication/stacking issues with cast item spells
    Modified the RemoveNoStackAurasDueToAuraHolder check to also account for
    the cast items, if they ever match we need to avoid stacking.
    I also fixed some of the passive spell check logic to account for all the
    SPELL_EFFECT_APPLY_AURA_* spell effects. This will ensure friendly auras
    are always check.
    
    Resolves https://github.com/cmangos/issues/issues/1943

--- src/game/Spells/SpellMgr.h
+++ src/game/Spells/SpellMgr.h
@@ -2330,6 +2330,7 @@ class SpellMgr
             return (spellId1 != spellId2 && GetFirstSpellInChain(spellId1) == GetFirstSpellInChain(spellId2));
         }
 
+        bool IsNoStackSpellDueToSpellAndCastItem(SpellAuraHolder const* spellHolder1, SpellAuraHolder const* spellHolder2) const;
         bool IsNoStackSpellDueToSpell(SpellEntry const* spellInfo_1, SpellEntry const* spellInfo_2) const;
         bool IsSingleTargetSpell(SpellEntry const* entry) const
         {
