3271af152c6c0d4d0d9b5291410898e4fd17ad9b
killerwife <killerwife@gmail.com>
2019-05-31 17:53:17 +0200

    Add note to attribute SPELL_ATTR_EX3_NO_DONE_BONUS about resisting

--- src/game/Globals/SharedDefines.h
+++ src/game/Globals/SharedDefines.h
@@ -401,7 +401,7 @@ enum SpellAttributesEx3
     SPELL_ATTR_EX3_CAN_PROC_FROM_TRIGGERED_SPECIAL = 0x04000000,// 26 Auras with this attribute can proc off SPELL_ATTR_EX3_TRIGGERED_CAN_TRIGGER_SPECIAL
     SPELL_ATTR_EX3_DRAIN_SOUL                  = 0x08000000,// 27
     SPELL_ATTR_EX3_UNK28                       = 0x10000000,// 28 always cast ok ? (requires more research)
-    SPELL_ATTR_EX3_NO_DONE_BONUS               = 0x20000000,// 29
+    SPELL_ATTR_EX3_NO_DONE_BONUS               = 0x20000000,// 29 Resistances should still affect damage
     SPELL_ATTR_EX3_DONT_DISPLAY_RANGE          = 0x40000000,// 30
     SPELL_ATTR_EX3_UNK31                       = 0x80000000,// 31
 };
