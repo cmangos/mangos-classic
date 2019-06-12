c6a853ea64bb2356b91ecdef18cd841464fd1f87
killerwife <killerwife@gmail.com>
2019-06-12 08:56:28 +0200

    AI: Fix mobs initiating stopmoving when casting a spell which can be cast while moving

--- src/game/AI/BaseAI/UnitAI.cpp
+++ src/game/AI/BaseAI/UnitAI.cpp
@@ -281,7 +281,7 @@ void UnitAI::OnSpellCastStateChange(Spell const* spell, bool state, WorldObject*
         return;
 
     // Creature should always stop before it will cast a non-instant spell
-    if (spell->GetCastTime() || (IsChanneledSpell(spellInfo) && spellInfo->ChannelInterruptFlags & CHANNEL_FLAG_MOVEMENT))
+    if ((spell->GetCastTime() && spellInfo->InterruptFlags & SPELL_INTERRUPT_FLAG_MOVEMENT) || (IsChanneledSpell(spellInfo) && spellInfo->ChannelInterruptFlags & CHANNEL_FLAG_MOVEMENT))
         m_unit->StopMoving();
 
     bool forceTarget = false;
