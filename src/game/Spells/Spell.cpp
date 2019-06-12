7b42b5dee897aac215fabd19694f55b675b74bb3
Cyberium <cyberium@users.noreply.github.com>
2019-06-12 19:30:34 +0200

    Check exact amount of item space requirement for create item spell effect
    So it can fail properly with stackable items. Without this it will consume mana/material in some case without droping any items.

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -6290,7 +6290,7 @@ SpellCastResult Spell::CheckItems()
                 if (!m_IsTriggeredSpell && m_spellInfo->EffectItemType[i])
                 {
                     ItemPosCountVec dest;
-                    InventoryResult msg = playerTarget->CanStoreNewItem(NULL_BAG, NULL_SLOT, dest, m_spellInfo->EffectItemType[i], 1);
+                    InventoryResult msg = playerTarget->CanStoreNewItem(NULL_BAG, NULL_SLOT, dest, m_spellInfo->EffectItemType[i], CalculateDamage(SpellEffectIndex(i), m_caster));
                     if (msg != EQUIP_ERR_OK)
                     {
                         p_caster->SendEquipError(msg, nullptr, nullptr, m_spellInfo->EffectItemType[i]);
