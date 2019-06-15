4b7325800f05d2d50bb536d47e5c9f5963026dd3
killerwife <killerwife@gmail.com>
2019-06-15 18:01:29 +0200

    Fix Sweeping Strikes being lost on shapeshift to defensive

--- sql/base/dbc/cmangos_fixes/Spell.sql
+++ sql/base/dbc/cmangos_fixes/Spell.sql
@@ -38,3 +38,6 @@ UPDATE `spell_template` SET `AttributesEx`=0 WHERE `id`=28370;
 
 -- Hurricane Spell From Druid And NPC must be interruptable
 UPDATE `spell_template` SET `InterruptFlags`=15 WHERE `id`=16914 OR `id`=17401 OR `id`=17402;
+
+-- sweeping strikes should not be lost on stance change to defensive
+UPDATE spell_template SET Attributes=Attributes|0x00010000 WHERE Id IN(12328);
