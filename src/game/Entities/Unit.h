2152ecb7634994cbd155af9a91a520eccc7a0fd5
killerwife <killerwife@gmail.com>
2019-06-18 09:58:37 +0200

    Fix some merge issues

--- src/game/Entities/Unit.h
+++ src/game/Entities/Unit.h
@@ -1705,7 +1705,7 @@ class Unit : public WorldObject
 
         virtual bool IsInWater() const;
         bool IsInSwimmableWater() const;
-        virtual bool IsUnderWater() const;
+        virtual bool IsUnderwater() const;
         bool isInAccessablePlaceFor(Unit const* unit) const;
 
         void EnergizeBySpell(Unit* pVictim, uint32 SpellID, uint32 Damage, Powers powertype);
