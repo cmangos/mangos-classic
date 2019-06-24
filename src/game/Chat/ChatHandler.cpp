72b5e00a9adbe16659d917d92e6a039a88fa30ff
Warlockbugs <warlockbugs@outlook.com>
2019-06-24 19:36:01 +0300

    Revert "Disable system notices for addon whispers"
    The change was reverted after further investigation.
    
    The traces of this UI issue can be tracked to the original date of addon whispers implementation (Patch 2.1.0, 2007).
    
    List of references (in reverse chronological order):
    https://web.archive.org/web/20190624162456/https://www.reddit.com/r/wow/comments/2r85gp/no_player_named_is_currently_playing/
    https://web.archive.org/web/20190624162319/https://www.wowinterface.com/forums/showthread.php?t=50872
    https://web.archive.org/web/20190624162728/https://eu.battle.net/forums/en/wow/topic/5616422568
    https://web.archive.org/web/20190624162806/https://eu.battle.net/forums/en/wow/topic/17617035488
    https://web.archive.org/web/20190624162928/https://www.dual-boxing.com/threads/18380-Jamba-how-do-you-disable-No-player-named-XXX-is-currently-playing
    https://web.archive.org/web/20190624163102/https://www.dual-boxing.com/threads/14724-JAMBA-How-to-stop-No-player-named-is-currently-playing-spam
    https://web.archive.org/web/20190624163246/https://authors.curseforge.com/forums/world-of-warcraft/general-chat/general-chat/222950-no-player-named-is-currently-playing-spam
    
    Conclusion: these messages are legit for addon messages and indicative of badly written addon communication protocol and should stay to prevent obscuration of the underlying issue from addon developers.
    
    Case closed.
    
    (cherry picked from commit 255ee19058fac3b3ff78923c0dfa7faafb7b14ae)

--- src/game/Chat/ChatHandler.cpp
+++ src/game/Chat/ChatHandler.cpp
@@ -189,8 +189,7 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
 
             if (!normalizePlayerName(to))
             {
-                if (lang != LANG_ADDON)
-                    SendPlayerNotFoundNotice(to);
+                SendPlayerNotFoundNotice(to);
                 break;
             }
 
@@ -199,8 +198,7 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
             uint32 pSecurity = player ? player->GetSession()->GetSecurity() : SEC_PLAYER;
             if (!player || (tSecurity == SEC_PLAYER && pSecurity > SEC_PLAYER && !player->isAcceptWhispers()))
             {
-                if (lang != LANG_ADDON)
-                    SendPlayerNotFoundNotice(to);
+                SendPlayerNotFoundNotice(to);
                 return;
             }
 
@@ -208,8 +206,7 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
             {
                 if (GetPlayer()->GetTeam() != player->GetTeam())
                 {
-                    if (lang != LANG_ADDON)
-                        SendWrongFactionNotice();
+                    SendWrongFactionNotice();
                     return;
                 }
             }
