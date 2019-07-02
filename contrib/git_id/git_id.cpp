cmangos/mangos-tbc

Add default remote variant to git_id

contrib/git_id/git_id.cpp
@@ -50,7 +50,7 @@
 
 // config
 
-#define NUM_REMOTES 3
+#define NUM_REMOTES 4
 #define NUM_DATABASES 3
 
 char remotes[NUM_REMOTES][MAX_REMOTE] =
@@ -58,6 +58,7 @@ char remotes[NUM_REMOTES][MAX_REMOTE] =
     "git@github.com:cmangos/mangos-tbc.git",
     "git://github.com/cmangos/mangos-tbc.git",      // used for fetch if present
     "https://github.com/cmangos/mangos-tbc.git"
+    "https://github.com/cmangos/mangos-tbc/"
 };
 
 char remote_branch[MAX_REMOTE] = "master";

cmangos/mangos-classic

contrib/git_id/git_id.cpp
@@ -50,14 +50,15 @@
 
 // config
 
-#define NUM_REMOTES 3
+#define NUM_REMOTES 4
 #define NUM_DATABASES 3
 
 char remotes[NUM_REMOTES][MAX_REMOTE] =
 {
     "git@github.com:cmangos/mangos-classic.git",
     "git://github.com/cmangos/mangos-classic.git",          // used for fetch if present
-    "https://github.com/cmangos/mangos-classic.git"
+    "https://github.com/cmangos/mangos-classic.git",
+    "https://github.com/cmangos/mangos-classic/"
 };
 
 char remote_branch[MAX_REMOTE] = "master";