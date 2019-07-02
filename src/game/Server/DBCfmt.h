cmangos/mangos-tbc

Fully reimplement Spell Range calculations using client method
Big thanks to trinitycore and their helpfulness
GetMinMaxRange corresponds to Spell_C_GetMinMaxRange
Added usage of SpellRangeEntry Flags

src/game/Server/DBCfmt.h
@@ -83,7 +83,7 @@ const char SpellFocusObjectfmt[] = "nxxxxxxxxxxxxxxxxx";
 const char SpellItemEnchantmentfmt[] = "niiiiiixxxiiissssssssssssssssxiiii";
 const char SpellItemEnchantmentConditionfmt[] = "nbbbbbxxxxxbbbbbbbbbbiiiiiXXXXX";
 const char SpellRadiusfmt[] = "nfxx";
-const char SpellRangefmt[] = "nffxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const char SpellRangefmt[] = "nffixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 const char SpellShapeshiftfmt[] = "nxxxxxxxxxxxxxxxxxxiixiixxxiiiiiiii";
 const char StableSlotPricesfmt[] = "ni";
 const char SummonPropertiesfmt[] = "niiiii";

cmangos/mangos-classic

src/game/Server/DBCfmt.h
@@ -61,7 +61,7 @@ const char SpellDurationfmt[] = "niii";
 const char SpellFocusObjectfmt[] = "nxxxxxxxxx";
 const char SpellItemEnchantmentfmt[] = "niiiiiixxxiiissssssssxii";
 const char SpellRadiusfmt[] = "nfxx";
-const char SpellRangefmt[] = "nffxxxxxxxxxxxxxxxxxxx";
+const char SpellRangefmt[] = "nffixxxxxxxxxxxxxxxxxx";
 const char SpellShapeshiftfmt[] = "nxxxxxxxxxxiix";
 const char StableSlotPricesfmt[] = "ni";
 const char TalentEntryfmt[] = "niiiiiiiixxxxixxixxxi";