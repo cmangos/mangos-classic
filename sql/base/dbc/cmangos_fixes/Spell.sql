123e685b99bf07ff38b54819b69e198b5dae4381
killerwife <killerwife@gmail.com>
2019-07-11 18:29:43 +0200

    Fix Improved Wing Clip not being applied to self

--- sql/base/dbc/cmangos_fixes/Spell.sql
+++ sql/base/dbc/cmangos_fixes/Spell.sql
@@ -50,6 +50,11 @@ UPDATE spell_template SET Attributes=Attributes|0x00010000 WHERE Id IN(12328);
 UPDATE spell_template SET EffectRadiusIndex1=EffectRadiusIndex2 WHERE Id IN(24811);
 -- Opening Termite Barrel - target flags for GO/ITEM, effect targets GO/ITEM yet target targets units - fixes apparent typo
 UPDATE spell_template SET EffectImplicitTargetA1=26 WHERE Id IN(18952);
+-- Talents - Improved Scorch and Shadow Weaving have TARGET_UNIT_ENEMY, yet the auras are placed on caster anyway
+-- It is possible talents are supposed to bypass normal casting, but evidenced by wotlk, the rest are all caster targeted, so assuming copypaste wrong data possibility as well
+UPDATE spell_template SET EffectImplicitTargetA1=1 WHERE Id IN(11095,12872,12873,15257,15331,15332,15333,15334);
+-- Improved Wing Clip - item equipped aura which is meant to be cast on self
+UPDATE spell_template SET EffectImplicitTargetA1=1 WHERE Id IN(19228,19232,19233,19234,19235);
 
 -- Mangletooth - These spells need to ignore LOS
 UPDATE spell_template SET AttributesEx2 = AttributesEx2|4 WHERE id IN (7764, 16618, 10767, 16610, 16612, 17013);
