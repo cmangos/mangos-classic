a83e5c2ee5e01dc349dab996c10cd76a22ec56ef
anonxs <anonxs@web.de>
2019-08-24 16:51:46 +0200

    [Spell] Drowning Death 16563 should not be removed on Evade

--- src/game/Spells/SpellMgr.h
+++ src/game/Spells/SpellMgr.h
@@ -408,6 +408,7 @@ inline bool IsSpellRemovedOnEvade(SpellEntry const* spellInfo)
         case 13299:         // Poison Proc
         case 13767:         // Hate to Zero (Hate to Zero)
         case 16140:         // Exploding Cadaver (Exploding Cadaver)
+        case 16563:         // Drowning Death
         case 16592:         // Shadowform
         case 17327:         // Spirit Particles
         case 17467:         // Unholy Aura
