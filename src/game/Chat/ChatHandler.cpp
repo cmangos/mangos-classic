6986ea7c05fe173f83a6464e378e7ea6a8fff828
Warlockbugs <warlockbugs@outlook.com>
2019-11-02 05:14:23 +0300

    Revert "Do not treat addon messages as byte arrays anymore"
    This reverts commit 1383c6716b66cda9982a25be541e2bf2213a404d.

--- src/game/Chat/ChatHandler.cpp
+++ src/game/Chat/ChatHandler.cpp
@@ -217,7 +217,11 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
         case CHAT_MSG_PARTY:
         {
             std::string msg;
-            recv_data >> msg;
+
+            if (lang == LANG_ADDON)
+                recv_data.read(msg , false);
+            else
+                recv_data >> msg;
 
             if (msg.empty())
                 break;
@@ -249,7 +253,11 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
         case CHAT_MSG_GUILD:
         {
             std::string msg;
-            recv_data >> msg;
+
+            if (lang == LANG_ADDON)
+                recv_data.read(msg , false);
+            else
+                recv_data >> msg;
 
             if (msg.empty())
                 break;
@@ -272,7 +280,11 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
         case CHAT_MSG_OFFICER:
         {
             std::string msg;
-            recv_data >> msg;
+
+            if (lang == LANG_ADDON)
+                recv_data.read(msg , false);
+            else
+                recv_data >> msg;
 
             if (msg.empty())
                 break;
@@ -295,7 +307,11 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
         case CHAT_MSG_RAID:
         {
             std::string msg;
-            recv_data >> msg;
+
+            if (lang == LANG_ADDON)
+                recv_data.read(msg , false);
+            else
+                recv_data >> msg;
 
             if (msg.empty())
                 break;
@@ -378,7 +394,11 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
         case CHAT_MSG_BATTLEGROUND:
         {
             std::string msg;
-            recv_data >> msg;
+
+            if (lang == LANG_ADDON)
+                recv_data.read(msg , false);
+            else
+                recv_data >> msg;
 
             if (!processChatmessageFurtherAfterSecurityChecks(msg, lang))
                 return;
