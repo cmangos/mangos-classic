b5c2da5272f00b384d2da4da2e73df90f0d12859
Killerwife <killerwife@gmail.com>
2019-12-25 16:26:33 +0100

    Unbreak forced despawn for temporary go summons

--- src/game/Entities/GameObject.cpp
+++ src/game/Entities/GameObject.cpp
@@ -552,7 +552,7 @@ void GameObject::Update(const uint32 diff)
             }
 
             // Remove wild summoned after use
-            if (!HasStaticDBSpawnData() && (!GetSpellId() || GetGOInfo()->GetDespawnPossibility() || GetGOInfo()->IsDespawnAtAction()))
+            if (!HasStaticDBSpawnData() && (!GetSpellId() || GetGOInfo()->GetDespawnPossibility() || GetGOInfo()->IsDespawnAtAction() || m_forcedDespawn))
             {
                 if (Unit* owner = GetOwner())
                     owner->RemoveGameObject(this, false);
