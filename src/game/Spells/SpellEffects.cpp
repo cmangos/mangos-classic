4680b95b005e0ec09afe3e770b2d4db449855cae
killerwife <killerwife@gmail.com>
2020-01-15 17:38:43 +0100

    Fix scripts not being launched in EffectTeleportUnits

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -1964,7 +1964,7 @@ void Spell::EffectTeleportUnits(SpellEffectIndex eff_idx)   // TODO - Use target
             float orientation = unitTarget->GetOrientation();
             // Teleport
             unitTarget->NearTeleportTo(x, y, z, orientation, unitTarget == m_caster);
-            return;
+            break;
         }
     }
 
