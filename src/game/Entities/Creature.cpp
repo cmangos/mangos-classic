2665879409dc9403dc4d079567283e2e787d99df
killerwife <killerwife@gmail.com>
2020-01-26 23:04:28 +0100

    Reinstate old mana regen formula for common mobs

--- src/game/Entities/Creature.cpp
+++ src/game/Entities/Creature.cpp
@@ -724,6 +724,8 @@ void Creature::RegeneratePower()
                     float ManaIncreaseRate = sWorld.getConfig(CONFIG_FLOAT_RATE_POWER_MANA);
                     float intellect = GetStat(STAT_INTELLECT);
                     addValue = sqrt(intellect) * OCTRegenMPPerSpirit() * ManaIncreaseRate;
+                    if (!IsPet() && !HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PLAYER_CONTROLLED) && addValue == 0.f)
+                        addValue = 17.f * ManaIncreaseRate / 5.f * 2.f;
                 }
             }
             else
