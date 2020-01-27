32e7dd67cf97936868b158465d30473189b2a956
Warlockbugs <warlockbugs@outlook.com>
2020-01-27 11:59:13 +0300

    Improve workaround for MM restoration for fleeing and confusion

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -9313,7 +9313,11 @@ bool Unit::SetConfused(bool apply, ObjectGuid casterGuid, uint32 spellID)
             if (apply)
                 GetMotionMaster()->MoveConfused();
             else if (IsFleeing() && !panic)
-                GetMotionMaster()->MoveFleeing((IsInWorld() ? GetMap()->GetUnit(casterGuid) : nullptr));
+            {
+                AuraList const& fears = GetAurasByType(SPELL_AURA_MOD_FEAR);
+                Unit* source = (fears.empty() ? nullptr : fears.back()->GetCaster());
+                GetMotionMaster()->MoveFleeing(source ? source : this);
+            }
         }
 
         if (apply)
@@ -9353,7 +9357,10 @@ bool Unit::SetFleeing(bool apply, ObjectGuid casterGuid/* = ObjectGuid()*/, uint
             if (IsConfused())
                 GetMotionMaster()->MoveConfused();
             else if (apply)
-                GetMotionMaster()->MoveFleeing((IsInWorld() ? GetMap()->GetUnit(casterGuid) : nullptr), duration);
+            {
+                Unit* source = (IsInWorld() ? GetMap()->GetUnit(casterGuid) : nullptr);
+                GetMotionMaster()->MoveFleeing((source ? source : this), duration);
+            }
         }
 
         if (apply)
