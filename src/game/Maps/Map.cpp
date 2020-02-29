a40dc40f7e45b497796b69a731df0a28cffff814
Killerwife <killerwife@gmail.com>
2020-02-29 23:15:17 +0100

    Remove random movement along Z axis in random movement generators
    Thx to Grz3s for pointing out

--- src/game/Maps/Map.cpp
+++ src/game/Maps/Map.cpp
@@ -2280,11 +2280,12 @@ bool Map::GetRandomPointUnderWater(float& x, float& y, float& z, float radius, G
         if (min_z > liquidLevel)
             return false;
 
-        float max_z = std::max(z + 0.7f * radius, min_z);
-        max_z = std::min(max_z, liquidLevel);
-        x = i_x;
-        y = i_y;
-        z = min_z + rand_norm_f() * (max_z - min_z);
+        // Mobs underwater do not move along Z axis
+        //float max_z = std::max(z + 0.7f * radius, min_z);
+        //max_z = std::min(max_z, liquidLevel);
+        //x = i_x;
+        //y = i_y;
+        //z = min_z + rand_norm_f() * (max_z - min_z);
         return true;
     }
     return false;
