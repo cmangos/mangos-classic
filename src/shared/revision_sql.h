e7ee7d23bbb1c880ebb45ba59e7f0ac3bdccb19f
Warlockbugs <warlockbugs@outlook.com>
2020-03-11 20:03:27 +0300

    [z2752] New Ticket System: SQL timestamp columns compliance
    Modify columns to be compatible with strict dates settings

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_z2748_01_realmd_banning"
- #define REVISION_DB_CHARACTERS "required_z2751_01_characters_new_ticket_system"
+ #define REVISION_DB_CHARACTERS "required_z2752_01_characters_new_ticket_system_sql_dates"
  #define REVISION_DB_MANGOS "required_z2751_01_mangos_new_ticket_system"
 #endif // __REVISION_SQL_H__
