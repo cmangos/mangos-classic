6d1ac572c215dd7d1c38d2507419f4df82479a59
killerwife <killerwife@gmail.com>
2020-03-16 13:46:39 +0100

    Experimental SMSG_SPELL_UPDATE_CHAIN_TARGETS packet implementation

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -3664,6 +3664,33 @@ void Spell::SendChannelStart(uint32 duration)
 
     m_timer = duration;
 
+    if (m_spellInfo->HasAttribute(SPELL_ATTR_EX_CHANNELED_1))
+    {
+        WorldPacket data(SMSG_SPELL_UPDATE_CHAIN_TARGETS);
+        data << m_caster->GetObjectGuid();
+        data << uint32(m_spellInfo->Id);
+        size_t count_pos = data.wpos();
+        data << uint32(0);
+        uint32 hit = 0;
+        for (TargetList::const_iterator itr = m_UniqueTargetInfo.begin(); itr != m_UniqueTargetInfo.end(); ++itr)
+        {
+            if (((itr->effectHitMask & (1 << EFFECT_INDEX_0)) && itr->reflectResult == SPELL_MISS_NONE &&
+                m_CastItem) || itr->targetGUID != m_caster->GetObjectGuid())
+            {
+                if (Unit* target = ObjectAccessor::GetUnit(*m_caster, itr->targetGUID))
+                {
+                    ++hit;
+                    data << target->GetObjectGuid();
+                }
+            }
+        }
+        if (hit)
+        {
+            data.put<uint32>(count_pos, hit);
+            m_caster->SendMessageToSet(data, true);
+        }
+    }
+
     if (target)
         m_caster->SetChannelObject(target);
 
