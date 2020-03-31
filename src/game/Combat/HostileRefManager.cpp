9220e8290b36f100a1a73384b7c3f9bab6b81730
killerwife <killerwife@gmail.com>
2020-03-31 18:52:38 +0200

    Safeguard assist threat against attributes and AI setting for not entering combat

--- src/game/Combat/HostileRefManager.cpp
+++ src/game/Combat/HostileRefManager.cpp
@@ -38,6 +38,9 @@ HostileRefManager::~HostileRefManager()
 
 void HostileRefManager::threatAssist(Unit* victim, float threat, SpellEntry const* threatSpell, bool singleTarget, bool ignoreTimer)
 {
+    if (threatSpell->HasAttribute(SPELL_ATTR_EX3_NO_INITIAL_AGGRO) || threatSpell->HasAttribute(SPELL_ATTR_EX_NO_THREAT) || !getOwner()->CanEnterCombat() || !victim->CanEnterCombat())
+        return;
+
     HostileReference* ref = getFirst();
     if (!ref)
         return;
