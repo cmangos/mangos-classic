ee97b5915aaf0aaef66c56387ef957cdc36562a8
killerwife <killerwife@gmail.com>
2020-04-02 18:00:39 +0200

    Disable Map::Update session processing due to race conditions
    Needs better scheme.

--- src/game/Maps/Map.cpp
+++ src/game/Maps/Map.cpp
@@ -595,17 +595,17 @@ void Map::Update(const uint32& t_diff)
     GetMessager().Execute(this);
 
     /// update worldsessions for existing players
-    for (m_mapRefIter = m_mapRefManager.begin(); m_mapRefIter != m_mapRefManager.end(); ++m_mapRefIter)
-    {
-        Player* plr = m_mapRefIter->getSource();
-        if (plr && plr->IsInWorld())
-        {
-            WorldSession* pSession = plr->GetSession();
-            MapSessionFilter updater(pSession);
+    //for (m_mapRefIter = m_mapRefManager.begin(); m_mapRefIter != m_mapRefManager.end(); ++m_mapRefIter)
+    //{
+    //    Player* plr = m_mapRefIter->getSource();
+    //    if (plr && plr->IsInWorld())
+    //    {
+    //        WorldSession* pSession = plr->GetSession();
+    //        MapSessionFilter updater(pSession);
 
-            pSession->Update(updater);
-        }
-    }
+    //        pSession->Update(updater);
+    //    }
+    //}
 
     /// update players at tick
     for (m_mapRefIter = m_mapRefManager.begin(); m_mapRefIter != m_mapRefManager.end(); ++m_mapRefIter)
