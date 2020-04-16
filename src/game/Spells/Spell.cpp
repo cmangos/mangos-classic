725706d9989bef7aed86340452c345834c5bb50f
killerwife <killerwife@gmail.com>
2020-04-16 11:44:59 +0200

    Remove max health erasure for SCHEME_LOWEST_HP_CHAIN
    Enables jumping to a low hp target if its too far
    
    Original author: Dromadoros

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -6810,12 +6810,6 @@ void Spell::FilterTargetMap(UnitList& filterUnitList, SpellEffectIndex effIndex,
                     continue;
                 }
 
-                if ((*next)->GetHealth() == (*next)->GetMaxHealth())
-                {
-                    next = filterUnitList.erase(next);
-                    continue;
-                }
-
                 prev = *next;
                 newList.push_back(prev);
                 filterUnitList.erase(next);
