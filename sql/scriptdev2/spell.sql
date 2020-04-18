930ee3004afdabce2d09de71ef90792ed13445eb
Warlockbugs <warlockbugs@outlook.com>
2020-04-18 19:52:29 +0300

    Fix scriptdev2 spellscript sql file structure
    Take in account repo-specific changes
    
    Close https://github.com/cmangos/issues/issues/2170

--- sql/scriptdev2/spell.sql
+++ sql/scriptdev2/spell.sql
@@ -53,7 +53,8 @@ INSERT INTO spell_scripts(Id, ScriptName) VALUES
 
 
 -- Priest
-(10060,'spell_stacking_rules_override'),
+INSERT INTO spell_scripts(Id, ScriptName) VALUES
+(10060,'spell_stacking_rules_override');
 
 -- Warlock
 INSERT INTO spell_scripts(Id, ScriptName) VALUES
