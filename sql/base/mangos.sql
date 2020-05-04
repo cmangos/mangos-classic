2b108c1e865869387c6f6456407aebbaddfe7be3
Muehe <Muehe@users.noreply.github.com>
2020-05-04 20:58:06 +0200

    Fix typo

--- sql/base/mangos.sql
+++ sql/base/mangos.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
-  `required_z2759_01_mangos_gameobjet_template` bit(1) DEFAULT NULL
+  `required_z2759_01_mangos_gameobject_template` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Used DB version notes';
 
 --
