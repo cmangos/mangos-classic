b0039a7704ee240a0eff37cea482c7e2d8340f2a
Cyberium <cyberium@users.noreply.github.com>
2020-05-25 23:43:07 +0200

    Fix build on clang

--- src/game/Entities/ObjectDefines.h
+++ src/game/Entities/ObjectDefines.h
@@ -21,34 +21,34 @@
 
 #include "Maps/GridDefines.h"
 
-static float const CONTACT_DISTANCE             = 0.5f;
-static float const INTERACTION_DISTANCE         = 5.0f;
-static float const ATTACK_DISTANCE              = 5.0f;
-static float const MELEE_LEEWAY                 = 8.0f / 3.0f;                  // Melee attack and melee spell leeway when moving
-static float const AOE_LEEWAY                   = 2.0f;                         // AOE leeway when moving
-static float const INSPECT_DISTANCE             = 28.0f;
-static float const TRADE_DISTANCE               = 11.11f;
+static constexpr float const CONTACT_DISTANCE               = 0.5f;
+static constexpr float const INTERACTION_DISTANCE           = 5.0f;
+static constexpr float const ATTACK_DISTANCE                = 5.0f;
+static constexpr float const MELEE_LEEWAY                   = 8.0f / 3.0f;                  // Melee attack and melee spell leeway when moving
+static constexpr float const AOE_LEEWAY                     = 2.0f;                         // AOE leeway when moving
+static constexpr float const INSPECT_DISTANCE               = 28.0f;
+static constexpr float const TRADE_DISTANCE                 = 11.11f;
 
-static float const MAX_VISIBILITY_DISTANCE      = SIZE_OF_GRIDS;                // max distance for visible object show, limited in 533 yards
-static float const VISIBILITY_DISTANCE_GIGANTIC = 400.0f;
-static float const VISIBILITY_DISTANCE_LARGE    = 200.0f;
-static float const VISIBILITY_DISTANCE_NORMAL   = 100.0f;
-static float const VISIBILITY_DISTANCE_SMALL    = 50.0f;
-static float const VISIBILITY_DISTANCE_TINY     = 25.0f;
-static float const DEFAULT_VISIBILITY_DISTANCE  = VISIBILITY_DISTANCE_NORMAL;   // default visible distance, 100 yards on continents
-static float const DEFAULT_VISIBILITY_INSTANCE  = 170.0f;                       // default visible distance in instances, 170 yards
-static float const DEFAULT_VISIBILITY_BG        = 533.0f;                       // default visible distance in BG/Arenas, 533 yards
+static constexpr float const MAX_VISIBILITY_DISTANCE        = SIZE_OF_GRIDS;                // max distance for visible object show, limited in 533 yards
+static constexpr float const VISIBILITY_DISTANCE_GIGANTIC   = 400.0f;
+static constexpr float const VISIBILITY_DISTANCE_LARGE      = 200.0f;
+static constexpr float const VISIBILITY_DISTANCE_NORMAL     = 100.0f;
+static constexpr float const VISIBILITY_DISTANCE_SMALL      = 50.0f;
+static constexpr float const VISIBILITY_DISTANCE_TINY       = 25.0f;
+static constexpr float const DEFAULT_VISIBILITY_DISTANCE    = VISIBILITY_DISTANCE_NORMAL;   // default visible distance, 100 yards on continents
+static constexpr float const DEFAULT_VISIBILITY_INSTANCE    = 170.0f;                       // default visible distance in instances, 170 yards
+static constexpr float const DEFAULT_VISIBILITY_BG          = 533.0f;                       // default visible distance in BG/Arenas, 533 yards
 
-static float const DEFAULT_WORLD_OBJECT_SIZE    = 0.388999998569489f;           // currently used (correctly?) for any non Unit world objects.
-                                                                                // This is actually the bounding_radius, like player/creature from
-                                                                                // creature_model_data
+static constexpr float const DEFAULT_WORLD_OBJECT_SIZE      = 0.388999998569489f;           // currently used (correctly?) for any non Unit world objects.
+                                                                                            // This is actually the bounding_radius, like player/creature from
+                                                                                            // creature_model_data
 
-static float const DEFAULT_OBJECT_SCALE         = 1.0f;                         // player/item scale as default, npc/go from database, pets from dbc
-static float const DEFAULT_TAUREN_MALE_SCALE    = 1.35f;                        // Tauren male player scale by default
-static float const DEFAULT_TAUREN_FEMALE_SCALE  = 1.25f;                        // Tauren female player scale by default
-static float const DEFAULT_COLLISION_HEIGHT     = 2.03128f;                     // Most common value in dbc
+static constexpr float const DEFAULT_OBJECT_SCALE           = 1.0f;                         // player/item scale as default, npc/go from database, pets from dbc
+static constexpr float const DEFAULT_TAUREN_MALE_SCALE      = 1.35f;                        // Tauren male player scale by default
+static constexpr float const DEFAULT_TAUREN_FEMALE_SCALE    = 1.25f;                        // Tauren female player scale by default
+static constexpr float const DEFAULT_COLLISION_HEIGHT       = 2.03128f;                     // Most common value in dbc
 
-static float const MAX_STEALTH_DETECT_RANGE     = 45.0f;
-static float const GRID_ACTIVATION_RANGE        = 45.0f;
+static constexpr float const MAX_STEALTH_DETECT_RANGE       = 45.0f;
+static constexpr float const GRID_ACTIVATION_RANGE          = 45.0f;
 
 #endif
