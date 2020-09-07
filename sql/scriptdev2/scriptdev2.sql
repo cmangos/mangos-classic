9b0203a8da00d8eaa3794cacd6cd313072c7625b
cala <cala@users.noreply.github.com>
2020-09-07 21:56:16 +0200

    Fix SD2 warnings with TBC+ texts
    * Fix SD2 warnings: mark as reserved SD2 text entries that were added after Classic and are using sound IDs not available in Classic DBC.

--- sql/scriptdev2/scriptdev2.sql
+++ sql/scriptdev2/scriptdev2.sql
@@ -1921,19 +1921,7 @@ INSERT INTO script_texts (entry,content_default,sound,type,language,emote,broadc
 
 ('-1189021','REUSE_ME','0','0','0','0','0','REUSE_ME'),
 
-('-1189022','It is over, your search is done! Let fate choose now, the righteous one.','11961','1','0','0','0','horseman SAY_ENTRANCE'),
-('-1189023','Here''s my body, fit and pure! Now, your blackened souls I''ll cure!','12567','1','0','0','0','horseman SAY_REJOINED'),
-('-1189024','So eager you are for my blood to spill, yet to vanquish me this my head you must kill!','11969','1','0','0','0','horseman SAY_BODY_DEFEAT'),
-('-1189025','Over here, you idiot!','12569','1','0','0','0','horseman SAY_LOST_HEAD'),
-('-1189026','Harken, cur! Tis you I spurn! Now, $N, feel the burn!','12573','1','0','0','0','horseman SAY_CONFLAGRATION'),
-('-1189027','Soldiers arise, stand and fight! Bring victory at last to this fallen knight!','11963','1','0','0','0','horseman SAY_SPROUTING_PUMPKINS'),
-('-1189028','Your body lies beaten, battered and broken. Let my curse be your own, fate has spoken.','11962','1','0','0','0','horseman SAY_SLAY'),
-('-1189029','This end have I reached before. What new adventure lies in store?','11964','1','0','0','0','horseman SAY_DEATH'),
-('-1189030','%s laughs.','0','2','0','0','0','horseman EMOTE_LAUGH'),
-('-1189031','Horseman rise...','0','0','0','0','0','horseman SAY_PLAYER1'),
-('-1189032','Your time is night...','0','0','0','0','0','horseman SAY_PLAYER2'),
-('-1189033','You felt death once...','0','0','0','0','0','horseman SAY_PLAYER3'),
-('-1189034','Now, know demise!','0','0','0','0','0','horseman SAY_PLAYER4'),
+-- USED IN TBC+ -1189022 TO -1189034
 
 ('-1189035','The master has fallen! Avenge him my brethren!','5834','1','0','0','0','trainee SAY_TRAINEE_SPAWN'),
 
