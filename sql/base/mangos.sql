18c576489cdb3875b8dae9f01a08a7354e7edf89
killerwife <killerwife@gmail.com>
2020-11-01 15:09:19 +0100

    [z2767] Change implementation of RegenerateStats new flag into db revision

--- sql/base/mangos.sql
+++ sql/base/mangos.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
-  `required_z2765_01_mangos_command_ahbot` bit(1) DEFAULT NULL
+  `required_z2767_01_mangos_power_regen` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Used DB version notes';
 
 --
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_z2766_01_realmd_account_logons"
  #define REVISION_DB_CHARACTERS "required_z2765_01_characters_item_instance_data_drop"
- #define REVISION_DB_MANGOS "required_z2765_01_mangos_command_ahbot"
+ #define REVISION_DB_MANGOS "required_z2767_01_mangos_power_regen"
 #endif // __REVISION_SQL_H__
