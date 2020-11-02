767224644036aeb8481d817329dd83d9cec9e55f
henhouse <henry925@gmail.com>
2020-11-02 21:28:33 +0100

    Fix team colors in arena scoreboard Closes https://github.com/cmangos/mangos-tbc/pull/406

--- src/game/BattleGround/BattleGroundMgr.cpp
+++ src/game/BattleGround/BattleGroundMgr.cpp
@@ -1025,7 +1025,7 @@ void BattleGroundMgr::BuildPvpLogDataPacket(WorldPacket& data, BattleGround* bg)
     else
     {
         data << uint8(1);                                  // bg ended
-        data << uint8(bg->GetWinner());                    // who wins
+        data << uint8(bg->GetWinner());                    // who won
     }
 
     data << (uint32)(bg->GetPlayerScoresSize());
