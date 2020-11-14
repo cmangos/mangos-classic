2c56a7b28ca58f9be12ee93d29b2c25c85a86663
killerwife <killerwife@gmail.com>
2020-11-14 01:25:41 +0100

    Remove redundant tbc disengage check
    This needs to be in spellscript in wotlk.

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -4277,10 +4277,6 @@ SpellCastResult Spell::CheckCast(bool strict)
         }
     }
 
-    // Spells like Disengage are allowed only in combat
-    if (!m_caster->IsInCombat() && m_spellInfo->HasAttribute(SPELL_ATTR_STOP_ATTACK_TARGET) && m_spellInfo->HasAttribute(SPELL_ATTR_EX2_UNAFFECTED_BY_AURA_SCHOOL_IMMUNE))
-        return SPELL_FAILED_CASTER_AURASTATE;
-
     Unit* target = m_targets.getUnitTarget();
     uint32 affectedMask = GetCheckCastEffectMask(m_spellInfo);
 
