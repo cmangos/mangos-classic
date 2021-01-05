34d90f59ebf03d325c6f369a286de03ef34f234d
Xfurry <xfurry.cmangos@outlook.com>
2021-01-05 19:48:22 +0100

    Fix mistake from previous commit
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>
    
    (based on cmangos/mangos-wotlk@5b8e0a339)
    
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>
    
    (based on cmangos/mangos-tbc@21be759e3)
    
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

--- src/game/AI/ScriptDevAI/include/sc_instance.cpp
+++ src/game/AI/ScriptDevAI/include/sc_instance.cpp
@@ -81,7 +81,11 @@ void ScriptedInstance::DoRespawnGameObject(ObjectGuid guid, uint32 timeToDespawn
 
     if (GameObject* pGo = instance->GetGameObject(guid))
     {
-        if (pGo->IsSpawnedByDefault()) // static spawned go - can only respawn
+        if (pGo->IsSpawned())
+            return;
+
+        // static spawned go - can only respawn
+        if (pGo->IsSpawnedByDefault())
             pGo->Respawn();
         else
         {
