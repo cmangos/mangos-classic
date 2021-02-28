6bb50ae3f5cacc3b0837339296bb77fbdfdd094a
Xfurry <xfurry.cmangos@outlook.com>
2021-02-28 14:21:12 +0100

    Fix some white spaces
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>
    
    (based on cmangos/mangos-wotlk@cd99999fa)
    
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>
    
    (based on cmangos/mangos-tbc@90b19c7ed)
    
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

--- src/game/AI/ScriptDevAI/include/sc_creature.cpp
+++ src/game/AI/ScriptDevAI/include/sc_creature.cpp
@@ -372,13 +372,13 @@ void ScriptedAI::SetEquipmentSlots(bool loadDefault, int32 mainHand, int32 offHa
     }
 
     if (mainHand >= 0)
-    { 
+    {
         m_creature->SetVirtualItem(VIRTUAL_ITEM_SLOT_0, mainHand);
-        m_creature->UpdateDamagePhysical(BASE_ATTACK);            
+        m_creature->UpdateDamagePhysical(BASE_ATTACK);
     }
 
     if (offHand >= 0)
-    { 
+    {
         m_creature->SetVirtualItem(VIRTUAL_ITEM_SLOT_1, offHand);
         if(offHand == 1)
             m_creature->SetCanDualWield(true);
