1e557f8f53a444fe03cfe92d13ba883239e3e09a
celguar <celguar@gmail.com>
2021-03-14 21:12:47 +0300

    Possible fix for disappearing items

--- src/game/Entities/Item.cpp
+++ src/game/Entities/Item.cpp
@@ -289,7 +289,8 @@ void Item::SaveToDB()
         case ITEM_CHANGED:
         {
             static SqlStatementID insItem;
-            SqlStatement stmt = CharacterDatabase.CreateStatement(insItem, uState == ITEM_NEW ?
+            static SqlStatementID updItem;
+            SqlStatement stmt = CharacterDatabase.CreateStatement(uState == ITEM_NEW ? insItem : updItem, uState == ITEM_NEW ?
                 "REPLACE INTO item_instance (owner_guid, itemEntry, creatorGuid, giftCreatorGuid, count, duration, charges, flags, enchantments, randomPropertyId, durability, itemTextId, guid) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)" :
                 "UPDATE item_instance SET owner_guid = ?, itemEntry = ?, creatorGuid = ?, giftCreatorGuid = ?, count = ?, duration = ?, charges = ?, flags = ?, enchantments = ?, randomPropertyId = ?, durability = ?, itemTextId = ? WHERE guid = ?"
             );
