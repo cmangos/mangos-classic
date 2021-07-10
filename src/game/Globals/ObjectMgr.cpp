917f7608f0242b661bb1726b674624327cb5811a
MantisLord <sabinprosper@gmail.com>
2021-07-09 22:31:57 -0400

    Fix typo in LoadGameObjectSpawnEntry
    Should not clear creature spawn entry map

--- src/game/Globals/ObjectMgr.cpp
+++ src/game/Globals/ObjectMgr.cpp
@@ -1500,7 +1500,7 @@ void ObjectMgr::LoadGameObjects()
 
 void ObjectMgr::LoadGameObjectSpawnEntry()
 {
-    m_creatureSpawnEntryMap.clear();
+    m_gameobjectSpawnEntryMap.clear();
 
     QueryResult* result = WorldDatabase.Query("SELECT guid, entry FROM gameobject_spawn_entry");
 
