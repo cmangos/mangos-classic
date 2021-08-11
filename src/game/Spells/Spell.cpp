e65398a62f11ed9ac19adebf25d6a0708a4508f3
killerwife <killerwife@gmail.com>
2021-08-12 00:12:23 +0200

    Immunities: Fix usage of SPELL_ATTR_UNAFFECTED_BY_INVULNERABILITY in Spell::CheckCast

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -4390,9 +4390,8 @@ SpellCastResult Spell::CheckCast(bool strict)
                 }
             }
 
-            uint32 affectedMask = GetCheckCastEffectMask(m_spellInfo);
-            if (IsPositiveSpell(m_spellInfo->Id) && !m_spellInfo->HasAttribute(SPELL_ATTR_UNAFFECTED_BY_INVULNERABILITY))
-                if (target->IsImmuneToSpell(m_spellInfo, target == m_caster, affectedMask) && !target->hasUnitState(UNIT_STAT_ISOLATED))
+            if (IsPositiveSpell(m_spellInfo->Id) && affectedMask)
+                if (target->IsImmuneToSpell(m_spellInfo, target == m_trueCaster, affectedMask))
                     return SPELL_FAILED_TARGET_AURASTATE;
 
             // Must be behind the target.
