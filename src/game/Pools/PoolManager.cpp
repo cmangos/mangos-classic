c20a1b4e5e6e8187bd41552be6d3e21dcba8aabf
killerwife <killerwife@gmail.com>
2021-09-07 10:14:50 +0200

    Dynguid/Pooling: Fix addressing of pooled dynguids properly

--- src/game/Pools/PoolManager.cpp
+++ src/game/Pools/PoolManager.cpp
@@ -312,7 +312,7 @@ void PoolGroup<Creature>::Despawn1Object(MapPersistentState& mapState, uint32 gu
             dataMapState->RemoveCreatureFromGrid(guid, data);
 
             if (Map* dataMap = dataMapState->GetMap())
-                if (Creature* pCreature = dataMap->GetCreature(data->GetObjectGuid(guid)))
+                if (Creature* pCreature = dataMap->GetCreature(guid))
                     pCreature->AddObjectToRemoveList();
         }
     }
@@ -330,7 +330,7 @@ void PoolGroup<GameObject>::Despawn1Object(MapPersistentState& mapState, uint32 
             dataMapState->RemoveGameobjectFromGrid(guid, data);
 
             if (Map* dataMap = dataMapState->GetMap())
-                if (GameObject* pGameobject = dataMap->GetGameObject(ObjectGuid(HIGHGUID_GAMEOBJECT, data->id, guid)))
+                if (GameObject* pGameobject = dataMap->GetGameObject(guid))
                     pGameobject->AddObjectToRemoveList();
         }
     }
@@ -548,7 +548,7 @@ void PoolGroup<GameObject>::ReSpawn1Object(MapPersistentState& mapState, PoolObj
         // for non-instanceable maps pool spawn can be at different map from provided mapState
         if (MapPersistentState* dataMapState = mapState.GetMapId() == data->mapid ? &mapState : sMapPersistentStateMgr.GetPersistentState(data->mapid, 0))
             if (Map* dataMap = dataMapState->GetMap())
-                if (GameObject* pGameobject = dataMap->GetGameObject(ObjectGuid(HIGHGUID_GAMEOBJECT, data->id, obj->guid)))
+                if (GameObject* pGameobject = dataMap->GetGameObject(obj->guid))
                     pGameobject->GetMap()->Add(pGameobject);
     }
 }
