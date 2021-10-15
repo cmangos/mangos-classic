0f5495a519391af610310e8563f8bb714cb93cf1
killerwife <killerwife@gmail.com>
2021-10-15 21:09:21 +0200

    Scripts: Fix crash due to nullptr
    Linux has pulled the wool over itself

--- src/game/DBScripts/ScriptMgr.cpp
+++ src/game/DBScripts/ScriptMgr.cpp
@@ -1396,7 +1396,7 @@ bool ScriptAction::HandleScriptStep()
         if (!GetScriptProcessTargets(dynamic_cast<WorldObject*>(source), dynamic_cast<WorldObject*>(target), sources, targets))
             return false;
 
-        if (source->isType(TYPEMASK_ITEM))
+        if (source && source->isType(TYPEMASK_ITEM))
             itemSource = source;
     }
 
