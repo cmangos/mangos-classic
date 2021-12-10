c021495942bd1dfa6452db5ec002e0811acdcce9
killerwife <killerwife@gmail.com>
2021-12-10 21:08:10 +0100

    Add missing default values to [s2438]

--- sql/updates/mangos/z2783_01_mangos_spawn_groups.sql
+++ sql/updates/mangos/z2783_01_mangos_spawn_groups.sql
@@ -5,9 +5,9 @@ CREATE TABLE spawn_group(
 Id INT NOT NULL COMMENT 'Spawn Group ID',
 Name VARCHAR(200) NOT NULL COMMENT 'Description of usage',
 Type INT NOT NULL COMMENT 'Creature or GO spawn group',
-MaxCount INT NOT NULL COMMENT 'Maximum total count of all spawns in a group',
-WorldState INT NOT NULL COMMENT 'Worldstate which enables spawning of given group',
-Flags INT UNSIGNED NOT NULL COMMENT 'Flags for various behaviour',
+MaxCount INT NOT NULL DEFAULT '0' COMMENT 'Maximum total count of all spawns in a group',
+WorldState INT NOT NULL DEFAULT '0' COMMENT 'Worldstate which enables spawning of given group',
+Flags INT UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Flags for various behaviour',
 PRIMARY KEY(Id)
 );
 
@@ -22,9 +22,9 @@ DROP TABLE IF EXISTS spawn_group_entry;
 CREATE TABLE spawn_group_entry(
 Id INT NOT NULL COMMENT 'Spawn Group ID',
 Entry INT NOT NULL COMMENT 'Entry of creature or GO',
-MinCount INT NOT NULL COMMENT 'Minimum count of entry in a group before random',
-MaxCount INT NOT NULL COMMENT 'Maximum total count of entry in a group',
-Chance INT NOT NULL COMMENT 'Chance for entry to be selected',
+MinCount INT NOT NULL DEFAULT '0' COMMENT 'Minimum count of entry in a group before random',
+MaxCount INT NOT NULL DEFAULT '0' COMMENT 'Maximum total count of entry in a group',
+Chance INT NOT NULL DEFAULT '0' COMMENT 'Chance for entry to be selected',
 PRIMARY KEY(Id, Entry)
 );
 
