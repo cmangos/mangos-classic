5f8b6afb732237f8973d2c475eb10d8eae4839fc
killerwife <killerwife@gmail.com>
2021-12-17 23:19:36 +0100

    Fix SPELL_AURA_MOD_POSSESS not using its innate radius index for channel range check
    Closes https://github.com/cmangos/issues/issues/928

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -3930,6 +3930,8 @@ void Spell::SendChannelStart(uint32 duration)
                     diminishLevel = itr->diminishLevel;
                 }
                 target = ObjectAccessor::GetUnit(*m_caster, itr->targetGUID);
+                if (m_spellInfo->EffectRadiusIndex[EFFECT_INDEX_0] != 0 && m_spellInfo->EffectApplyAuraName[EFFECT_INDEX_0] == SPELL_AURA_MOD_POSSESS)
+                    m_maxRange = GetSpellRadius(sSpellRadiusStore.LookupEntry(m_spellInfo->EffectRadiusIndex[EFFECT_INDEX_0]));
                 break;
             }
         }
