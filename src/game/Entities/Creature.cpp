5caa0ca993866f83a0c5d335659b4e8ced11e386
killerwife <killerwife@gmail.com>
2022-02-17 17:47:10 +0100

    Creature: Do not allow calling Creature::SetInCombatWithZone when AI is not yet existant
    Happens during Creature Create, all calls needs to be after spawning. We can not silently fail Creature::CallAssistance because it will lead to it not working
    Closes https://github.com/cmangos/issues/issues/2677
    Closes https://github.com/cmangos/mangos-tbc/pull/508

--- src/game/Entities/Creature.cpp
+++ src/game/Entities/Creature.cpp
@@ -2034,6 +2034,8 @@ void Creature::CallAssistance()
     // FIXME: should player pets call for assistance?
     if (!m_AlreadyCallAssistance && GetVictim() && !HasCharmer())
     {
+        MANGOS_ASSERT(AI());
+
         SetNoCallAssistance(true);
 
         if (!CanCallForAssistance())
@@ -2207,6 +2209,12 @@ void Creature::SetInCombatWithZone(bool checkAttackability)
     if (!CanHaveThreatList())
     {
         sLog.outError("Creature entry %u call SetInCombatWithZone but creature cannot have threat list.", GetEntry());
+        return;
+    }
+
+    if (!AI())
+    {
+        sLog.outError("Creature entry %u call SetInCombatWithZone but creature does not have AI. Possible call during create.", GetEntry());
         return;
     }
 
