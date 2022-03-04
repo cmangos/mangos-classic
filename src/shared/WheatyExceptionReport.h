d492bb29477132646eee4bda6fe20054e308062a
Cyberium <cyberium@users.noreply.github.com>
2022-03-04 23:59:39 +0100

    [WheatyExceptionReport] Sync WheatyExceptionReport file with TC

--- src/shared/WheatyExceptionReport.h
+++ src/shared/WheatyExceptionReport.h
@@ -1,10 +1,22 @@
 #ifndef _WHEATYEXCEPTIONREPORT_
 #define _WHEATYEXCEPTIONREPORT_
 
-#pragma warning(push)
-#pragma warning(disable:4091)
+#define _NO_CVCONST_H
+
+#include <windows.h>
+#include <winnt.h>
+#include <winternl.h>
 #include <dbghelp.h>
-#pragma warning(pop)
+#include <set>
+#include <cstdlib>
+#include <cstdio>
+#include <stack>
+#include <mutex>
+
+#define WER_MAX_ARRAY_ELEMENTS_COUNT 10
+#define WER_MAX_NESTING_LEVEL 4
+#define WER_SMALL_BUFFER_SIZE 1024
+#define WER_LARGE_BUFFER_SIZE WER_SMALL_BUFFER_SIZE * 16
 
 enum BasicType                                              // Stolen from CVCONST.H in the DIA 2.0 SDK
 {
@@ -25,43 +37,99 @@ enum BasicType                                              // Stolen from CVCON
     btComplex = 28,
     btBit = 29,
     btBSTR = 30,
-    btHresult = 31
+    btHresult = 31,
+
+    // Custom types
+    btStdString = 101
 };
 
-const char* const rgBaseType[] =
+enum DataKind                                              // Stolen from CVCONST.H in the DIA 2.0 SDK
 {
-    " <user defined> ",                                     // btNoType = 0,
-    " void ",                                               // btVoid = 1,
-    " char* ",                                              // btChar = 2,
-    " wchar_t* ",                                           // btWChar = 3,
-    " signed char ",
-    " unsigned char ",
-    " int ",                                                // btInt = 6,
-    " unsigned int ",                                       // btUInt = 7,
-    " float ",                                              // btFloat = 8,
-    " <BCD> ",                                              // btBCD = 9,
-    " bool ",                                               // btBool = 10,
-    " short ",
-    " unsigned short ",
-    " long ",                                               // btLong = 13,
-    " unsigned long ",                                      // btULong = 14,
-    " __int8 ",
-    " __int16 ",
-    " __int32 ",
-    " __int64 ",
-    " __int128 ",
-    " unsigned __int8 ",
-    " unsigned __int16 ",
-    " unsigned __int32 ",
-    " unsigned __int64 ",
-    " unsigned __int128 ",
-    " <currency> ",                                         // btCurrency = 25,
-    " <date> ",                                             // btDate = 26,
-    " VARIANT ",                                            // btVariant = 27,
-    " <complex> ",                                          // btComplex = 28,
-    " <bit> ",                                              // btBit = 29,
-    " BSTR ",                                               // btBSTR = 30,
-    " HRESULT "                                             // btHresult = 31
+    DataIsUnknown,
+    DataIsLocal,
+    DataIsStaticLocal,
+    DataIsParam,
+    DataIsObjectPtr,
+    DataIsFileStatic,
+    DataIsGlobal,
+    DataIsMember,
+    DataIsStaticMember,
+    DataIsConstant
+};
+
+char const* const rgBaseType[] =
+{
+    "<user defined>",                                     // btNoType = 0,
+    "void",                                               // btVoid = 1,
+    "char",//char*                                        // btChar = 2,
+    "wchar_t*",                                           // btWChar = 3,
+    "signed char",
+    "unsigned char",
+    "int",                                                // btInt = 6,
+    "unsigned int",                                       // btUInt = 7,
+    "float",                                              // btFloat = 8,
+    "<BCD>",                                              // btBCD = 9,
+    "bool",                                               // btBool = 10,
+    "short",
+    "unsigned short",
+    "long",                                               // btLong = 13,
+    "unsigned long",                                      // btULong = 14,
+    "int8",
+    "int16",
+    "int32",
+    "int64",
+    "int128",
+    "uint8",
+    "uint16",
+    "uint32",
+    "uint64",
+    "uint128",
+    "<currency>",                                         // btCurrency = 25,
+    "<date>",                                             // btDate = 26,
+    "VARIANT",                                            // btVariant = 27,
+    "<complex>",                                          // btComplex = 28,
+    "<bit>",                                              // btBit = 29,
+    "BSTR",                                               // btBSTR = 30,
+    "HRESULT"                                             // btHresult = 31
+};
+
+struct SymbolPair
+{
+    SymbolPair(DWORD type, DWORD_PTR offset)
+    {
+        _type = type;
+        _offset = offset;
+    }
+
+    bool operator<(SymbolPair const& other) const
+    {
+        return _offset < other._offset ||
+              (_offset == other._offset && _type < other._type);
+    }
+
+    DWORD _type;
+    DWORD_PTR _offset;
+};
+typedef std::set<SymbolPair> SymbolPairs;
+
+struct SymbolDetail
+{
+    SymbolDetail() : Prefix(), Type(), Suffix(), Name(), Value(), Logged(false), HasChildren(false) {}
+
+    std::string ToString();
+
+    bool empty() const
+    {
+        return Value.empty() && !HasChildren;
+    }
+
+    std::string Prefix;
+    std::string Type;
+    std::string Suffix;
+    std::string Name;
+    std::string Value;
+    bool Logged;
+    bool HasChildren;
 };
 
 class WheatyExceptionReport
@@ -75,7 +143,9 @@ class WheatyExceptionReport
         static LONG WINAPI WheatyUnhandledExceptionFilter(
             PEXCEPTION_POINTERS pExceptionInfo);
 
-        static void printTracesForAllThreads();
+        static void __cdecl WheatyCrtHandler(wchar_t const* expression, wchar_t const* function, wchar_t const* file, unsigned int line, uintptr_t pReserved);
+
+        static void printTracesForAllThreads(bool);
     private:
         // where report info is extracted and generated
         static void GenerateExceptionReport(PEXCEPTION_POINTERS pExceptionInfo);
@@ -84,30 +154,48 @@ class WheatyExceptionReport
         static BOOL _GetProcessorName(TCHAR* sProcessorName, DWORD maxcount);
 
         // Helper functions
-        static LPTSTR GetExceptionString(DWORD dwCode);
+        static LPCTSTR GetExceptionString(DWORD dwCode);
         static BOOL GetLogicalAddress(PVOID addr, PTSTR szModule, DWORD len,
-                                      DWORD& section, DWORD_PTR& offset);
+            DWORD& section, DWORD_PTR& offset);
 
         static void WriteStackDetails(PCONTEXT pContext, bool bWriteVariables, HANDLE pThreadHandle);
 
         static BOOL CALLBACK EnumerateSymbolsCallback(PSYMBOL_INFO, ULONG, PVOID);
 
-        static bool FormatSymbolValue(PSYMBOL_INFO, STACKFRAME*, char* pszBuffer, unsigned cbBuffer);
+        static bool FormatSymbolValue(PSYMBOL_INFO, STACKFRAME64 *);
 
-        static char* DumpTypeIndex(char*, DWORD64, DWORD, unsigned, DWORD_PTR, bool&, char*);
+        static void DumpTypeIndex(DWORD64, DWORD, DWORD_PTR, bool &, char const*, char const*, bool, bool);
 
-        static char* FormatOutputValue(char* pszCurrBuffer, BasicType basicType, DWORD64 length, PVOID pAddress);
+        static void FormatOutputValue(char * pszCurrBuffer, BasicType basicType, DWORD64 length, PVOID pAddress, size_t bufferSize, size_t countOverride = 0);
 
         static BasicType GetBasicType(DWORD typeIndex, DWORD64 modBase);
+        static DWORD_PTR DereferenceUnsafePointer(DWORD_PTR address);
 
-        static int _tprintf(const TCHAR* format, ...);
+        static int __cdecl Log(const TCHAR * format, ...);
+
+        static bool StoreSymbol(DWORD type , DWORD_PTR offset);
+        static void ClearSymbols();
 
         // Variables used by the class
         static TCHAR m_szLogFileName[MAX_PATH];
+        static TCHAR m_szDumpFileName[MAX_PATH];
         static LPTOP_LEVEL_EXCEPTION_FILTER m_previousFilter;
-        static HANDLE m_hReportFile;
+        static _invalid_parameter_handler m_previousCrtHandler;
+        static FILE* m_hReportFile;
+        static HANDLE m_hDumpFile;
         static HANDLE m_hProcess;
+        static SymbolPairs symbols;
+        static std::stack<SymbolDetail> symbolDetails;
+        static bool alreadyCrashed;
+        static std::mutex alreadyCrashedLock;
+        typedef NTSTATUS(NTAPI* pRtlGetVersion)(PRTL_OSVERSIONINFOW lpVersionInformation);
+        static pRtlGetVersion RtlGetVersion;
+
+        static void PushSymbolDetail();
+        static void PopSymbolDetail();
+        static void PrintSymbolDetail();
+
 };
 
 extern WheatyExceptionReport g_WheatyExceptionReport;       //  global instance of class
-#endif                                                      // WheatyExceptionReport
+#endif                                                      // _WHEATYEXCEPTIONREPORT_
