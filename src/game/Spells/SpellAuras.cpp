375e0c4f8538a3ffbdbadd5e07a3e08bcd86bbf4
killerwife <killerwife@gmail.com>
2022-05-25 17:45:41 +0200

    Pet/Player: Do not save auras with AURA_INTERRUPT_FLAG_LEAVE_WORLD

--- src/game/Spells/SpellAuras.cpp
+++ src/game/Spells/SpellAuras.cpp
@@ -5980,6 +5980,8 @@ bool SpellAuraHolder::IsSaveToDbHolder() const
     if (GetTrackedAuraType() == TRACK_AURA_TYPE_SINGLE_TARGET)
         return false;
 
+    if (m_spellProto->AuraInterruptFlags & AURA_INTERRUPT_FLAG_LEAVE_WORLD)
+        return false;
 
     if (IsPermanent())
     {
