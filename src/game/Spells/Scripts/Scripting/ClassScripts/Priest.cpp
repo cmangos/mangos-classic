f29f254a8112352e1d087fef278aef664ccd349f
killerwife <killerwife@gmail.com>
2022-07-07 21:54:55 +0200

    Priest: Fix wrong function being used for blackout
    https://github.com/cmangos/issues/issues/3054

--- src/game/Spells/Scripts/Scripting/ClassScripts/Priest.cpp
+++ src/game/Spells/Scripts/Scripting/ClassScripts/Priest.cpp
@@ -47,7 +47,7 @@ struct Blackout : public AuraScript
 {
     bool OnCheckProc(Aura* /*aura*/, ProcExecutionData& data) const override
     {
-        if (data.isHeal || (!data.damage && data.spellInfo && !IsSpellAppliesAura(data.spellInfo, SPELL_AURA_PERIODIC_DAMAGE)))
+        if (data.isHeal || (!data.damage && data.spellInfo && !IsSpellHaveAura(data.spellInfo, SPELL_AURA_PERIODIC_DAMAGE)))
             return false;
         return true;
     }
