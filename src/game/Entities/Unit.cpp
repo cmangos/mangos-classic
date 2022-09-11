937f59e3814e03eb70aae07a3339ef68e5e5f7a6
Henry Henderson <henhouse@users.noreply.github.com>
2022-09-11 11:54:48 +0200

    Fix DR last hit not updating on subsequent debuff fall off (#551)

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -8783,15 +8783,13 @@ void Unit::ApplyDiminishingAura(DiminishingGroup group, bool apply)
         if (i.DRGroup != group)
             continue;
 
+        i.hitTime = WorldTimer::getMSTime();
+
         if (apply)
             i.stack += 1;
         else if (i.stack)
-        {
             i.stack -= 1;
-            // Remember time after last aura from group removed
-            if (i.stack == 0)
-                i.hitTime = WorldTimer::getMSTime();
-        }
+
         break;
     }
 }
