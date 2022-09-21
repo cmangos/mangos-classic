303fc9ca7d21408b5d7917c67a3ac061e2d9dfdb
killerwife <killerwife@gmail.com>
2022-09-21 19:17:54 +0200

    WarEffort: Fix sending of remaining days to send 5 days on start and not 4

--- src/game/World/WorldState.cpp
+++ src/game/World/WorldState.cpp
@@ -1008,7 +1008,7 @@ std::string AhnQirajData::GetData()
 
 uint32 AhnQirajData::GetDaysRemaining() const
 {
-    return uint32(m_timer / (DAY * IN_MILLISECONDS));
+    return uint32(m_timer / (DAY * IN_MILLISECONDS)) + 1;
 }
 
 // Scourge invasion section
