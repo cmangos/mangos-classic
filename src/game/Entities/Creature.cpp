ea020cb4a0597cdfa0ac6fa6cb2566abc46f4123
killerwife <killerwife@gmail.com>
2022-09-30 15:36:28 +0200

    GameEvent: Fix despawn of creatures on game event stop

--- src/game/Entities/Creature.cpp
+++ src/game/Entities/Creature.cpp
@@ -2593,7 +2593,7 @@ void Creature::FillGuidsListFromThreatList(GuidVector& guids, uint32 maxamount /
 
 struct AddCreatureToRemoveListInMapsWorker
 {
-    AddCreatureToRemoveListInMapsWorker(ObjectGuid guid) : i_guid(guid) {}
+    AddCreatureToRemoveListInMapsWorker(uint32 dbGuid) : i_guid(dbGuid) {}
 
     void operator()(Map* map)
     {
@@ -2601,12 +2601,12 @@ struct AddCreatureToRemoveListInMapsWorker
             pCreature->AddObjectToRemoveList();
     }
 
-    ObjectGuid i_guid;
+    uint32 i_guid;
 };
 
 void Creature::AddToRemoveListInMaps(uint32 db_guid, CreatureData const* data)
 {
-    AddCreatureToRemoveListInMapsWorker worker(data->GetObjectGuid(db_guid));
+    AddCreatureToRemoveListInMapsWorker worker(db_guid);
     sMapMgr.DoForAllMapsWithMapId(data->mapid, worker);
 }
 
