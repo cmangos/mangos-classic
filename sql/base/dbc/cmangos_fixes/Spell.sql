e9f952982a571e2acaab9b89b8360d527940b1ef
killerwife <killerwife@gmail.com>
2022-10-18 12:02:54 +0200

    Add some spell.sql changes from tbc

--- sql/base/dbc/cmangos_fixes/Spell.sql
+++ sql/base/dbc/cmangos_fixes/Spell.sql
@@ -288,3 +288,10 @@ UPDATE spell_template SET AttributesServerside = 4 WHERE id IN (11816);
 -- Curse of Tongues - 1.12.0 - unexported to client but present in tbc - "This spell will no longer debuff the target if they are immune to the spellcast slowing effect."
 UPDATE spell_template SET AttributesEx4=AttributesEx4|0x00000800 WHERE Id IN(1714,11719);
 
+-- Bruise should not be removed on evade
+UPDATE spell_template SET AttributesServerside = 4 WHERE id IN (4133);
+
+-- Viewing Room Student Transform - Effect should not be removed on evade - instance_scholomance::HandleDawnGambitEvent()
+UPDATE spell_template SET AttributesServerside = 4 WHERE id IN (18115);
+
+
