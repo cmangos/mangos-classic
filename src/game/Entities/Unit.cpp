5e6c96289e0f0322287aaf48336997ad1f561402
evil-at-wow <evil.at.wow@gmail.com>
2023-01-03 22:41:29 +0100

    Fix memory leaks caused by not really removing summoned pets.
    The story here starts somewhat similar as in the previous commit, and
    starts in Spell::DoSummonPet() with the newly created pet there. This
    time the pet doesn't end up as a guardian pet but as a summoned pet.
    Note that the link from NPC to a summoned pet is basically limited to
    the UNIT_FIELD_SUMMON value.
    
    Now, when both the NPC and the pet are still around at the time their
    grid gets unloaded, the ObjectGridUnloader will basically unload
    the NPC by calling its RemoveFromWorld() and then delete it. Note that
    pets don't get unloaded explicitly because they are so called 'world
    objects' but not 'grid objects'. For an NPC with a summoned pet, none
    of the RemoveFromWorld() call (Creature, Unit, Object) actually removed
    the pet, thus leaking the memory.
    
    So let's just make sure that any non-gardian pets are also unsummoned.

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -9348,6 +9348,11 @@ void Unit::RemoveFromWorld()
         BreakCharmOutgoing();
         BreakCharmIncoming();
         RemoveGuardians(IsPlayer() || IsCreature() && static_cast<Creature*>(this)->IsTotem());
+        // Remove non-guardian pet
+        if (Pet* pet = GetPet())
+        {
+            pet->Unsummon(PET_SAVE_AS_DELETED, this);
+        }
         RemoveMiniPet();
         RemoveAllGameObjects();
         RemoveAllDynObjects();
