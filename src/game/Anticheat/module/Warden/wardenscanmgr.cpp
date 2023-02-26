0e429dcfb11e40d4bb1c584f1149fcfb25abb54c
evil-at-wow <evil.at.wow@gmail.com>
2023-02-26 01:40:02 +0100

    WardenScanMgr::loadFromDB(): fix missing break.
    Apart from fixing a wrong scan being added, this also fixes a memory
    leak where the intended WindowsHookScan was leaked because the 'scan'
    variable was assigned another (wrong) scan due to the missing break.

--- src/game/Anticheat/module/Warden/wardenscanmgr.cpp
+++ src/game/Anticheat/module/Warden/wardenscanmgr.cpp
@@ -183,6 +183,7 @@ void WardenScanMgr::loadFromDB()
                 }
 
                 scan = new WindowsHookScan(module, proc, &hash[0], offset, length, comment, flags);
+                break;
             }
 
             case FIND_DRIVER_BY_NAME:
