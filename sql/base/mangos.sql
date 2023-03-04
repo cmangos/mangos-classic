e72350f2b7f02f4b0978a33664fa59a7ae0620eb
killerwife <killerwife@gmail.com>
2023-03-04 14:52:42 +0100

    Fix db update files

--- sql/base/mangos.sql
+++ sql/base/mangos.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
-  `required_z2802_01_mangos_reference_loot_template_names` bit(1) DEFAULT NULL
+  `required_z2806_01_mangos_creature_drops` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Used DB version notes';
 
 --
--- sql/updates/mangos/z2806_01_mangos_creature_drops.sql
+++ sql/updates/mangos/z2806_01_mangos_creature_drops.sql
@@ -1,4 +1,4 @@
-ALTER TABLE db_version CHANGE COLUMN required_z2801_01_mangos_aggro_range required_z2806_01_mangos_creature_drops bit;
+ALTER TABLE db_version CHANGE COLUMN required_z2805_01_mangos_addon_drop required_z2806_01_mangos_creature_drops bit;
 
 ALTER TABLE creature DROP COLUMN modelid;
 ALTER TABLE creature DROP COLUMN currentwaypoint;
