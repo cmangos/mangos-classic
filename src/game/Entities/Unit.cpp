2b9ac12e0b5e1bc76a14560bb87a25a78c3ef696
killerwife <killerwife@gmail.com>
2023-05-15 12:17:58 +0200

    Fix UnitCondition result in Unit::MeetsSelectAttackingRequirement not being used

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -12117,8 +12117,8 @@ bool Unit::MeetsSelectAttackingRequirement(Unit* target, SpellEntry const* spell
         }
     }
 
-    if (unitConditionId)
-        sObjectMgr.IsUnitConditionSatisfied(unitConditionId, target, this);
+    if (unitConditionId && !sObjectMgr.IsUnitConditionSatisfied(unitConditionId, target, this))
+        return false;
 
     return true;
 }
