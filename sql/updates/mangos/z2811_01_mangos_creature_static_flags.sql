7dfead618788fe8443e3a591780ca1e72a000cf8
killerwife <killerwife@gmail.com>
2023-05-18 18:51:45 +0200

    Fix typoe in previous commit

--- sql/updates/mangos/z2811_01_mangos_creature_static_flags.sql
+++ sql/updates/mangos/z2811_01_mangos_creature_static_flags.sql
@@ -7,7 +7,7 @@ ALTER TABLE creature_template ADD COLUMN `StaticFlags4` INT UNSIGNED NOT NULL DE
 
 -- ports from extraflags
 UPDATE creature_template SET ExtraFlags=ExtraFlags&~0x00000020,StaticFlags2=StaticFlags2|0x00000010 WHERE ExtraFlags&0x00000020;
-UPDATE creature_template SET ExtraFlags=ExtraFlags&~0x00000040,StaticFlags=StaticFlags|0x00000002 WHERE ExtraFlags&0x00000040;
+UPDATE creature_template SET ExtraFlags=ExtraFlags&~0x00000040,StaticFlags1=StaticFlags1|0x00000002 WHERE ExtraFlags&0x00000040;
 UPDATE creature_template SET ExtraFlags=ExtraFlags&~0x00000100,StaticFlags2=StaticFlags2|0x00000040 WHERE ExtraFlags&0x00000100;
 UPDATE creature_template SET ExtraFlags=ExtraFlags&~0x00400000,StaticFlags2=StaticFlags2|0x00000100 WHERE ExtraFlags&0x00400000;
 UPDATE creature_template SET ExtraFlags=ExtraFlags&~0x01000000,StaticFlags2=StaticFlags2|0x40000000 WHERE ExtraFlags&0x01000000;
