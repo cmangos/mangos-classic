cb94498da75f5f1ec29c644fa159690e09f15f6a
killerwife <killerwife@gmail.com>
2023-08-18 21:16:20 +0200

    Map: Properly pass instance id to dk map
    https://github.com/cmangos/issues/issues/3528

--- src/game/Maps/Map.h
+++ src/game/Maps/Map.h
@@ -506,7 +506,7 @@ class WorldMap : public Map
     private:
         using Map::GetPersistentState;                      // hide in subclass for overwrite
     public:
-        WorldMap(uint32 id, time_t expiry) : Map(id, expiry, 0) {}
+        WorldMap(uint32 id, time_t expiry, uint32 InstanceId) : Map(id, expiry, InstanceId) {}
         ~WorldMap() {}
 
         // can't be nullptr for loaded map
