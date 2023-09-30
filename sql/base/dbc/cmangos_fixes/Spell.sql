f07f023a12c28be2e86fca0dd78f4e6942586107
AnonXS <anonxsdev@gmail.com>
2023-09-30 11:05:26 +0200

    [Spell] Fire Shield 13377 should not be removed on Evade

--- sql/base/dbc/cmangos_fixes/Spell.sql
+++ sql/base/dbc/cmangos_fixes/Spell.sql
@@ -283,6 +283,7 @@ UPDATE `spell_template` SET `AttributesServerSide` = `AttributesServerSide`|4 WH
 10868, -- Frost Vulnerable
 11011, -- Stone Watcher of Norgannon Passive
 11048, -- Perm. Illusion Bishop Tyriona
+13377, -- Fire Shield
 15978, -- Puncture
 21911, -- Puncture
 24692, -- Hakkar Power
