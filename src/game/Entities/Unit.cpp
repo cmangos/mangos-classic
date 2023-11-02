815a84df3c086a981f42e60e7cbe3e84a1cbcd98
Yury <rip95_95@mail.ru>
2023-11-02 22:26:31 +0200

    Unit: Stop chasing the charmer to allow following him correctly after being charmed

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -11300,6 +11300,10 @@ bool Unit::TakeCharmOf(Unit* charmed, uint32 spellId, bool advertised /*= true*/
         charmed->ClearInCombat();
     }
 
+    MotionMaster* mm = charmed->GetMotionMaster();
+    if (mm->GetCurrentMovementGeneratorType() == CHASE_MOTION_TYPE)
+        mm->MovementExpired();
+
     if (UnitAI* ai = charmed->AI())
         ai->JustGotCharmed(this);
 
