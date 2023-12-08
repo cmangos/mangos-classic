a847a04953462ddf052da489050e663524c4ca02
Scarabol <scarabol@gmail.com>
2023-12-08 01:36:22 +0100

    Fix set facing for objects leaving dirty movement flags behind

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -4366,7 +4366,7 @@ void Unit::SetFacingTo(float ori)
     init.Launch();
     // orientation change is in-place
     UpdateSplinePosition();
-    movespline->_Finalize();
+    DisableSpline();
 }
 
 void Unit::SetFacingToObject(WorldObject* object)
@@ -4380,7 +4380,7 @@ void Unit::SetFacingToObject(WorldObject* object)
     init.Launch();
     // orientation change is in-place
     UpdateSplinePosition();
-    movespline->_Finalize();
+    DisableSpline();
 }
 
 bool Unit::isInAccessablePlaceFor(Unit const* unit) const
