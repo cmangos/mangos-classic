8fd9ed78dfd528ca2001b387f26b176ef320ce38
killerwife <killerwife@gmail.com>
2024-01-31 16:18:11 +0100

    Realm/Core: Improve start script for mangosd and add realmd one

--- src/realmd/Main.cpp
+++ src/realmd/Main.cpp
@@ -68,13 +68,14 @@ bool StartDB();
 void UnhookSignals();
 void HookSignals();
 
-bool stopEvent = false;                                     ///< Setting it to true stops the server
+bool stopEvent = false;                                     // Setting it to true stops the server
+bool restart = false;
 
-DatabaseType LoginDatabase;                                 ///< Accessor to the realm server database
+DatabaseType LoginDatabase;                                 // Accessor to the realm server database
 
 boost::asio::io_service service;
 
-/// Launch the realm server
+// Launch the realm server
 int main(int argc, char* argv[])
 {
     std::string configFile, serviceParameter;
@@ -164,7 +165,7 @@ int main(int argc, char* argv[])
     sLog.outString("Using commit hash(%s) committed on %s", REVISION_ID, REVISION_DATE);
     sLog.outString("Using configuration file %s.", configFile.c_str());
 
-    ///- Check the version of the configuration file
+    // Check the version of the configuration file
     uint32 confVersion = sConfig.GetIntDefault("ConfVersion", 0);
     if (confVersion < _REALMDCONFVERSION)
     {
@@ -197,7 +198,7 @@ int main(int argc, char* argv[])
     sLog.outString();
     sLog.outString("<Ctrl-C> to stop.");
 
-    /// realmd PID file creation
+    // realmd PID file creation
     std::string pidfile = sConfig.GetStringDefault("PidFile");
     if (!pidfile.empty())
     {
@@ -212,14 +213,14 @@ int main(int argc, char* argv[])
         sLog.outString("Daemon PID: %u\n", pid);
     }
 
-    ///- Initialize the database connection
+    // Initialize the database connection
     if (!StartDB())
     {
         Log::WaitBeforeContinueIfNeed();
         return 1;
     }
 
-    ///- Get the list of realms for the server
+    // Get the list of realms for the server
     sRealmList.Initialize(sConfig.GetIntDefault("RealmsStateUpdateDelay", 20));
     if (sRealmList.size() == 0)
     {
@@ -245,10 +246,10 @@ int main(int argc, char* argv[])
     for (uint32 i = 0; i < networkThreadCount; ++i)
         threads.emplace_back([&]() { service.run(); });
 
-    ///- Catch termination signals
+    // Catch termination signals
     HookSignals();
 
-    ///- Handle affinity for multiple processors and process priority on Windows
+    // Handle affinity for multiple processors and process priority on Windows
 #ifdef _WIN32
     {
         HANDLE hProcess = GetCurrentProcess();
@@ -322,37 +323,38 @@ int main(int argc, char* argv[])
     for (uint32 i = 0; i < networkThreadCount; ++i)
         threads[i].join();
 
-    ///- Wait for the delay thread to exit
+    // Wait for the delay thread to exit
     LoginDatabase.HaltDelayThread();
 
-    ///- Remove signal handling before leaving
+    // Remove signal handling before leaving
     UnhookSignals();
 
     sLog.outString("Halting process...");
-    return 0;
+    return restart ? 2 : 0; // unified exit codes with mangosd
 }
 
-/// Handle termination signals
+// Handle termination signals
 /** Put the global variable stopEvent to 'true' if a termination signal is caught **/
 void OnSignal(int s)
 {
     switch (s)
     {
         case SIGINT:
-        case SIGTERM:
             stopEvent = true;
+            restart = true;
             break;
+        case SIGTERM:
 #ifdef _WIN32
         case SIGBREAK:
+#endif
             stopEvent = true;
             break;
-#endif
     }
 
     signal(s, OnSignal);
 }
 
-/// Initialize connection to the database
+// Initialize connection to the database
 bool StartDB()
 {
     std::string dbstring = sConfig.GetStringDefault("LoginDatabaseInfo");
@@ -372,7 +374,7 @@ bool StartDB()
 
     if (!LoginDatabase.CheckRequiredField("realmd_db_version", REVISION_DB_REALMD))
     {
-        ///- Wait for already started DB delay threads to end
+        // Wait for already started DB delay threads to end
         LoginDatabase.HaltDelayThread();
         return false;
     }
@@ -380,7 +382,7 @@ bool StartDB()
     return true;
 }
 
-/// Define hook 'OnSignal' for all termination signals
+// Define hook 'OnSignal' for all termination signals
 void HookSignals()
 {
     signal(SIGINT, OnSignal);
@@ -390,7 +392,7 @@ void HookSignals()
 #endif
 }
 
-/// Unhook the signals before leaving
+// Unhook the signals before leaving
 void UnhookSignals()
 {
     signal(SIGINT, nullptr);
