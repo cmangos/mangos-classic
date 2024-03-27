ba87869979e44557194a3158b8ea0f354ed887ad
huptiq <78695922+huptiq@users.noreply.github.com>
2024-03-27 17:07:38 +0100

    Granting honor on PvP-Kill blizzlike
    Added "IsTrivialLevelDifference" to grant honor when the victim has green, yellow or red name.
    
    According to blizzards wow website in 2005 (waybackmachine) honor is granted when the name is green, yellow or red.
    
    "Honorable Kill: killing a player character within your XP range, i.e. a character whose name is colored (green to red), not grey. [...]"
    
    source: https://web.archive.org/web/20050619021442/http://www.worldofwarcraft.com/pvp/honorguide.html

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -6650,7 +6650,7 @@ bool Player::RewardHonor(Unit* uVictim, uint32 groupsize)
         if (GetTeam() == pVictim->GetTeam())
             return false;
 
-        if (GetLevel() < (pVictim->GetLevel() + 5))
+        if (!MaNGOS::XP::IsTrivialLevelDifference(GetLevel(), pVictim->GetLevel()))
         {
             AddHonorCP(MaNGOS::Honor::HonorableKillPoints(this, pVictim, groupsize), HONORABLE, pVictim);
             return true;
