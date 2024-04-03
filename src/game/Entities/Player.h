0fa92a19bc8449ab1e8f8d0b135ac3f158c39ceb
Exxenoz <exxenoz@gmx.at>
2024-04-03 23:36:51 +0200

    Core/PacketIO: Update MSG_QUEST_PUSH_RESULT
    Picked from: https://github.com/vmangos/core
    
    Co-authored-by: brotalnia <brotalnia@gmail.com>
    
    https://github.com/vmangos/core/commit/b9b006db08fcdc055057ff68a3f9fe295ead3ba5

--- src/game/Entities/Player.h
+++ src/game/Entities/Player.h
@@ -1335,7 +1335,7 @@ class Player : public Unit
         void SendQuestTimerFailed(uint32 quest_id) const;
         void SendCanTakeQuestResponse(uint32 msg) const;
         void SendQuestConfirmAccept(Quest const* pQuest, Player* pReceiver) const;
-        void SendPushToPartyResponse(Player* pPlayer, uint32 msg) const;
+        void SendPushToPartyResponse(Player* pPlayer, QuestShareMessages msg) const;
         void SendQuestUpdateAddItem(Quest const* pQuest, uint32 item_idx, uint32 current, uint32 count);
         void SendQuestUpdateAddCreatureOrGo(Quest const* pQuest, ObjectGuid guid, uint32 creatureOrGO_idx, uint32 count);
 
