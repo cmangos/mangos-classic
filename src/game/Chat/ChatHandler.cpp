8a569a946ce367efa29b0cef098f7af6c45d27d6
Flekz <davidparraausina@gmail.com>
2024-04-27 19:32:38 +0100

    playerbots: Fix chat commands for group to not be sent into all the raid members

--- src/game/Chat/ChatHandler.cpp
+++ src/game/Chat/ChatHandler.cpp
@@ -335,7 +335,7 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
             for (GroupReference* itr = group->GetFirstMember(); itr != NULL; itr = itr->next())
             {
                 Player* player = itr->getSource();
-                if (player && player->GetPlayerbotAI())
+                if (player && player->GetPlayerbotAI() && player->IsInGroup(GetPlayer(), true))
                 {
                     player->GetPlayerbotAI()->HandleCommand(type, msg, *GetPlayer(), lang);
                     GetPlayer()->m_speakTime = 0;
