7a2c28af4c87cdf6f859ea0ecf5c8c9e5dc8a1ef
insunaa <insuna@no.capfr.fr>
2024-05-07 23:03:34 +0200

    [z2824] Creature: Fix naming of modelId in Classic Closes https://github.com/cmangos/mangos-classic/pull/554

---
+++ sql/updates/mangos/z2824_01_mangos_model_unification.sql
@@ -0,0 +1,5 @@
+ALTER TABLE db_version CHANGE COLUMN required_z2823_01_mangos_displayid_probability required_z2824_01_mangos_model_unification bit;
+
+ALTER TABLE `creature_model_info` RENAME COLUMN `modelid_other_team` TO `modelid_alternative`;
+
+
