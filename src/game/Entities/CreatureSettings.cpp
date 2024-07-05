5e7656e31c23b22f0bd5f907f55ae5294bdb859f
killerwife <killerwife@gmail.com>
2024-07-05 09:59:03 +0200

    Creature: Implement several needed static flags

--- src/game/Entities/CreatureSettings.cpp
+++ src/game/Entities/CreatureSettings.cpp
@@ -40,6 +40,13 @@ void CreatureSettings::ResetStaticFlags(CreatureStaticFlags staticFlags, Creatur
         m_owner->GetVisibilityData().SetVisibilityDistanceOverride(VisibilityDistanceType::Gigantic);
     if (HasFlag(CreatureStaticFlags3::INFINITE_AOI))
         m_owner->GetVisibilityData().SetVisibilityDistanceOverride(VisibilityDistanceType::Infinite);
+
+    if (HasFlag(CreatureStaticFlags::UNINTERACTIBLE))
+        m_owner->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_UNINTERACTIBLE);
+    if (HasFlag(CreatureStaticFlags::IMMUNE_TO_PC))
+        m_owner->SetImmuneToPlayer(true);
+    if (HasFlag(CreatureStaticFlags::IMMUNE_TO_NPC))
+        m_owner->SetImmuneToNPC(true);
 }
 
 void CreatureSettings::SetFlag(CreatureStaticFlags flag)
