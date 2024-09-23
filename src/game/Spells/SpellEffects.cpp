f0193e024a72d004b6912c9cab58928dc2ae38ab
killerwife <killerwife@gmail.com>
2024-09-23 18:43:34 +0200

    Spell: Fix division by zero in pull effect

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -5366,14 +5366,11 @@ void Spell::EffectPullTowards(SpellEffectIndex eff_idx)
         dist = sqrt(unitTarget->GetDistance2d(x, y, DIST_CALC_NONE));
     }
 
-    if (damage && dist > damage)
-        dist = float(damage);
-
     if (dist < 0.1f)
         return;
 
     // Projectile motion
-    float speedXY = float(m_spellInfo->EffectMiscValue[eff_idx]) * 0.1f;
+    float speedXY = float(std::max(1, m_spellInfo->EffectMiscValue[eff_idx])) * 0.1f;
     float time = dist / speedXY;
     float speedZ = ((z - unitTarget->GetPositionZ()) + 0.5f * time * time * Movement::gravity) / time;
     float angle = unitTarget->GetAngle(x, y);
