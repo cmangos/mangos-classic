c961cfc8ce33631e7236d381d83a0775f4d78ca2
AnonXS <anonxs@web.de>
2024-11-07 13:01:11 +0100

    [Spell] Barbs 8393 should not be removed on Evade

--- sql/base/dbc/cmangos_fixes/Spell.sql
+++ sql/base/dbc/cmangos_fixes/Spell.sql
@@ -302,6 +302,7 @@ UPDATE `spell_template` SET `AttributesServerSide` = `AttributesServerSide`|0x00
 6742,  -- Bloodlust
 7486, -- Butcher Drain
 8273, -- Bonus Dodge
+8393, -- Barbs
 8852,  -- Moss Hide
 9463, -- Mirefin Fungus
 11816, -- Land Mine Arming
