68023af89b94f75f3d1b75f3cec8358ed7bc1ca3
_daanieL <daaniel743@gmail.com>
2025-05-07 12:23:42 +0200

    Spell: Compact Harvest Reaper should have same level as the summoning… (#733)
    * Spell: Compact Harvest Reaper should have same level as the summoning npc
    
    * Remove unneded aurascript

--- sql/scriptdev2/spell.sql
+++ sql/scriptdev2/spell.sql
@@ -29,6 +29,7 @@ INSERT INTO spell_scripts(Id, ScriptName) VALUES
 (6467,'spell_unarmed_woodcutter'),
 (7054,'spell_forsaken_skill'),
 (7131,'spell_illusion_passive'),
+(7979,'spell_compact_harvest_reaper'),
 (8555,'spell_left_for_dead'),
 (8603,'spell_tribal_death'),
 (8655,'spell_tribal_death'),
--- src/game/AI/ScriptDevAI/scripts/kalimdor/stonetalon_mountains.cpp
+++ src/game/AI/ScriptDevAI/scripts/kalimdor/stonetalon_mountains.cpp
@@ -212,6 +212,15 @@ GameObjectAI* GetAIgo_covertops(GameObject* pGo)
     return new go_covertopsAI(pGo);
 }
 
+// Venture Co. Machine Smith - summon Compact Harvest Reaper
+struct CompactHarvestReaper : public SpellScript
+{
+    void OnSummon(Spell* spell, Creature* summon) const override
+    {
+        summon->SetLevel(spell->GetCaster()->GetLevel());
+    }
+};
+
 /*######
 ## AddSC
 ######*/
@@ -228,4 +237,6 @@ void AddSC_stonetalon_mountains()
     pNewScript->Name = "go_covert_ops";
     pNewScript->GetGameObjectAI = &GetAIgo_covertops;
     pNewScript->RegisterSelf();
+
+    RegisterSpellScript<CompactHarvestReaper>("spell_compact_harvest_reaper");
 }
