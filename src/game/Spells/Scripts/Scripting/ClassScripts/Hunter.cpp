0952fb28d102f058af32a7f391d6c5ad0ae5452a
Miraco <daaniel743@gmail.com>
2025-10-27 12:35:34 +0100

    [Hunter] Fix 'Taming the Beast' issues Closes https://github.com/cmangos/mangos-tbc/pull/777

--- src/game/Spells/Scripts/Scripting/ClassScripts/Hunter.cpp
+++ src/game/Spells/Scripts/Scripting/ClassScripts/Hunter.cpp
@@ -42,8 +42,35 @@ struct TameBeastDummy : public SpellScript
 
 // TODO: some evidence tbc pet growl scales with hunter AP
 
+// 19678 - Tame Adult Plainstrider
+struct TamingPetRodAura : public AuraScript
+{
+    void OnApply(Aura* aura, bool apply) const override
+    {
+        Unit* caster = aura->GetCaster();
+        Unit* target = aura->GetTarget();
+
+        if (!target->IsCreature())
+            return;
+
+        Creature* creature = static_cast<Creature*>(target);
+
+        if (apply)
+        {
+            if (caster && caster->IsPlayer())
+                creature->GetMotionMaster()->MoveFollow(caster, PET_FOLLOW_DIST, PET_FOLLOW_ANGLE);   
+        }
+        else
+        {
+            if (aura->GetRemoveMode() == AURA_REMOVE_BY_EXPIRE)
+                creature->ForcedDespawn();     
+        }
+    }
+};
+
 void LoadHunterScripts()
 {
     RegisterSpellScript<TameBeastChannel>("spell_tame_beast_channel");
     RegisterSpellScript<TameBeastDummy>("spell_tame_beast_dummy");
+    RegisterSpellScript<TamingPetRodAura>("spell_taming_pet_rod");
 }
