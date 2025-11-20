4803f0370df85047d3cff39e5b347cea2a788c1d
killerwife <killerwife@gmail.com>
2025-11-20 17:33:59 +0100

    Fix default of iconname

--- sql/updates/mangos/z2830_01_mangos_icon_name.sql
+++ sql/updates/mangos/z2830_01_mangos_icon_name.sql
@@ -1,3 +1,3 @@
 ALTER TABLE db_version CHANGE COLUMN required_z2829_01_mangos_proc_cooldown required_z2830_01_mangos_icon_name bit;
 
-ALTER TABLE `gameobject_template` ADD COLUMN `IconName` varchar(100) NOT NULL AFTER `name`;
+ALTER TABLE `gameobject_template` ADD COLUMN `IconName` varchar(100) NOT NULL DEFAULT '' AFTER `name`;
