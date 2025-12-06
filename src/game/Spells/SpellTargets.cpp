21fb99db3ae8de083500bb2c75a8b19a1ace6113
killerwife <killerwife@gmail.com>
2025-12-06 22:32:56 +0100

    Spell/Targeting: Fix case of event effect consuming target for subsequent

--- src/game/Spells/SpellTargets.cpp
+++ src/game/Spells/SpellTargets.cpp
@@ -390,6 +390,8 @@ void SpellTargetMgr::Initialize()
                                             ignore = true;
                                             break;
                                         }
+                                        if (effectTargetingSource.requiredTarget == TARGET_TYPE_NONE)
+                                            continue;
                                         // aoe and chain case
                                         if (info.enumerator == TARGET_ENUMERATOR_CHAIN) // chain checks chaining
                                             // if chain targets are not the same, cant ignore
@@ -487,7 +489,12 @@ bool SpellTargetMgr::CanEffectConsumeTarget(SpellTargetImplicitType targetType, 
     switch (targetType)
     {
         // incomplete mapping - adjust as needed
-        case TARGET_TYPE_UNIT: if (effectTargetType == TARGET_TYPE_LOCATION_DEST) return false; // 43178
+        case TARGET_TYPE_UNIT:
+            if (effectTargetType == TARGET_TYPE_LOCATION_DEST)
+                return false; // 43178
+            if (effectTargetType == TARGET_TYPE_NONE)
+                return false; // 51957
+            break;
         default: break;
     }
     return true;
