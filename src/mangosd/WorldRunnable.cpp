d12358b6ddf198025ee67538ca49fc7349c073c4
Khallil <khallilbailey@gmail.com>
2026-06-07 20:38:47 -0700

    fix: use atomic load for world loop counter in logging

--- src/mangosd/WorldRunnable.cpp
+++ src/mangosd/WorldRunnable.cpp
@@ -96,7 +96,7 @@ void WorldRunnable::run()
         else
         {
             ++overCounter;
-            sLog.outString("WorldRunnable:run Long loop #%d : %dms (total : %d loop(s), %.3f%%)", World::m_worldLoopCounter, diffTime, overCounter, (float)(100*overCounter) / (float)World::m_worldLoopCounter);
+            sLog.outString("WorldRunnable:run Long loop #%d : %dms (total : %d loop(s), %.3f%%)", World::m_worldLoopCounter.load(), diffTime, overCounter, (float)(100*overCounter) / (float)World::m_worldLoopCounter.load());
         }
 #endif
 
