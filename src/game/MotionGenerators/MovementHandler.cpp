9ba2ca4580521b701199710712321cbb50d7e5d6
killerwife <killerwife@gmail.com>
2026-06-10 18:39:08 +0200

    Movement: Send movement packets to all who see me rather than grid search

--- src/game/MotionGenerators/MovementHandler.cpp
+++ src/game/MotionGenerators/MovementHandler.cpp
@@ -389,7 +389,7 @@ void WorldSession::HandleMovementOpcodes(WorldPacket& recv_data)
     WorldPacket data(opcode, recv_data.size());
     data << mover->GetPackGUID();             // write guid
     movementInfo.Write(data);                               // write data
-    mover->SendMessageToSetExcept(data, _player);
+    mover->SendMessageToAllWhoSeeMe(data, false);
 }
 
 void WorldSession::HandleForceSpeedChangeAckOpcodes(WorldPacket& recv_data)
@@ -446,7 +446,7 @@ void WorldSession::HandleForceSpeedChangeAckOpcodes(WorldPacket& recv_data)
     data << guid.WriteAsPacked();
     data << movementInfo;
     data << newspeed;
-    mover->SendMessageToSetExcept(data, _player);
+    mover->SendMessageToAllWhoSeeMe(data, false);
 
     // skip all forced speed changes except last and unexpected
     // in run/mounted case used one ACK and it must be skipped.m_forced_speed_changes[MOVE_RUN} store both.
@@ -561,7 +561,7 @@ void WorldSession::HandleMoveKnockBackAck(WorldPacket& recv_data)
     data << movementInfo.jump.sinAngle;
     data << movementInfo.jump.xyspeed;
     data << movementInfo.jump.zspeed;
-    mover->SendMessageToSetExcept(data, _player);
+    mover->SendMessageToAllWhoSeeMe(data, false);
 }
 
 void WorldSession::SendKnockBack(Unit* who, float angle, float horizontalSpeed, float verticalSpeed)
@@ -630,7 +630,7 @@ void WorldSession::HandleMoveFlagChangeOpcode(WorldPacket& recv_data)
     WorldPacket data(response, 8);
     data << guid.WriteAsPacked();
     data << movementInfo;
-    mover->SendMessageToSetExcept(data, _player);
+    mover->SendMessageToAllWhoSeeMe(data, false);
 }
 
 void WorldSession::HandleMoveRootAck(WorldPacket& recv_data)
@@ -666,11 +666,10 @@ void WorldSession::HandleMoveRootAck(WorldPacket& recv_data)
     if (!ProcessMovementInfo(movementInfo, mover, _player, recv_data))
         return;
 
-
     WorldPacket data(opcode == CMSG_FORCE_MOVE_UNROOT_ACK ? MSG_MOVE_UNROOT : MSG_MOVE_ROOT);
     data << guid.WriteAsPacked();
     data << movementInfo;
-    mover->SendMessageToSetExcept(data, _player);
+    mover->SendMessageToAllWhoSeeMe(data, false);
 }
 
 void WorldSession::HandleSummonResponseOpcode(WorldPacket& recv_data)
