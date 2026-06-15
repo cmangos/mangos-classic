b65a02eda1fa4f98df8131d30d691b59617026b8
killerwife <killerwife@gmail.com>
2026-06-15 21:47:58 +0200

    Fix crash due to unported MoveSplineInitArgs::Validate logging code
    Closes https://github.com/cmangos/issues/issues/4064
    
    Co-Authored-By: insuna <insuna@no.capfr.fr>

--- src/game/Movement/MoveSpline.cpp
+++ src/game/Movement/MoveSpline.cpp
@@ -168,7 +168,10 @@ namespace Movement
 #define CHECK(exp) \
     if (!(exp))\
     {\
-        sLog.outError("MoveSplineInitArgs::Validate: expression '%s' failed for %s", #exp, unit->GetGuidStr().c_str());\
+        if (unit)\
+            sLog.outError("MoveSplineInitArgs::Validate: expression '%s' failed for %s", #exp, unit->GetGuidStr().c_str());\
+        else\
+            sLog.outError("MoveSplineInitArgs::Validate: expression '%s' failed for cyclic spline continuation", #exp);\
         return false;\
     }
         CHECK(path.size() > 1);
