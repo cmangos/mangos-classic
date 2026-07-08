a7ba01274ee4b1010739f6b042833f32c0881cd1
insunaa <dev.insuna@gmail.com>
2026-07-08 11:49:44 +0200

    Fixes "Auto-Shot" spamming clipping itself.

--- src/game/Spells/SpellHandler.cpp
+++ src/game/Spells/SpellHandler.cpp
@@ -394,6 +394,11 @@ void WorldSession::HandleCastSpellOpcode(WorldPacket& recvPacket)
     if (HasMissingTargetFromClient(spellInfo))
         targets.setUnitTarget(mover->GetTarget());
 
+    if (IsAutoRepeatRangedSpell(spellInfo))
+        if (const Spell* repeatSpell = caster->GetCurrentSpell(CURRENT_AUTOREPEAT_SPELL))
+            if (spellId == repeatSpell->m_spellInfo->Id)
+                return;
+
     Spell* spell = new Spell(caster, spellInfo, TRIGGERED_NONE);
     spell->m_clientCast = true;
     spell->SpellStart(&targets);
