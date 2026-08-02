cmangos/mangos-tbc

Hunter: Reimplement script for tame beast and add level check

src/game/Spells/Scripts/Scripting/ClassScripts/Hunter.cpp
@@ -69,6 +69,27 @@ struct HuntersMark : public AuraScript
     }
 };
 
+// 1515 - Tame Beast
+struct TameBeast : public SpellScript, public AuraScript
+{
+    SpellCastResult OnCheckCast(Spell* spell, bool /*strict*/) const override
+    {
+        Unit* target = spell->m_targets.getUnitTarget();
+        if (!target)
+            return SPELL_FAILED_BAD_IMPLICIT_TARGETS;
+        if (target->GetLevel() > spell->GetCaster()->GetLevel())
+            return SPELL_FAILED_HIGHLEVEL;
+        return SPELL_CAST_OK;
+    }
+
+    void OnApply(Aura* aura, bool apply) const override
+    {
+        if (aura->GetEffIndex() == EFFECT_INDEX_0 && apply)
+            if (Unit* caster = aura->GetCaster()) // Wotlk - sniff - adds 1000 threat
+                aura->GetTarget()->AddThreat(caster, 1000.0f, false, GetSpellSchoolMask(aura->GetSpellProto()), aura->GetSpellProto());
+    }
+};
+
 // 34026 - Kill Command
 struct KillCommand : public SpellScript
 {
@@ -187,6 +208,7 @@ struct TamingPetRodAura : public AuraScript
 void LoadHunterScripts()
 {
     RegisterSpellScript<HuntersMark>("spell_hunters_mark");
+    RegisterSpellScript<TameBeast>("spell_tame_beast");
     RegisterSpellScript<KillCommand>("spell_kill_command");
     RegisterSpellScript<Misdirection>("spell_misdirection");
     RegisterSpellScript<ExposeWeakness>("spell_expose_weakness");

cmangos/mangos-classic

src/game/Spells/Scripts/Scripting/ClassScripts/Hunter.cpp
@@ -19,8 +19,19 @@
 #include "Spells/Scripts/SpellScript.h"
 #include "Spells/SpellAuras.h"
 
-struct TameBeastChannel : public AuraScript
+// 1515 - Tame Beast
+struct TameBeastChannel : public SpellScript, public AuraScript
 {
+    SpellCastResult OnCheckCast(Spell* spell, bool /*strict*/) const override
+    {
+        Unit* target = spell->m_targets.getUnitTarget();
+        if (!target)
+            return SPELL_FAILED_BAD_IMPLICIT_TARGETS;
+        if (target->GetLevel() > spell->GetCaster()->GetLevel())
+            return SPELL_FAILED_HIGHLEVEL;
+        return SPELL_CAST_OK;
+    }
+
     void OnPeriodicTrigger(Aura* aura, PeriodicTriggerData& data) const override
     {
         data.caster = aura->GetCaster();
@@ -28,6 +39,7 @@ struct TameBeastChannel : public AuraScript
     }
 };
 
+// 13535 - Tame Beast
 struct TameBeastDummy : public SpellScript
 {
     void OnEffectExecute(Spell* spell, SpellEffectIndex effIdx) const override